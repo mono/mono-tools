463ba0e5f81fafa89724176e539fa4bd1659ab20
Miguel de Icaza <miguel@gnome.org>
2008-01-22 21:34:19 +0000

    Autotoolize
    svn path=/trunk/mono-tools/; revision=93578

--- gui-compare/gtk-gui/guicompare.ProviderSelector.cs
+++ gui-compare/gtk-gui/guicompare.ProviderSelector.cs
@@ -71,7 +71,6 @@ namespace guicompare {
             this.radiobutton1 = new Gtk.RadioButton(Mono.Unix.Catalog.GetString("API Description"));
             this.radiobutton1.CanFocus = true;
             this.radiobutton1.Name = "radiobutton1";
-            this.radiobutton1.Active = true;
             this.radiobutton1.DrawIndicator = true;
             this.radiobutton1.UseUnderline = true;
             this.radiobutton1.Group = new GLib.SList(System.IntPtr.Zero);
