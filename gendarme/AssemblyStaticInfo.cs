2d2b5ea7493266b5b16b63abaad3596ac1832a1b
Sebastien Pouliot <sebastien@ximian.com>
2008-02-17 21:42:24 +0000

    2008-02-17  Sebastien Pouliot  <sebastien@ximian.com>
    	* AssemblyInfo.cs.in: Remove static stuff and keep only the version.
    	* AssemblyStaticInfo.cs: New. Keep static/shared stuff here.
    	* self-test.ignore: New. Start an ignore list for self-test.
    	* gendarme.mds: Update MonoDevelop project file.
    	* Makefile.am: Update build file.
    
    
    svn path=/trunk/mono-tools/; revision=96006

---
+++ gendarme/AssemblyStaticInfo.cs
@@ -0,0 +1,23 @@
+//
+// AssemblyStaticInfo.cs for Gendarme
+//
+// Authors:
+//	Sebastien Pouliot  <sebastien@ximian.com>
+//
+// Copyright (C) 2005-2008 Novell, Inc (http://www.novell.com)
+//
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+
+[assembly: AssemblyTitle ("Gendarme")]
+[assembly: AssemblyDescription ("Rule-based assembly analyzer")]
+[assembly: AssemblyCopyright ("Copyright (C) 2005-2008 Novell, Inc. and contributors")]
+[assembly: AssemblyCompany ("Novell, Inc.")]
+
+[assembly: PermissionSet (SecurityAction.RequestMinimum, Unrestricted = true)]
+[assembly: CLSCompliant (false)]
+[assembly: ComVisible (false)]
