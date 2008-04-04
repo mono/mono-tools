11e255708a678cd6786c3c06dbc87c9b5ef30388
Sebastien Pouliot <sebastien@ximian.com>
2008-04-04 02:05:45 +0000

    2008-04-03  Daniel Abramov <ex@vingrad.ru>
    	* ImplementGenericCollectionInterfacesTest.cs: New. Unit tests
    	for ImplementGenericCollectionInterfacesRule.
    	* Test.Rules.Design.mdp: Add rule to MD project file.
    
    
    svn path=/trunk/mono-tools/; revision=99824

---
+++ gendarme/rules/Gendarme.Rules.Design/Test/ImplementGenericCollectionInterfacesTest.cs
@@ -0,0 +1,131 @@
+//
+// Unit tests for ImplementGenericCollectionInterfacesRule
+//
+// Authors:
+//	Daniel Abramov <ex@vingrad.ru>
+//
+// Copyright (C) 2008 Daniel Abramov
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Mono.Cecil;
+
+using Gendarme.Rules.Design;
+
+using NUnit.Framework;
+using Test.Rules.Definitions;
+using Test.Rules.Fixtures;
+using Test.Rules.Helpers;
+
+namespace Test.Rules.Design {
+
+	[TestFixture]
+	public class ImplementGenericCollectionInterfacesTest : TypeRuleTestFixture<ImplementGenericCollectionInterfacesRule> {
+		
+		public class PublicIDictionary : IDictionary {
+			bool ICollection.IsSynchronized { get { throw new NotImplementedException (); } }
+			object ICollection.SyncRoot { get { throw new NotImplementedException (); } }
+			bool IDictionary.IsFixedSize { get { throw new NotImplementedException (); } }
+			bool IDictionary.IsReadOnly { get { throw new NotImplementedException (); } }
+			
+			object IDictionary.this [object key] {
+				get { throw new NotImplementedException (); }
+				set { throw new NotImplementedException (); }
+			}
+
+			void IDictionary.Add (object key, object value) { throw new NotImplementedException (); }
+			bool IDictionary.Contains (object key) { throw new NotImplementedException (); }
+			
+			ICollection IDictionary.Keys { get { throw new NotImplementedException (); } }
+			ICollection IDictionary.Values { get { throw new NotImplementedException (); } }
+			IDictionaryEnumerator IDictionary.GetEnumerator () { throw new NotImplementedException (); }
+			
+			void IDictionary.Remove (object key) { throw new NotImplementedException (); }
+			void ICollection.CopyTo (Array dest, int index) { throw new NotImplementedException (); }
+			IEnumerator IEnumerable.GetEnumerator () { throw new NotImplementedException (); }
+
+			public void Clear () { throw new NotImplementedException (); }
+			public int Count { get { throw new NotImplementedException (); } }
+		
+		}
+		
+		internal class InternalNoUseOfGenerics : IEnumerable {	
+			public IEnumerator GetEnumerator () { throw new NotImplementedException (); }			
+		}
+		
+		public class NoUseOfGenerics : IEnumerable {
+			public IEnumerator GetEnumerator () { throw new NotImplementedException (); }			
+		}
+		
+		public class GenericsAreUsed : IEnumerable<object> {
+			public IEnumerator<object> GetEnumerator () { throw new NotImplementedException (); }
+			IEnumerator IEnumerable.GetEnumerator () { throw new NotImplementedException (); }
+		}
+		
+		[Test]
+		public void NotApplicable ()
+		{
+			// the rule does not apply to:
+			// * interfaces and enums
+			AssertRuleDoesNotApply (SimpleTypes.Enum);
+			AssertRuleDoesNotApply (SimpleTypes.Interface);
+			
+			// TODO: add SimpleType for compiler-generated code and corresponding check?
+			
+			// * classes not implementing IEnumerable
+			AssertRuleDoesNotApply<ImplementGenericCollectionInterfacesTest> ();
+			// * classes implementing IDictionary
+			AssertRuleDoesNotApply<PublicIDictionary> ();
+			//  * non-public types
+			AssertRuleDoesNotApply<InternalNoUseOfGenerics> ();
+		}
+		
+		[Test]
+		public void NotApplicableBefore2_0 ()
+		{
+			// ensure that the rule does not apply for types defined in 1.x assemblies
+			TypeDefinition violator = DefinitionLoader.GetTypeDefinition<NoUseOfGenerics> ();
+			TargetRuntime realRuntime = violator.Module.Assembly.Runtime;
+
+			// fake assembly runtime version and do the check
+			violator.Module.Assembly.Runtime = TargetRuntime.NET_1_1;
+			AssertRuleDoesNotApply (violator);
+
+			// rollback
+			violator.Module.Assembly.Runtime = realRuntime;
+		}
+		
+		[Test]
+		public void GenericInterfaceImplementedReturnsSuccess ()
+		{
+			AssertRuleSuccess<GenericsAreUsed> ();
+		}
+		
+		[Test]
+		public void GenericInterfaceNotImplementedReturnsFailure ()
+		{
+			AssertRuleFailure<NoUseOfGenerics> ();
+		}
+	}
+}
