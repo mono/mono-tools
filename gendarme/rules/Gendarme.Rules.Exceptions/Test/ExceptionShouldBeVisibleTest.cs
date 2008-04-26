01ee2ab6e10b07d1ac507d44c805b8191a65ce72
Sebastien Pouliot <sebastien@ximian.com>
2008-04-26 20:18:26 +0000

    2008-04-26  Sebastien Pouliot  <sebastien@ximian.com>
    	* ExceptionShouldBeVisibleTest.cs: New. Unit tests for 
    	ExceptionShouldBeVisibleRule.
    
    
    svn path=/trunk/mono-tools/; revision=101948

---
+++ gendarme/rules/Gendarme.Rules.Exceptions/Test/ExceptionShouldBeVisibleTest.cs
@@ -0,0 +1,112 @@
+//
+// Unit tests for ExceptionShouldBeVisibleRule
+//
+// Authors:
+//	Sebastien Pouliot <sebastien@ximian.com>
+//
+// Copyright (C) 2008 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+
+using Gendarme.Rules.Exceptions;
+
+using NUnit.Framework;
+using Test.Rules.Definitions;
+using Test.Rules.Fixtures;
+
+namespace Test.Rules.Exceptions {
+
+	[TestFixture]
+	public class ExceptionShouldBeVisibleTest : TypeRuleTestFixture<ExceptionShouldBeVisibleRule> {
+
+		[Test]
+		public void NotAnException ()
+		{
+			AssertRuleDoesNotApply<ExceptionShouldBeVisibleRule> ();
+		}
+
+		internal class InternalException : Exception {
+		}
+
+		internal abstract class AbstractException : Exception {
+		}
+
+		public class MyException : Exception {
+		}
+
+		[Test]
+		public void InheritsFromException ()
+		{
+			AssertRuleFailure<InternalException> ();
+			AssertRuleSuccess<AbstractException> ();
+			AssertRuleSuccess<MyException> ();
+		}
+
+		private class PrivateSystemException : SystemException {
+		}
+
+		protected class ProtectedSystemException : SystemException {
+		}
+
+		[Test]
+		public void InheritsFromSystemException ()
+		{
+			AssertRuleFailure<PrivateSystemException> ();
+			AssertRuleSuccess<ProtectedSystemException> ();
+		}
+
+		class PrivateApplicationException : ApplicationException {
+		}
+
+		public class PublicApplicationException : ApplicationException {
+		}
+
+		[Test]
+		public void InheritsFromApplicationException ()
+		{
+			AssertRuleFailure<PrivateApplicationException> ();
+			AssertRuleSuccess<PublicApplicationException> ();
+		}
+
+		private class PrivateArgumentException : ArgumentException {
+		}
+
+		internal class InternalArgumentException : ArgumentException {
+		}
+
+		protected class ProtectedArgumentException : ArgumentException {
+		}
+
+		public class PublicArgumentException : ArgumentException {
+		}
+
+		[Test]
+		public void InheritsFromArgumentException ()
+		{
+			AssertRuleDoesNotApply<PrivateArgumentException> ();
+			AssertRuleDoesNotApply<InternalArgumentException> ();
+			AssertRuleDoesNotApply<ProtectedArgumentException> ();
+			AssertRuleDoesNotApply<PublicArgumentException> ();
+		}
+	}
+}
