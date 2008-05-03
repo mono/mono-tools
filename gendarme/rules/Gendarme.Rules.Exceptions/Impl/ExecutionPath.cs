890b399bd22889f135eda8223e96313b824609b8
Sebastien Pouliot <sebastien@ximian.com>
2008-05-03 17:51:59 +0000

    2008-05-03  Sebastien Pouliot  <sebastien@ximian.com>
    	* ExecutionPath.cs: Refactor to simplify and use generics 
    	fix ImplementGenericCollectionInterfacesRule defect on type.
    
    
    svn path=/trunk/mono-tools/; revision=102434

--- gendarme/rules/Gendarme.Rules.Exceptions/Impl/ExecutionPath.cs
+++ gendarme/rules/Gendarme.Rules.Exceptions/Impl/ExecutionPath.cs
@@ -1,75 +1,31 @@
 using System;
-using System.Collections;
-using Mono.Cecil;
+using System.Collections.Generic;
+using System.Linq;
+
 using Mono.Cecil.Cil;
 
 namespace Gendarme.Rules.Exceptions.Impl {
 
-	public class ExecutionPathCollection : CollectionBase, ICloneable {
+	public class ExecutionPathCollection : List<ExecutionBlock> {
 	
 		public ExecutionPathCollection ()
 		{
 		}
 
-		public void Add (ExecutionBlock block)
+		private ExecutionPathCollection (ExecutionPathCollection coll)
 		{
-			List.Add (block);
+			foreach (ExecutionBlock block in coll)
+				Add ((ExecutionBlock) block.Clone ());
 		}
 
 		public bool Contains (Instruction instruction)
 		{
-			foreach (ExecutionBlock block in List) {
-				if (block.Contains (instruction))
-					return true;
-			}
-
-			return false;
+			return this.Any (block => block.Contains (instruction));
 		}
-
-		public override bool Equals (object obj)
-		{
-			if (obj == null)
-				return false;
-
-			return Equals (obj as ExecutionPathCollection);
-		}
-
-		public bool Equals (ExecutionPathCollection path)
-		{
-			if (path == null)
-				return false;
-
-			if (path.Count != Count)
-				return false;
-
-			IEnumerator thisEnum = List.GetEnumerator ();
-			IEnumerator otherEnum = path.GetEnumerator ();
-			while (thisEnum.MoveNext () && otherEnum.MoveNext ()) {
-				ExecutionBlock thisBlock = (ExecutionBlock) thisEnum.Current;
-				ExecutionBlock otherBlock = (ExecutionBlock) otherEnum.Current;
-				if (thisBlock.First.Offset != otherBlock.First.Offset ||
-					thisBlock.Last.Offset != otherBlock.Last.Offset)
-					return false;
-			}
-
-			return true;
-		}
-
-		public override int GetHashCode ()
-		{
-			return List.GetHashCode ();
-		}
-		#region ICloneable Members
 
 		public object Clone ()
 		{
-			ExecutionPathCollection other = new ExecutionPathCollection ();
-			foreach (ExecutionBlock block in List)
-				other.Add((ExecutionBlock)block.Clone ());
-
-			return other;
+			return new ExecutionPathCollection (this);
 		}
-
-		#endregion
 	}
 }
