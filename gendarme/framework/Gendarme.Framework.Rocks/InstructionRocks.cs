27e93fd3407a32406b248b85c327c47808fde01b
Sebastien Pouliot <sebastien@ximian.com>
2008-06-23 23:49:48 +0000

    2008-06-23  Sebastien Pouliot  <sebastien@ximian.com>
    	* InstructionRocks.cs: Add GetParameter, IsLoadArgument and 
    	IsStoreArgument rocks. Move GetPopCount and GetPushCount from 
    	StackEntryAnalysis.
    
    
    svn path=/trunk/mono-tools/; revision=106473

--- gendarme/framework/Gendarme.Framework.Rocks/InstructionRocks.cs
+++ gendarme/framework/Gendarme.Framework.Rocks/InstructionRocks.cs
@@ -3,8 +3,10 @@
 //
 // Authors:
 //	Sebastien Pouliot  <sebastien@ximian.com>
+//	Andreas Noever <andreas.noever@gmail.com>
 //
 // Copyright (C) 2008 Novell, Inc (http://www.novell.com)
+// (C) 2008 Andreas Noever
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -112,6 +114,142 @@ namespace Gendarme.Framework.Rocks {
 		}
 
 		/// <summary>
+		/// Get the ParameterDefinition associated with the Instruction.
+		/// </summary>
+		/// <param name="self">The Instruction on which the extension method can be called.</param>
+		/// <param name="method">The method inside which the instruction comes from. 
+		/// Needed for the macro instruction where only the variable index is available.</param>
+		/// <returns>The ParameterDefinition associated with the instruction 
+		/// or null if the instruction does apply to arguments.</returns>
+		public static ParameterDefinition GetParameter (this Instruction self, MethodDefinition method)
+		{
+			if ((self == null) || (method == null))
+				return null;
+
+			int index;
+			switch (self.OpCode.Code) {
+			case Code.Ldarg_0:
+			case Code.Ldarg_1:
+			case Code.Ldarg_2:
+			case Code.Ldarg_3:
+				index = self.OpCode.Code - Code.Ldarg_0;
+				if (!method.IsStatic) {
+					index--;
+					if (index < 0)
+						return null;
+				}
+				return method.Parameters [index];
+			case Code.Ldarg:
+			case Code.Ldarg_S:
+			case Code.Ldarga:
+			case Code.Ldarga_S:
+			case Code.Starg:
+			case Code.Starg_S:
+				return (self.Operand as ParameterDefinition);
+			default:
+				return null;
+			}
+		}
+
+		/// <summary>
+		/// Get the number of values removed on the stack for this instruction.
+		/// </summary>
+		/// <param name="self">The Instruction on which the extension method can be called.</param>
+		/// <param name="method">The method inside which the instruction comes from 
+		/// (needed for StackBehaviour.Varpop).
+		/// <returns>The number of value removed (pop) from the stack for this instruction.</returns>
+		public static int GetPopCount (this Instruction self, MethodDefinition method)
+		{
+			switch (self.OpCode.StackBehaviourPop) {
+			case StackBehaviour.Pop0:
+				return 0;
+
+			case StackBehaviour.Pop1:
+			case StackBehaviour.Popi:
+			case StackBehaviour.Popref:
+				return 1;
+
+			case StackBehaviour.Pop1_pop1:
+			case StackBehaviour.Popi_pop1:
+			case StackBehaviour.Popi_popi8:
+			case StackBehaviour.Popi_popr4:
+			case StackBehaviour.Popi_popr8:
+			case StackBehaviour.Popref_pop1:
+			case StackBehaviour.Popref_popi:
+			case StackBehaviour.Popi_popi:
+				return 2;
+
+			case StackBehaviour.Popi_popi_popi:
+			case StackBehaviour.Popref_popi_popi:
+			case StackBehaviour.Popref_popi_popi8:
+			case StackBehaviour.Popref_popi_popr4:
+			case StackBehaviour.Popref_popi_popr8:
+			case StackBehaviour.Popref_popi_popref:
+				return 3;
+
+			case StackBehaviour.Varpop:
+				switch (self.OpCode.FlowControl) {
+				case FlowControl.Return:
+					return method.ReturnType.ReturnType.FullName == "System.Void" ? 0 : 1;
+
+				case FlowControl.Call:
+					IMethodSignature calledMethod = (IMethodSignature) self.Operand;
+					int n = calledMethod.Parameters.Count;
+					if (self.OpCode.Code != Code.Newobj) {
+						if (calledMethod.HasThis)
+							n++;
+					}
+					return n;
+
+				default:
+					throw new NotImplementedException ("Varpop not supported for this Instruction.");
+				}
+
+			case StackBehaviour.PopAll:
+				throw new NotImplementedException ("PopAll not supported for this Instruction.");
+			default:
+				string unknown = String.Format ("'{0}' is not a valid value for instruction '{1}'.",
+					self.OpCode.StackBehaviourPush, self.OpCode);
+				throw new InvalidOperationException (unknown);
+			}
+		}
+
+		/// <summary>
+		/// Get the number of values placed on the stack by this instruction.
+		/// </summary>
+		/// <param name="self">The Instruction on which the extension method can be called.</param>
+		/// <returns>The number of value added (push) to the stack by this instruction.</returns>
+		public static int GetPushCount (this Instruction self)
+		{
+			switch (self.OpCode.StackBehaviourPush) {
+			case StackBehaviour.Push0:
+				return 0;
+
+			case StackBehaviour.Push1:
+			case StackBehaviour.Pushi:
+			case StackBehaviour.Pushi8:
+			case StackBehaviour.Pushr4:
+			case StackBehaviour.Pushr8:
+			case StackBehaviour.Pushref:
+				return 1;
+
+			case StackBehaviour.Push1_push1:
+				return 2;
+
+			case StackBehaviour.Varpush:
+				IMethodSignature calledMethod = (IMethodSignature) self.Operand;
+				if (calledMethod != null)
+					return (calledMethod.ReturnType.ReturnType.FullName == "System.Void") ? 0 : 1;
+
+				throw new NotImplementedException ("Varpush not supported for this Instruction.");
+			default:
+				string unknown = String.Format ("'{0}' is not a valid value for instruction '{1}'.",
+					self.OpCode.StackBehaviourPush, self.OpCode);
+				throw new InvalidOperationException (unknown);
+			}
+		}
+
+		/// <summary>
 		/// Get the VariableDefinition associated with the Instruction.
 		/// </summary>
 		/// <param name="self">The Instruction on which the extension method can be called.</param>
@@ -151,6 +289,31 @@ namespace Gendarme.Framework.Rocks {
 		}
 
 		/// <summary>
+		/// Return if the Instruction is a load of an argument (ldarg* family).
+		/// </summary>
+		/// <param name="self">The Instruction on which the extension method can be called.</param>
+		/// <returns>True if the instruction is a load argument/parameter, False otherwise</returns>
+		public static bool IsLoadArgument (this Instruction self)
+		{
+			if (self == null)
+				return false;
+
+			switch (self.OpCode.Code) {
+			case Code.Ldarg_0:
+			case Code.Ldarg_1:
+			case Code.Ldarg_2:
+			case Code.Ldarg_3:
+			case Code.Ldarg:
+			case Code.Ldarg_S:
+			case Code.Ldarga:
+			case Code.Ldarga_S:
+				return true;
+			default:
+				return false;
+			}
+		}
+
+		/// <summary>
 		/// Return if the Instruction is a load of a local variable (ldloc* family).
 		/// </summary>
 		/// <param name="self">The Instruction on which the extension method can be called.</param>
@@ -169,6 +332,25 @@ namespace Gendarme.Framework.Rocks {
 			case Code.Ldloc_S:
 			case Code.Ldloca:
 			case Code.Ldloca_S:
+				return true;
+			default:
+				return false;
+			}
+		}
+
+		/// <summary>
+		/// Return if the Instruction is a store of an argument (starg* family).
+		/// </summary>
+		/// <param name="self">The Instruction on which the extension method can be called.</param>
+		/// <returns>True if the instruction is a store of a parameter, False otherwise</returns>
+		public static bool IsStoreArgument (this Instruction self)
+		{
+			if (self == null)
+				return false;
+
+			switch (self.OpCode.Code) {
+			case Code.Starg:
+			case Code.Starg_S:
 				return true;
 			default:
 				return false;
