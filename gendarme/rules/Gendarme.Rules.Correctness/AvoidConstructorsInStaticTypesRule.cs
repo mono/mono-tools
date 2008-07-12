3ea9f270d7246dc3b073f218999b4c6a99207c05
Sebastien Pouliot <sebastien@ximian.com>
2008-07-12 21:54:48 +0000

    2008-07-12  Sebastien Pouliot  <sebastien@ximian.com>
    	* AvoidConstructorsInStaticTypesRule.cs: Rework the rule to
    	be more specific to 1.x code - where we want to the possibility 
    	to instantiate "static" types by avoiding a visible default ctor.
    	* Makefile.am: Fix typo that limited the number of tests being 
    	built and executed.
    
    
    svn path=/trunk/mono-tools/; revision=107800

--- gendarme/rules/Gendarme.Rules.Correctness/AvoidConstructorsInStaticTypesRule.cs
+++ gendarme/rules/Gendarme.Rules.Correctness/AvoidConstructorsInStaticTypesRule.cs
@@ -28,46 +28,64 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
-using System;
-
 using Mono.Cecil;
-using Mono.Cecil.Cil;
+
 using Gendarme.Framework;
 using Gendarme.Framework.Rocks;
 
 namespace Gendarme.Rules.Correctness {
 
-	[Problem ("This type contains only static fields and methods and have a non static constructor.")]
-	[Solution ("You should use an static constructor or make the fields and methods non-static.")]
+	// note: this rule is similar to Gendarme.Rules.Design.ConsiderUsingStaticTypeRule
+	// but is also applicable to the 1.x runtime (hidding the default ctor was still 
+	// the correct way of handling this back then). The goal is to make this ctor 
+	// non-visible so people can't instantiate it (e.g. by mistake)
+
+	[Problem ("This type (and ancestors) contains only static fields and methods but has a visible, non static, constructor.")]
+	[Solution ("You should remove the visible constructor to make sure this type cannot be instancied, or if using 2.0+, change it to a static type.")]
 	public class AvoidConstructorsInStaticTypesRule : Rule, ITypeRule {
+
+		static bool IsAllStatic (TypeDefinition type)
+		{
+			foreach (MethodDefinition ctor in type.Constructors) {
+				// let's the default ctor pass (since it's always here for 1.x code)
+				if (!ctor.IsStatic && (ctor.Parameters.Count > 1))
+					return false;
+			}
+
+			foreach (MethodDefinition method in type.Methods) {
+				if (!method.IsStatic)
+					return false;
+			}
+
+			foreach (FieldDefinition field in type.Fields) {
+				if (!field.IsStatic)
+					return false;
+			}
+
+			if (type.BaseType.FullName == "System.Object")
+				return true;
+
+			return IsAllStatic (type.BaseType.Resolve ());
+		}
 
 		public RuleResult CheckType (TypeDefinition type)
 		{
-			// rule applies only if the type as method or fields
-			if (type.Methods.Count == 0 && type.Fields.Count == 0)
+			// rule applies only if the type isn't: an enum, an interface, a struct, a delegate or compiler generated
+			if (type.IsEnum || type.IsInterface || type.IsValueType || type.IsDelegate () || type.IsGeneratedCode ())
 				return RuleResult.DoesNotApply;
 
-			// rule applies only if all methods are static
-			foreach (MethodDefinition method in type.Methods) {
-				if (!method.IsStatic)
-					return RuleResult.DoesNotApply;
-			}
-
-			// rule applies only if all fields are static
-			foreach (FieldDefinition field in type.Fields) {
-				if (!field.IsStatic)
-					return RuleResult.DoesNotApply;
-			}
-
-			// rule applies only if the type isn't compiler generated
-			if (type.IsGeneratedCode ())
+			// it also does not apply if the type is static (2.0+ only)
+			if (type.IsStatic ())
 				return RuleResult.DoesNotApply;
+
+			if (!IsAllStatic (type))
+				return RuleResult.Success;
 
 			// rule applies!
 
 			foreach (MethodDefinition ctor in type.Constructors) {
-				if (!ctor.IsStatic && ctor.IsPublic) {
-					Runner.Report (ctor, Severity.Low, Confidence.High, String.Empty);
+				if (!ctor.IsStatic && ctor.IsVisible ()) {
+					Runner.Report (ctor, Severity.Low, Confidence.High);
 				}
 			}
 
@@ -75,4 +93,3 @@ namespace Gendarme.Rules.Correctness {
 		}
 	}
 }
-
