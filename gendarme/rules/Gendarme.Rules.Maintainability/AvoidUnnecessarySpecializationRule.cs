b013604b7b675f3d4f6ffd85eaeb4ec7f534cddd
Sebastien Pouliot <sebastien@ximian.com>
2008-07-13 23:42:41 +0000

    2008-07-13  Sebastien Pouliot  <sebastien@ximian.com>
    	* AvoidUnnecessarySpecializationRule.cs: New. Rule to check if some
    	method parameters could use a type lower in the hierarchy than the one 
    	being used. [committed for Cedric Vivier]
    	* Gendarme.Rules.Maintainability.mdp: Update MD project files.
    	* Makefile.am:  Add the rule and the test to the build.
    
    
    svn path=/trunk/mono-tools/; revision=107837

---
+++ gendarme/rules/Gendarme.Rules.Maintainability/AvoidUnnecessarySpecializationRule.cs
@@ -0,0 +1,340 @@
+//
+// Gendarme.Rules.Maintainability.AvoidUnnecessarySpecializationRule class
+//
+// Authors:
+//	Cedric Vivier <cedricv@neonux.com>
+//
+// Copyright (C) 2008 Cedric Vivier
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+using Gendarme.Framework;
+using Gendarme.Framework.Rocks;
+using Gendarme.Framework.Helpers;
+
+namespace Gendarme.Rules.Maintainability {
+
+	[Problem ("This method has a parameter whose type is more specialized than necessary. It can be harder to reuse and/or extend the method in derived types.")]
+	[Solution ("Replace parameter type with the least specialized type necessary, or make use of the specifics of the actual parameter type.")]
+	public class AvoidUnnecessarySpecializationRule : Rule, IMethodRule {
+
+		private StackEntryAnalysis sea;
+		private TypeReference[] leastTypes;
+		private int[] leastDepths;
+
+		private static ParameterDefinition GetParameter (MethodDefinition method, Instruction ins)
+		{
+			switch (ins.OpCode.Code) {
+			case Code.Ldarg_0:
+			case Code.Ldarg_1:
+			case Code.Ldarg_2:
+			case Code.Ldarg_3:
+				int index = ins.OpCode.Code - Code.Ldarg_0;
+				if (!method.IsStatic) {
+					index--;
+					if (index < 0) return null;
+				}
+				return method.Parameters [index];
+			case Code.Ldarg_S :
+			case Code.Ldarga_S :
+			case Code.Ldarg:
+			case Code.Ldarga:
+				return (ins.Operand as ParameterDefinition);
+			}
+
+			throw new ArgumentException("not a ldarg instruction", "ins");
+		}
+
+		private static TypeReference GetActualType (TypeReference type)
+		{
+			GenericParameter gp = (type as GenericParameter);
+			if ((gp != null) && (gp.Constraints.Count == 1))
+				type = gp.Constraints [0];
+			return type;
+		}
+
+		private static int GetActualTypeDepth (TypeReference type)
+		{
+			return GetTypeDepth (GetActualType (type));
+		}
+
+		private static int GetTypeDepth (TypeReference type)
+		{
+			return GetTypeDepth (GetActualType (type).Resolve ());
+		}
+
+		private static int GetTypeDepth (TypeDefinition type)
+		{
+			int depth = 0;
+			while (null != type && type.BaseType != null) {
+				depth++;
+				type = type.BaseType.Resolve ();
+			}
+			return depth;
+		}
+
+		private static TypeDefinition GetBaseImplementor (TypeReference type, MethodSignature signature)
+		{
+			return GetBaseImplementor (type.Resolve(), signature);
+		}
+
+		private static TypeDefinition GetBaseImplementor (TypeDefinition type, MethodSignature signature)
+		{
+			TypeDefinition implementor = type;
+
+			while (null != type && type.IsVisible ()) {
+				//search for matching interface
+				TypeDefinition ifaceDef = GetInterfaceImplementor (type, signature);
+				if (null != ifaceDef)
+					implementor = ifaceDef;
+				else if (null != type.GetMethod (signature))
+					implementor = type;
+				type = type.BaseType.Resolve ();
+			}
+
+			return implementor;
+		}
+
+		private static TypeDefinition GetInterfaceImplementor (TypeDefinition type, MethodSignature signature)
+		{
+			TypeDefinition ifaceDef = null;
+
+			foreach (TypeReference iface in type.Interfaces) {
+				TypeDefinition candidate = iface.Resolve ();
+
+				if (!candidate.IsVisible () || candidate.Name.StartsWith("_"))
+					continue; //ignore non-cls-compliant interfaces
+				if (null == candidate.GetMethod (signature))
+					continue; //does not implement
+
+				if (null == ifaceDef) {
+					ifaceDef = candidate;
+				} else if (ifaceDef.GenericParameters.Count < candidate.GenericParameters.Count) {
+					//prefer the most generic interface
+					ifaceDef = candidate;
+				} else if (ifaceDef.Interfaces.Count < candidate.Interfaces.Count) {
+					//prefer the most specific interface
+					ifaceDef = candidate;
+				} else if (ifaceDef.Interfaces.Count >= candidate.Interfaces.Count
+					|| ifaceDef.GenericParameters.Count >= candidate.GenericParameters.Count) {
+					continue; //we already have a better match
+				} else {
+					//ambiguous/explicit, ignore
+					ifaceDef = null;
+					break;
+				}
+			}
+
+			return ifaceDef;
+		}
+
+		private static MethodSignature GetSignature (MethodReference method)
+		{
+			string[] parameters = new string [method.Parameters.Count];
+			for (int i = 0; i < method.Parameters.Count; ++i) {
+				TypeReference pType = method.Parameters [i].ParameterType;
+				if (pType is GenericParameter)
+					parameters [i] = null; //TODO: constructed mapping?
+				else
+					parameters [i] = pType.FullName;
+			}
+			return new MethodSignature (method.Name, GetReturnTypeSignature (method), parameters);
+		}
+
+		private static string GetReturnTypeSignature (MethodReference method)
+		{
+			if (method.Name == "GetEnumerator" && 0 == method.Parameters.Count) //special exception
+				return null;
+			return method.ReturnType.ReturnType.FullName;
+		}
+
+		private static bool IsSystemObjectMethod (MethodReference method)
+		{
+			switch (method.Name) {
+			case "Finalize" :
+			case "GetHashCode" :
+			case "GetType" :
+			case "MemberwiseClone" :
+			case "ToString" :
+				return method.Parameters.Count == 0;
+			case "Equals" :
+				return method.Parameters.Count == 1 || method.Parameters.Count == 2;
+			case "ReferenceEquals" :
+				return method.Parameters.Count == 2;
+
+			//HACK: BOO:
+			case "EqualityOperator" :
+				return method.Parameters.Count == 2 && method.DeclaringType.FullName == "Boo.Lang.Runtime.RuntimeServices";
+			}
+			return false;
+		}
+
+		private void UpdateParameterLeastType (ParameterDefinition parameter, StackEntryUsageResult[] usageResults)
+		{
+			int pIndex = parameter.Sequence - 1;
+			if (pIndex < 0) throw new InvalidOperationException("parameter.Sequence < 1");
+			int parameterDepth = GetActualTypeDepth (parameter.ParameterType);
+
+			TypeReference currentLeastType = null;
+			int currentLeastDepth = 0;
+
+			foreach (var usage in usageResults) {
+				bool needUpdate = false;
+
+				switch (usage.Instruction.OpCode.Code) {
+				case Code.Newobj :
+				case Code.Call :
+				case Code.Callvirt :
+					MethodReference method = (MethodReference) usage.Instruction.Operand;
+
+					//potential generalization to object does not really make sense
+					//from a readability/maintainability point of view
+					if (IsSystemObjectMethod (method)) continue;
+
+					MethodSignature signature = GetSignature (method);
+
+					if (usage.StackOffset == method.Parameters.Count) {
+						//argument is used as `this` in the call
+						currentLeastType = GetBaseImplementor (GetActualType (method.DeclaringType), signature);
+					} else {
+						//argument is also used as an argument in the call
+						currentLeastType = method.Parameters [method.Parameters.Count - usage.StackOffset - 1].ParameterType;
+					}
+
+					//if the best we could find is object, ignore this round
+					if (currentLeastType.FullName == "System.Object") continue;
+
+					needUpdate = true;
+					break;
+
+				case Code.Stfld :
+				case Code.Stsfld :
+					FieldReference field = (FieldReference) usage.Instruction.Operand;
+					currentLeastType = field.FieldType;
+
+					needUpdate = true;
+					break;
+				}
+
+				if (needUpdate) {
+					currentLeastDepth = GetActualTypeDepth (currentLeastType);
+					if (null == leastTypes [pIndex] || currentLeastDepth > leastDepths [pIndex]) {
+						leastTypes [pIndex] = currentLeastType;
+						leastDepths [pIndex] = currentLeastDepth;
+					}
+					if (currentLeastDepth == parameterDepth) //no need to check further
+						return;
+				}
+			}
+		}
+
+		private void CheckParameter (MethodDefinition method, Instruction ins)
+		{
+			ParameterDefinition parameter = GetParameter (method, ins);
+			if (null == parameter) //this is `this`, we do not care
+				return;
+			if (parameter.IsOut || parameter.IsOptional || parameter.ParameterType.IsValueType)
+				return;
+			if (parameter.ParameterType.IsArray () || parameter.ParameterType.IsDelegate ())
+				return; //TODO: these are more complex to handle, not supported for now
+
+			if (null == sea || sea.Method != method)
+				sea = new StackEntryAnalysis (method);
+
+			StackEntryUsageResult [] usage = sea.GetStackEntryUsage (ins);
+
+			//FIXME: below opt. would require a non-repeating SEA with multiple ldarg
+			//if (null != leastTypes [parameter.Sequence-1]) //already analyzed, next!
+			//	return;
+
+			UpdateParameterLeastType (parameter, usage);
+		}
+
+		public RuleResult CheckMethod (MethodDefinition method)
+		{
+			if (!method.HasBody || method.IsGeneratedCode () || method.IsCompilerControlled)
+				return RuleResult.DoesNotApply;
+			if (method.Parameters.Count == 0 || method.IsProperty ())
+				return RuleResult.DoesNotApply;
+
+			leastTypes = new TypeReference [method.Parameters.Count];
+			leastDepths = new int [leastTypes.Length];
+
+			//look at each argument usage
+			foreach (Instruction ins in method.Body.Instructions) {
+				switch (ins.OpCode.Code) {
+				case Code.Ldarg_0 :
+				case Code.Ldarg_1 :
+				case Code.Ldarg_2 :
+				case Code.Ldarg_3 :
+				case Code.Ldarg_S :
+				case Code.Ldarga_S :
+				case Code.Ldarg :
+				case Code.Ldarga :
+					CheckParameter (method, ins);
+					break;
+				default:
+					continue;
+				}
+			}
+
+			CheckParametersSpecializationDelta (method);
+
+			return Runner.CurrentRuleResult;
+		}
+
+		private void CheckParametersSpecializationDelta (MethodDefinition method)
+		{
+			for (int i = 0; i < method.Parameters.Count; ++i) {
+				if (null == leastTypes [i]) continue; //argument is not used
+
+				ParameterDefinition parameter = method.Parameters [i];
+				int delta = GetActualTypeDepth (parameter.ParameterType) - leastDepths [i];
+
+				if (delta > 0) {
+					string message = GetSuggestionMessage (parameter);
+					Severity sev = (delta < 3) ? Severity.Medium : Severity.High;
+					Runner.Report (method, sev, Confidence.High, message);
+				}
+			}
+		}
+
+		private string GetSuggestionMessage (ParameterDefinition parameter)
+		{
+			System.Text.StringBuilder sb = new System.Text.StringBuilder ();
+			sb.Append ("Parameter '");
+			sb.Append (parameter.Name);
+			if (parameter.ParameterType is GenericParameter)
+				sb.Append ("' could be constrained to type '");
+			else
+				sb.Append ("' could be of type '");
+			sb.Append (leastTypes [parameter.Sequence - 1].FullName);
+			sb.Append ("'.");
+			return sb.ToString ();
+		}
+
+	}
+
+}
+
