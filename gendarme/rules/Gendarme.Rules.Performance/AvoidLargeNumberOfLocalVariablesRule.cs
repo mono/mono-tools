2c604bfa65f4f5ee6a072629531b19d0e2369043
Sebastien Pouliot <sebastien@ximian.com>
2008-07-13 16:42:48 +0000

    2008-07-13  Sebastien Pouliot  <sebastien@ximian.com>
    	* AvoidLargeNumberOfLocalVariablesRule.cs: Rule to warn if a method
    	has a very large of local variables - since it's likely the JIT won't be
    	able to do a good job assigning registers to them.
    	* Makefile.am: add rule and tests to the build.
    
    
    svn path=/trunk/mono-tools/; revision=107827

---
+++ gendarme/rules/Gendarme.Rules.Performance/AvoidLargeNumberOfLocalVariablesRule.cs
@@ -0,0 +1,71 @@
+//
+// Gendarme.Rules.Performance.AvoidLargeNumberOfLocalVariablesRule
+//
+// Authors:
+//	Sebastien Pouliot <sebastien@ximian.com>
+//
+// Copyright (C) 2008 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+using System;
+
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+using Gendarme.Framework;
+using Gendarme.Framework.Rocks;
+
+namespace Gendarme.Rules.Performance {
+
+	[Problem ("The number of local variables is too large to allow the JIT to properly allocate registers.")]
+	[Solution ("Refactor your code to reduce the number of variables or split the variables into several methods.")]
+	public class AvoidLargeNumberOfLocalVariablesRule : Rule, IMethodRule {
+
+		private int max_variables = 64;
+
+		public int MaximumVariables {
+			get { return max_variables; }
+			set { max_variables = value; }
+		}
+
+		public RuleResult CheckMethod (MethodDefinition method)
+		{
+			// ingore methods without body and generated code
+			if (!method.HasBody || method.IsGeneratedCode ())
+				return RuleResult.DoesNotApply;
+
+			// special case for System.Windows.Forms since it's designer does not
+			// mark this code as generated :-(
+			if (method.Name == "InitializeComponent") {
+				if (method.DeclaringType.Inherits ("System.Windows.Forms.Form"))
+					return RuleResult.DoesNotApply;
+			}
+
+			int num = method.Body.Variables.Count;
+			if (num <= MaximumVariables)
+				return RuleResult.Success;
+
+			string msg = String.Format ("Found {0} local variables (maximum {1}).", num, MaximumVariables);
+			Runner.Report (method, Severity.High, Confidence.High, msg);
+			return RuleResult.Failure;
+		}
+	}
+}
