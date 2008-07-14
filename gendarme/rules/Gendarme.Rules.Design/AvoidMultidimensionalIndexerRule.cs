5fa752f2eae3d3b6ff4ee75e52860ff44a7b0587
Sebastien Pouliot <sebastien@ximian.com>
2008-07-14 04:27:24 +0000

    2008-07-13  Sebastien Pouliot  <sebastien@ximian.com>
    	* AvoidMultidimensionalIndexerRule.cs: New. Rule that warns if 
    	indexers using multidimentional indexes are found.
    	* PreferIntegerOrStringForIndexersRule.cs: New. Rule that warns 
    	if indexers uses something other that string, integers or object as 
    	their index.
    	* Gendarme.Rules.Design.mdp: Add rule to MD project file.
    	* Makefile.am: Add rule and test to the build.
    
    
    svn path=/trunk/mono-tools/; revision=107853

---
+++ gendarme/rules/Gendarme.Rules.Design/AvoidMultidimensionalIndexerRule.cs
@@ -0,0 +1,56 @@
+//
+// Gendarme.Rules.Design.AvoidMultidimensionalIndexerRule
+//
+// Authors:
+//	Sebastien Pouliot <sebastien@ximian.com>
+//
+// Copyright (C) 2008 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+
+using Mono.Cecil;
+
+using Gendarme.Framework;
+using Gendarme.Framework.Rocks;
+
+namespace Gendarme.Rules.Design {
+
+	[Problem ("This indexer use multiple indexes and this limit its usability.")]
+	[Solution ("Convert this indexer into a method.")]
+	public class AvoidMultidimensionalIndexerRule : Rule, IMethodRule {
+
+		public RuleResult CheckMethod (MethodDefinition method)
+		{
+			// rule only applies to indexers
+			if (method.Name != "get_Item")
+				return RuleResult.DoesNotApply;
+
+			// if there is a single argument or if the method is not visible outside the assembly
+			if ((method.Parameters.Count == 1) || !method.IsVisible ())
+				return RuleResult.Success;
+
+			Runner.Report (method, Severity.Medium, Confidence.Total);
+			return RuleResult.Failure;
+		}
+	}
+}
---
+++ gendarme/rules/Gendarme.Rules.Design/PreferIntegerOrStringForIndexersRule.cs
@@ -0,0 +1,67 @@
+//
+// Gendarme.Rules.Design.AvoidMultidimensionalIndexerRule
+//
+// Authors:
+//	Sebastien Pouliot <sebastien@ximian.com>
+//
+// Copyright (C) 2008 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+
+using Mono.Cecil;
+
+using Gendarme.Framework;
+using Gendarme.Framework.Rocks;
+
+namespace Gendarme.Rules.Design {
+
+	[Problem ("This indexer should be using integers or strings for its indexes.")]
+	[Solution ("Convert this indexer into a method if an integer or a string cannot be used.")]
+	public class PreferIntegerOrStringForIndexersRule : Rule, IMethodRule {
+
+		public RuleResult CheckMethod (MethodDefinition method)
+		{
+			// rule only applies to indexers
+			if (method.Name != "get_Item")
+				return RuleResult.DoesNotApply;
+
+			// ok if the method is not visible outside the assembly
+			if (!method.IsVisible ())
+				return RuleResult.Success;
+
+			foreach (ParameterDefinition parameter in method.Parameters) {
+				switch (parameter.ParameterType.FullName) {
+				case "System.Int32":
+				case "System.Int64":
+				case "System.String":
+				case "System.Object": // tolarable in some circumstances
+					break;
+				default:
+					Runner.Report (parameter, Severity.Medium, Confidence.Total);
+					break;
+				}
+			}
+			return Runner.CurrentRuleResult;
+		}
+	}
+}
