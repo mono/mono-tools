ea981070139e2fb2cdb2f59726f3d59b202f5200
Sebastien Pouliot <sebastien@ximian.com>
2008-08-03 16:28:55 +0000

    2008-08-03  Sebastien Pouliot  <sebastien@ximian.com>
    	* ConsiderUsingStopwatchRule.cs: Use new rocks to simplify code.
    	Rework parameter checks to consider Ref and Out parameters.
    
    
    svn path=/trunk/mono-tools/; revision=109517

--- gendarme/rules/Gendarme.Rules.Maintainability/ConsiderUsingStopwatchRule.cs
+++ gendarme/rules/Gendarme.Rules.Maintainability/ConsiderUsingStopwatchRule.cs
@@ -1,10 +1,12 @@
 //
-// Gendarme.Rules.Design.ConsiderUsingStopwatch
+// Gendarme.Rules.Maintainability.ConsiderUsingStopwatchRule
 //
 // Authors:
-//  Cedric Vivier <cedricv@neonux.com>
+//	Cedric Vivier <cedricv@neonux.com>
+//	Sebastien Pouliot  <sebastien@ximian.com>
 //
 // Copyright (C) 2008 Cedric Vivier
+// Copyright (C) 2008 Novell, Inc (http://www.novell.com)
 //
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
@@ -43,7 +45,6 @@ namespace Gendarme.Rules.Maintainability {
 
 		private const string DateTime = "System.DateTime";
 		private const string GetNow = "get_Now";
-		private const string Subtraction = "op_Subtraction";
 
 		public override void Initialize (IRunner runner)
 		{
@@ -60,80 +61,56 @@ namespace Gendarme.Rules.Maintainability {
 			};
 		}
 
-		private static bool AreMirrorInstructions(Instruction ld, Instruction st)
+		private static bool AreMirrorInstructions (Instruction ld, Instruction st, MethodDefinition method)
 		{
-			int ldIndex = -1;
-			switch (ld.OpCode.Code) {
-				case Code.Ldloc_0 :
-					ldIndex = 0;
-					break;
-				case Code.Ldloc_1 :
-					ldIndex = 1;
-					break;
-				case Code.Ldloc_2 :
-					ldIndex = 2;
-					break;
-				case Code.Ldloc_3 :
-					ldIndex = 3;
-					break;
-				case Code.Ldloc_S :
-				case Code.Ldloc :
-					ldIndex = ((VariableDefinition) ld.Operand).Index;
-					break;
-			}
-
-			int stIndex = -1;
-			switch (st.OpCode.Code) {
-				case Code.Stloc_0 :
-					stIndex = 0;
-					break;
-				case Code.Stloc_1 :
-					stIndex = 1;
-					break;
-				case Code.Stloc_2 :
-					stIndex = 2;
-					break;
-				case Code.Stloc_3 :
-					stIndex = 3;
-					break;
-				case Code.Stloc_S :
-				case Code.Stloc :
-					stIndex = ((VariableDefinition) st.Operand).Index;
-					break;
-			}
-
-			return ldIndex == stIndex;
+			return (ld.GetVariable (method).Index == st.GetVariable (method).Index);
 		}
 
 		private static bool IsGetNow (MethodDefinition method, Instruction ins)
 		{
-			switch (ins.OpCode.Code) {
-			case Code.Call :
-				MethodReference calledMethod = (MethodReference) ins.Operand;
-				return calledMethod.DeclaringType.FullName == DateTime && calledMethod.Name == GetNow;
+			if (ins.OpCode.Code != Code.Call)
+				return false;
 
-			case Code.Ldloc_0 :
-			case Code.Ldloc_1 :
-			case Code.Ldloc_2 :
-			case Code.Ldloc_3 :
-			case Code.Ldloc_S :
-			case Code.Ldloc :
-				Instruction prev = ins.Previous;
-				while (null != prev)
-				{
-					if (AreMirrorInstructions(ins, prev))
+			MethodReference calledMethod = (MethodReference) ins.Operand;
+			return calledMethod.DeclaringType.FullName == DateTime && calledMethod.Name == GetNow;
+		}
+		
+		private static bool CheckParameters (MethodDefinition method, Instruction ins)
+		{
+			Instruction prev;
+			if (ins.IsLoadLocal ()) {
+				prev = ins.Previous;
+				while (null != prev) {
+					// look for a STLOC* instruction and compare the variable indexes
+					if (prev.IsStoreLocal () && AreMirrorInstructions (ins, prev, method))
 						return IsGetNow (method, prev.Previous);
 					prev = prev.Previous;
 				}
-				break;
+			} else if (ins.OpCode.Code == Code.Ldobj) {
+				prev = ins.Previous;
+				int arg = prev.GetParameter (method).Sequence;
+				prev = prev.Previous;
+				while (null != prev) {
+					// look for a STOBJ instruction and compare the objects
+					if (prev.OpCode.Code == Code.Stobj) {
+						prev = prev.Previous.Previous;
+						int arg2 = prev.GetParameter (method).Sequence;
+						return (arg == arg2);
+					}
+					prev = prev.Previous;
+				}
+			} else {
+				return IsGetNow (method, ins);
 			}
-
 			return false;
 		}
 
 		private static bool CheckUsage (MethodDefinition method, Instruction ins)
 		{
-			return IsGetNow (method, ins.Previous.Previous) && IsGetNow (method, ins.Previous);
+			// track the two parameters given to DateTime.op_Substraction
+			Instruction param1 = ins.TraceBack (method, -1);
+			Instruction param2 = ins.TraceBack (method);
+			return CheckParameters (method, param1) && CheckParameters (method, param2);
 		}
 
 		public RuleResult CheckMethod (MethodDefinition method)
@@ -142,17 +119,20 @@ namespace Gendarme.Rules.Maintainability {
 				return RuleResult.DoesNotApply;
 
 			foreach (Instruction ins in method.Body.Instructions) {
-				if (ins.OpCode.Code == Code.Call) {
-					MethodReference calledMethod = (MethodReference) ins.Operand;
-					if (calledMethod.DeclaringType.FullName == DateTime && calledMethod.Name == Subtraction)
-						if (CheckUsage (method, ins))
-							Runner.Report (method, ins, Severity.Low, Confidence.High);
-				}
+				if (ins.OpCode.Code != Code.Call)
+					continue;
+
+				MethodReference calledMethod = (MethodReference) ins.Operand;
+				if (calledMethod.DeclaringType.FullName != DateTime)
+					continue;
+				if (!MethodSignatures.op_Subtraction.Matches (calledMethod))
+					continue;
+
+				if (CheckUsage (method, ins))
+					Runner.Report (method, ins, Severity.Low, Confidence.High);
 			}
 
 			return Runner.CurrentRuleResult;
 		}
 	}
-
 }
-
