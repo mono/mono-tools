f3e008cd6a53084fcd96370e826fa13d40d593c7
Massimiliano Mantione <massi@mono-cvs.ximian.com>
2008-08-12 12:44:20 +0000

    More mono-tools integration relate work...
    svn path=/trunk/mono-tools/; revision=110225

--- Mono.Profiler/adjust-makefiles.sh
+++ Mono.Profiler/adjust-makefiles.sh
@@ -7,4 +7,4 @@ for F in ./*/Makefile.am; do
 sed -i -e 's/include $(top_srcdir)\/Makefile.include/include $(top_srcdir)\/Mono.Profiler\/Makefile.include/' $F;
 sed -i -e 's/GTK_SHARP_20_LIBS/GTK_SHARP_LIBS/' $F;
 done
-rm autogen.sh configure.ac;
+rm autogen.sh configure.ac expansions.m4;
