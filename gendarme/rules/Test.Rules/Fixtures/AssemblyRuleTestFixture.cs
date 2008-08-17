1a7821fdeab715d7b7dee49a9d7ed537ecb58e16
Néstor Salceda <nestor@mono-cvs.ximian.com>
2008-08-17 16:39:19 +0000

    2008-08-17  Néstor Salceda  <nestor.salceda@gmail.com>
    	* AssemblyRuleTestFixture.cs: New.  A test fixture for checking
    	assembly rules.
    
    
    svn path=/trunk/mono-tools/; revision=110684

---
+++ gendarme/rules/Test.Rules/Fixtures/AssemblyRuleTestFixture.cs
@@ -0,0 +1,38 @@
+//
+// Test.Rules.Fixtures.AssemblyRuleTestFixture<T>
+// Base class for assembly rule test fixtures which simplifies writting unit
+// tests for Gendarme.
+//
+// Authors:
+//	Néstor Salceda <nestor.salceda@gmail.com>
+//	Based on a previous work of Daniel Abramov
+//
+// 	(C) 2008 Néstor Salceda
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using Gendarme.Framework;
+using Mono.Cecil;
+
+namespace Test.Rules.Fixtures {
+	public abstract class AssemblyRuleTestFixture<TAssemblyRule> : RuleTestFixture<TAssemblyRule, AssemblyDefinition> where TAssemblyRule : IAssemblyRule, new () {
+	}
+}
