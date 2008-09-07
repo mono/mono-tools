832ef64f02dfe5d412ecfa83c6b424800c21c8fe
Sebastien Pouliot <sebastien@ximian.com>
2008-09-07 16:56:17 +0000

    2008-09-07  Sebastien Pouliot  <sebastien@ximian.com>
    	* AvoidMethodWithUnusedGenericTypeRule.cs: New. This rule warns 
    	if some generic parameters types are not being used by the method 
    	parameters.
    	* Makefile.am: Add new files to the build.
    
    
    svn path=/trunk/mono-tools/; revision=112465

---
+++ gendarme/rules/Gendarme.Rules.Design.Generic/AvoidMethodWithUnusedGenericTypeRule.cs
@@ -0,0 +1,89 @@
+//
+// Gendarme.Rules.Interoperability.DoNotCastIntPtrToInt32Rule
+//
+// Authors:
+//	Sebastien Pouliot <sebastien@ximian.com>
+//
+// Copyright (C) 2008 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+
+using Mono.Cecil;
+
+using Gendarme.Framework;
+
+namespace Gendarme.Rules.Design.Generic {
+
+	[Problem ("The method parameters are not using all generic type parameters defined.")]
+	[Solution ("Not infering all generic typers in the method parameters can lead to confusing, hard to use, API definitions.")]
+	[FxCopCompatibility ("Microsoft.Design", "CA1004:GenericMethodsShouldProvideTypeParameter")]
+	public class AvoidMethodWithUnusedGenericTypeRule : Rule, IMethodRule {
+
+		public override void Initialize (IRunner runner)
+		{
+			base.Initialize (runner);
+
+			// we only want to run this on assemblies that use 2.0 or later
+			// since generics were not available before
+			Runner.AnalyzeAssembly += delegate (object o, RunnerEventArgs e) {
+				Active = (e.CurrentAssembly.Runtime >= TargetRuntime.NET_2_0);
+			};
+		}
+
+		public RuleResult CheckMethod (MethodDefinition method)
+		{
+			// rule applis only if the method has generic type parameters
+			if (method.GenericParameters.Count == 0)
+				return RuleResult.DoesNotApply;
+
+			// look if every generic type parameter...
+			foreach (GenericParameter gp in method.GenericParameters) {
+				bool found = false;
+				// ... is being used by the method parameters
+				foreach (ParameterDefinition pd in method.Parameters) {
+					if (pd.ParameterType.FullName == gp.FullName) {
+						found = true;
+						break;
+					}
+
+					// handle things like ICollection<T>
+					GenericInstanceType git = (pd.ParameterType as GenericInstanceType);
+					if (git == null)
+						continue;
+
+					foreach (GenericParameter igp in git.GenericArguments) {
+						if (igp.FullName == gp.FullName) {
+							found = true;
+							break;
+						}
+					}
+				}
+				if (!found) {
+					string msg = String.Format ("Generic parameter '{0}' is not used by the method parameters.", gp.FullName);
+					Runner.Report (method, Severity.Medium, Confidence.High, msg);
+				}
+			}
+			return Runner.CurrentRuleResult;
+		}
+	}
+}
