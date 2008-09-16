ebf78a74afc9337cf25582f6318862b1a10480e2
Sebastien Pouliot <sebastien@ximian.com>
2008-09-16 01:42:08 +0000

    2008-09-15  Sebastien Pouliot  <sebastien@ximian.com>
    	* AvoidArgumentExceptionDefaultConstructorRule.cs,
    	AvoidThrowingBasicExceptionsRule.cs, DoNotDestroyStackTraceRule.cs,
    	DoNotThrowReservedExceptionRule.cs, 
    	DontSwallowErrorsCatchingNonspecificExceptionsRule.cs,
    	ExceptionShouldBeVisibleRule.cs, 
    	InstantiateArgumentExceptionCorrectlyRule.cs, 
    	MissingExceptionConstructorsRule.cs: Add XML documentation.
    	* SEHHandlerType.cs: Change XML documentation into normal comments.
    
    
    svn path=/trunk/mono-tools/; revision=113119

--- gendarme/rules/Gendarme.Rules.Exceptions/SEHHandlerType.cs
+++ gendarme/rules/Gendarme.Rules.Exceptions/SEHHandlerType.cs
@@ -1,32 +1,21 @@
 using System;
 
 namespace Gendarme.Rules.Exceptions {
-	/// <summary>
-	/// Enumerates the types of SEH handler blocks
-	/// </summary>
+
+	// Enumerates the types of SEH handler blocks
 	public enum SEHHandlerType {
-		/// <summary>
-		/// An SEH catch block, invoked if an exception is thrown in the
-		/// guarded block that matches or descends from a specified class
-		/// </summary>
+		// An SEH catch block, invoked if an exception is thrown in the
+		// guarded block that matches or descends from a specified class
 		Catch,
 
-		/// <summary>
-		/// An SEH fault block, invoked if any exception is thrown in
-		/// the guarded block
-		/// </summary>
+		// An SEH fault block, invoked if any exception is thrown in the guarded block
 		Fault,
 
-		/// <summary>
-		/// An SEH filter block, invoked if any exception is thrown in
-		/// the guarded block
-		/// </summary>
+		// An SEH filter block, invoked if any exception is thrown in the guarded block
 		Filter,
 
-		/// <summary>
-		/// An SEH finally block, invoked regardless of whether or not
-		/// an exception is thrown in the guarded block
-		/// </summary>
+		// An SEH finally block, invoked regardless of whether or not
+		// an exception is thrown in the guarded block
 		Finally,
 	}
 }
