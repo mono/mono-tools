5cbdd0a1bf5582c46e695630ad9e64b15ba83beb
Sebastien Pouliot <sebastien@ximian.com>
2008-09-19 01:32:20 +0000

    2008-09-18  Sebastien Pouliot  <sebastien@ximian.com>
    	* DeclareEventHandlersCorrectlyRule.cs: I misguided Nestor :(
    	The logic is now to report against the EventDefinition (in the
    	assembly being analyzed) instead of reporting against the 
    	delegate (which could be outside of the developer's control).
    
    
    svn path=/trunk/mono-tools/; revision=113488

--- gendarme/rules/Gendarme.Rules.Design/DeclareEventHandlersCorrectlyRule.cs
+++ gendarme/rules/Gendarme.Rules.Design/DeclareEventHandlersCorrectlyRule.cs
@@ -37,38 +37,53 @@ namespace Gendarme.Rules.Design {
 	[Problem ("The delegate which handles the event haven't the correct signature.")]
 	[Solution ("You should correct the signature, return type, parameter types or parameter names.")]
 	public class DeclareEventHandlersCorrectlyRule : Rule, ITypeRule {
-		static IList<TypeReference> possibleTargets = new List<TypeReference> ();
+		static IList<TypeReference> valid_event_handler_types = new List<TypeReference> ();
 
-		private void CheckReturnVoid (TypeReference eventType, MethodReference invoke)
+		private bool CheckReturnVoid (TypeReference eventType, MethodReference invoke)
 		{
-			if (String.Compare (invoke.ReturnType.ReturnType.FullName, "System.Void") != 0)
-				Runner.Report (eventType, Severity.Medium, Confidence.High, String.Format ("The delegate should return void, not {0}", invoke.ReturnType.ReturnType.FullName));
+			if (String.Compare (invoke.ReturnType.ReturnType.FullName, "System.Void") == 0)
+				return true;
+
+			Runner.Report (eventType, Severity.Medium, Confidence.High, String.Format ("The delegate should return void, not {0}", invoke.ReturnType.ReturnType.FullName));
+			return false;
 		}
 
-		private void CheckAmountOfParameters (TypeReference eventType, MethodReference invoke)
+		private bool CheckAmountOfParameters (TypeReference eventType, MethodReference invoke)
 		{
-			if (invoke.Parameters.Count != 2)
-				Runner.Report (eventType, Severity.Medium, Confidence.High, String.Format ("The delegate should have 2 parameters"));
+			if (invoke.Parameters.Count == 2)
+				return true;
+
+			Runner.Report (eventType, Severity.Medium, Confidence.High, String.Format ("The delegate should have 2 parameters"));
+			return false;
 		}
 
-		private void CheckParameterTypes (TypeReference eventType, MethodReference invoke)
+		private bool CheckParameterTypes (TypeReference eventType, MethodReference invoke)
 		{
+			bool ok = true;
 			if (invoke.Parameters.Count >= 1) {
-				if (String.Compare (invoke.Parameters[0].ParameterType.FullName, "System.Object") != 0)
+				if (String.Compare (invoke.Parameters[0].ParameterType.FullName, "System.Object") != 0) {
 					Runner.Report (eventType, Severity.Medium, Confidence.High, String.Format ("The first parameter should have an object, not {0}", invoke.Parameters[0].ParameterType.FullName));
+					ok = false;
+				}
 			}
 			if (invoke.Parameters.Count >= 2) {
-				if (!invoke.Parameters[1].ParameterType.Inherits ("System.EventArgs"))
+				if (!invoke.Parameters[1].ParameterType.Inherits ("System.EventArgs")) {
 					Runner.Report (eventType, Severity.Medium, Confidence.High, "The second parameter should be a subclass of System.EventArgs");
+					ok = false;
+				}
 			}
+			return ok;
 		}
 
-		private void CheckParameterName (TypeReference eventType, MethodReference invoke, int position, string expected)
+		private bool CheckParameterName (TypeReference eventType, MethodReference invoke, int position, string expected)
 		{
 			if (invoke.Parameters.Count >= position + 1) {
-				if (String.Compare (invoke.Parameters[position].Name, expected) != 0)
+				if (String.Compare (invoke.Parameters[position].Name, expected) != 0) {
 					Runner.Report (eventType, Severity.Low, Confidence.High, String.Format ("The expected name is {0}, not {1}", expected, invoke.Parameters[position].Name));
+					return false;
+				}
 			}
+			return true;
 		}
 
 		public RuleResult CheckType (TypeDefinition type)
@@ -76,26 +91,41 @@ namespace Gendarme.Rules.Design {
 			if (type.Events.Count == 0)
 				return RuleResult.DoesNotApply;
 
+			// Note: this is a bit more complex than it seems at first sight
+			// The "real" defect is on the delegate type, which can be a type (reference) outside the
+			// specified assembly set (i.e. where we have no business to report defects against) but
+			// we still want to report the defect against the EventDefinition (which is inside one of
+			// the specified assemblies)
 			foreach (EventDefinition each in type.Events) {
-				if (!possibleTargets.Contains (each.EventType))
-					possibleTargets.Add (each.EventType);	
+				TypeReference tr = each.EventType;
+				// don't check the same type over and over again
+				if (valid_event_handler_types.Contains (tr))
+					continue;
+
+				TypeDefinition td = tr.Resolve ();
+				if (td == null)
+					continue;
+
+				MethodReference invoke = td.GetMethod (MethodSignatures.Invoke);
+				if (invoke == null)
+					continue;
+
+				bool valid = CheckReturnVoid (td, invoke);
+				valid &= CheckAmountOfParameters (td, invoke);
+				valid &= CheckParameterTypes (td, invoke);
+				valid &= CheckParameterName (td, invoke, 0, "sender");
+				valid &= CheckParameterName (td, invoke, 1, "e");
+
+				// avoid re-processing the same *valid* type multiple times
+				if (valid)
+					valid_event_handler_types.Add (tr);
 			}
 			return Runner.CurrentRuleResult;
 		}
 
 		public override void TearDown (IRunner runner)
 		{
-			foreach (TypeReference type in possibleTargets) {
-				MethodReference invoke = type.GetMethod (MethodSignatures.Invoke);
-				if (invoke != null) {
-					CheckReturnVoid (type, invoke);
-					CheckAmountOfParameters (type, invoke);
-					CheckParameterTypes (type, invoke);
-					CheckParameterName (type, invoke, 0, "sender");
-					CheckParameterName (type, invoke, 1, "e");
-				}
-			}
-			possibleTargets.Clear ();
+			valid_event_handler_types.Clear ();
 			base.TearDown (runner);
 		}
 	}
