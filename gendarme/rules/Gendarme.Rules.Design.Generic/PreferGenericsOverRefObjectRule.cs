6c6e5ab7dfcf734f3c1230ac13d43d59f31336b2
Sebastien Pouliot <sebastien@ximian.com>
2008-10-26 15:54:22 +0000

    2008-10-26  Sebastien Pouliot  <sebastien@ximian.com>
    	* PreferGenericsOverRefObjectRule.cs: New. Rule that check method
    	parameters to look for reference to System.Object that could be, with
    	2.0 and later, replaced by generics.
    	* Makefile.am: Add new rule and test to the build.
    	* Gendarme.Rules.Design.Generic.mdp: Update MD project file.
    
    
    svn path=/trunk/mono-tools/; revision=117096

---
+++ gendarme/rules/Gendarme.Rules.Design.Generic/PreferGenericsOverRefObjectRule.cs
@@ -0,0 +1,98 @@
+//
+// Gendarme.Rules.Design.Generic.PreferGenericsOverRefObjectRule
+//
+// Authors:
+//	Sebastien Pouliot <sebastien@ximian.com>
+//
+// Copyright (C) 2008 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+
+using Mono.Cecil;
+
+using Gendarme.Framework;
+using Gendarme.Framework.Helpers;
+using Gendarme.Framework.Rocks;
+
+namespace Gendarme.Rules.Design.Generic {
+
+	/// <summary>
+	/// This rule looks at each parameter of every methods to find reference, <c>ref</c> or
+	/// <c>out</c> in C#, to an <c>System.Object</c>. When using the CLR 2 (and later) such
+	/// parameters can generally be rewritten using generics. This provides additional type
+	/// safety, reduce casts and makes the API easier to consume.
+	/// </summary>
+	/// <example>
+	/// Bad example:
+	/// <code>
+	/// // common before 2.0 but we can do better now
+	/// public bool TryGetValue (string key, ref object value)
+	/// {
+	///	// ...
+	/// }
+	/// </code>
+	/// </example>
+	/// <example>
+	/// Good example:
+	/// <code>
+	/// public bool TryGetValue&lt;T&gt; (string key, ref T value)
+	/// {
+	///	// ...
+	/// }
+	/// </code>
+	/// </example>
+	/// <remarks>This rule is available since Gendarme 2.2</remarks>
+
+	[Problem ("This method contains an object reference parameter that could be changed into a generic.")]
+	[Solution ("Change the parameter to use a generic type where the caller will provide the type.")]
+	[FxCopCompatibility ("Microsoft.Design", "CA1007:UseGenericsWhereAppropriate")]
+	public class PreferGenericsOverRefObjectRule : Rule, IMethodRule {
+
+		public override void Initialize (IRunner runner)
+		{
+			base.Initialize (runner);
+
+			// we only want to run this on assemblies that use 2.0 or later
+			// since generics were not available before
+			Runner.AnalyzeAssembly += delegate (object o, RunnerEventArgs e) {
+				Active = (e.CurrentAssembly.Runtime >= TargetRuntime.NET_2_0);
+			};
+		}
+
+		public RuleResult CheckMethod (MethodDefinition method)
+		{
+			// rule does not apply to properties, events, without parameters or for generated code
+			if (method.IsSpecialName || (method.Parameters.Count == 0) || method.IsGeneratedCode ())
+				return RuleResult.DoesNotApply;
+
+			foreach (ParameterDefinition parameter in method.Parameters) {
+				if (parameter.ParameterType.FullName != "System.Object&")
+					continue;
+
+				// suggest using generics
+				Runner.Report (parameter, Severity.Medium, Confidence.High);
+			}
+			return Runner.CurrentRuleResult;
+		}
+	}
+}
