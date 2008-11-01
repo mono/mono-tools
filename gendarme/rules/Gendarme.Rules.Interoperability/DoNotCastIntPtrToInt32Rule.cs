4116150dd3c472b6fea7f2343f27fc93953ecd2b
Sebastien Pouliot <sebastien@ximian.com>
2008-11-01 16:37:49 +0000

    2008-11-01  Sebastien Pouliot  <sebastien@ximian.com>
    	* DoNotCastIntPtrToInt32Rule.cs: Now cover the case where Marshal.
    	ReadInt[32|64] is used to read pointers (i.e. casted into IntPtr). 
    	Renamed rule to (more general) DoNotAssumeIntPtrSizeRule
    
    
    svn path=/trunk/mono-tools/; revision=117613

--- gendarme/rules/Gendarme.Rules.Interoperability/DoNotCastIntPtrToInt32Rule.cs
+++ gendarme/rules/Gendarme.Rules.Interoperability/DoNotCastIntPtrToInt32Rule.cs
@@ -1,5 +1,5 @@
 //
-// Gendarme.Rules.Interoperability.DoNotCastIntPtrToInt32Rule
+// Gendarme.Rules.Interoperability.DoNotAssumeIntPtrSizeRule
 //
 // Authors:
 //	Sebastien Pouliot <sebastien@ximian.com>
@@ -34,16 +34,19 @@ using Mono.Cecil.Cil;
 using Gendarme.Framework;
 using Gendarme.Framework.Engines;
 using Gendarme.Framework.Helpers;
+using Gendarme.Framework.Rocks;
 
 namespace Gendarme.Rules.Interoperability {
 
 	/// <summary>
 	/// This rule checks for any cast of <code>IntPtr</code> or <code>UIntPtr</code> into a
-	/// 32bits (or smaller) value. <code>IntPtr</code> are generally used to reference a 
-	/// memory location and downcasting them to 32bits will make the code fail on 64bits CPU.
+	/// 32bits (or smaller) value. It will also check if memory read with <c>Marshal.ReadInt32</c>
+	/// and <c>Marshal.ReadInt64</c> methods are being casted into an <code>IntPtr</code> or 
+	/// <code>UIntPtr</code>. <code>IntPtr</code> are generally used to reference a  memory 
+	/// location and downcasting them to 32bits will make the code fail on 64bits CPU.
 	/// </summary>
 	/// <example>
-	/// Bad example:
+	/// Bad example (cast):
 	/// <code>
 	/// int ptr = dest.ToInt32 ();
 	/// for (int i = 0; i &lt; 16; i++) {
@@ -53,7 +56,14 @@ namespace Gendarme.Rules.Interoperability {
 	/// </code>
 	/// </example>
 	/// <example>
-	/// Good example:
+	/// Bad example (Marshal.Read*):
+	/// <code>
+	/// // that won't work on 64 bits platforms
+	/// IntPtr p = (IntPtr) Marshal.ReadInt32 (p);
+	/// </code>
+	/// </example>
+	/// <example>
+	/// Good example (cast):
 	/// <code>
 	/// long ptr = dest.ToInt64 ();
 	/// for (int i = 0; i &lt; 16; i++) {
@@ -62,11 +72,18 @@ namespace Gendarme.Rules.Interoperability {
 	/// }
 	/// </code>
 	/// </example>
+	/// <example>
+	/// Good example (Marshal.Read*):
+	/// <code>
+	/// IntPtr p = (IntPtr) Marshal.ReadIntPtr (p);
+	/// </code>
+	/// </example>
+	/// <remarks>This rule is available since Gendarme 2.0 but was named DoNotCastIntPtrToInt32Rule before 2.2</remarks>
 
 	[Problem ("This method cast a [U]IntPtr to a 32 bits value and this won't work on 64 bits architectures.")]
 	[Solution ("You should always use 64 bits integers, signed or unsigned, when computing pointers.")]
 	[EngineDependency (typeof (OpCodeEngine))]
-	public class DoNotCastIntPtrToInt32Rule : Rule, IMethodRule {
+	public class DoNotAssumeIntPtrSizeRule : Rule, IMethodRule {
 
 		private void Report (MethodDefinition method, Instruction ins, string typeName)
 		{
@@ -77,7 +94,7 @@ namespace Gendarme.Rules.Interoperability {
 		private static string Convert (Instruction ins)
 		{
 			if (ins == null)
-				return null;
+				return String.Empty;
 
 			switch (ins.OpCode.Code) {
 			case Code.Conv_I1:
@@ -93,7 +110,56 @@ namespace Gendarme.Rules.Interoperability {
 			case Code.Conv_U4:
 				return Constants.UInt32;
 			default:
-				return null;
+				return String.Empty;
+			}
+		}
+
+		private void CheckCastOnIntPtr (MethodDefinition method, Instruction ins, MethodReference mr, bool intptr, bool uintptr)
+		{
+			// check for calls to IntPtr.ToInt32, UIntPtr.ToUInt32 or 
+			// cast to anything other than [U]IntPtr, [U]Int64 or Void*
+			if (intptr && (mr.Name == "ToInt32")) {
+				Runner.Report (method, ins, Severity.High, Confidence.Normal, "Call to 'IntPtr.ToInt32()'.");
+			} else if (uintptr && (mr.Name == "ToUInt32")) {
+				Runner.Report (method, ins, Severity.High, Confidence.Normal, "Call to 'UIntPtr.ToUInt32()'.");
+			} else if (mr.Name == "op_Explicit") {
+				switch (mr.ReturnType.ReturnType.FullName) {
+				case "System.Int64":
+				case "System.UInt64":
+					// valid cases unless (like [g]mcs does) it's followed by a convertion
+					string msg = Convert (ins.Next);
+					if (msg.Length > 0)
+						Report (method, ins, msg);
+					break;
+				case "System.IntPtr": // used to cast void*
+				case "System.UIntPtr": // used to cast void*
+				case "System.Void*": // used to cast void*
+					// valid cases
+					break;
+				default:
+					// problematic cases
+					Report (method, ins, mr.ReturnType.ReturnType.FullName);
+					break;
+				}
+			}
+		}
+
+		private void CheckCastOnMarshal (MethodDefinition method, Instruction ins, MethodReference mr)
+		{
+			// if we're reading an int32 or an int64 then we should not be assigning it to an IntPtr
+			string method_name = mr.Name;
+			if ((method_name == "ReadInt32") || (method_name == "ReadInt64")) {
+				Instruction next = ins.Next;
+				if (next.OpCode.Code != Code.Call)
+					return;
+
+				MethodReference m = (next.Operand as MethodReference);
+				if (m.Name != "op_Explicit")
+					return;
+
+				string msg = String.Format ("A '{0}' value is casted into an '{1}' when reading marshalled memory.",
+					mr.ReturnType.ReturnType.FullName, m.Parameters [0].ParameterType.FullName);
+				Runner.Report (method, ins, Severity.High, Confidence.Normal, msg);
 			}
 		}
 
@@ -119,34 +185,10 @@ namespace Gendarme.Rules.Interoperability {
 				string type = mr.DeclaringType.FullName;
 				bool intptr = (type == "System.IntPtr");
 				bool uintptr = (type == "System.UIntPtr");
-				if (!intptr && !uintptr)
-					continue;
-
-				// check for calls to IntPtr.ToInt32, UIntPtr.ToUInt32 or 
-				// cast to anything other than [U]IntPtr, [U]Int64 or Void*
-				if (intptr && (mr.Name == "ToInt32")) {
-					Runner.Report (method, ins, Severity.High, Confidence.Normal, "Call to 'IntPtr.ToInt32()'.");
-				} else if (uintptr && (mr.Name == "ToUInt32")) {
-					Runner.Report (method, ins, Severity.High, Confidence.Normal, "Call to 'UIntPtr.ToUInt32()'.");
-				} else if (mr.Name == "op_Explicit") {
-					switch (mr.ReturnType.ReturnType.FullName) {
-					case "System.Int64":
-					case "System.UInt64":
-						// valid cases unless (like [g]mcs does) it's followed by a convertion
-						string msg = Convert (ins.Next);
-						if (msg != null)
-							Report (method, ins, msg);
-						break;
-					case "System.IntPtr": // used to cast void*
-					case "System.UIntPtr": // used to cast void*
-					case "System.Void*": // used to cast void*
-						// valid cases
-						break;
-					default:
-						// problematic cases
-						Report (method, ins, mr.ReturnType.ReturnType.FullName);
-						break;
-					}
+				if (intptr || uintptr) {
+					CheckCastOnIntPtr (method, ins, mr, intptr, uintptr);
+				} else if (type == "System.Runtime.InteropServices.Marshal") {
+					CheckCastOnMarshal (method, ins, mr);
 				}
 			}
 			return Runner.CurrentRuleResult;
