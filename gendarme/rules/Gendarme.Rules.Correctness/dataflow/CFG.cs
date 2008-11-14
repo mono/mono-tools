fc0d2f4ee3464ab65e9ed0dcdc9089a975ee7d5d
Sebastien Pouliot <sebastien@ximian.com>
2008-11-14 01:45:20 +0000

    2008-11-13  Sebastien Pouliot  <sebastien@ximian.com>
    	* dataflow/CFG.cs: Extract helper class MethodPrinter. Patch by
    	Jesse Jones
    
    
    svn path=/trunk/mono-tools/; revision=118804

--- gendarme/rules/Gendarme.Rules.Correctness/dataflow/CFG.cs
+++ gendarme/rules/Gendarme.Rules.Correctness/dataflow/CFG.cs
@@ -14,7 +14,9 @@
 
 using System;
 using System.Collections;
+using System.Diagnostics;
 using System.IO;
+using Gendarme.Framework;
 using Mono.Cecil;
 using Mono.Cecil.Cil;
 
@@ -24,8 +26,12 @@ public class CFG : Graph {
 
     [NonNull] private InstructionCollection instructions;
     [NonNull] private MethodDefinition method;
-    [NonNull] private IDictionary branchTable;
+    [NonNull] private MethodPrinter printer;
     private BasicBlock entryPoint;
+
+    public MethodPrinter Printer {
+        get { return printer; }
+    }
 
     public BasicBlock EntryPoint {
         get { return entryPoint; }
@@ -33,12 +39,16 @@ public class CFG : Graph {
 
     public CFG([NonNull] MethodDefinition method)
     {
+        printer = new MethodPrinter (method);
         Init(method.Body.Instructions, method);
     }
 
-    public CFG(InstructionCollection instructions)
+    public bool BeginsCatch(BasicBlock bb)
     {
-        Init(instructions, null);
+        ExceptionHandler handler = printer.StartsHandlerRegion (bb.FirstInstruction);
+        if (handler != null && handler.Type == ExceptionHandlerType.Catch)
+            return true;
+        return false;
     }
 
     private void Init([NonNull] InstructionCollection instructions,
@@ -46,25 +56,7 @@ public class CFG : Graph {
     {
         this.instructions = instructions;
         this.method = method;
-        InitBranchTable();
         BuildGraph();
-    }
-
-    private static bool IsBranch(Instruction instruction)
-    {
-        if(instruction == null)
-            return false;
-        switch(instruction.OpCode.FlowControl) {
-            case FlowControl.Branch: return true;
-            case FlowControl.Cond_Branch: return true;
-            case FlowControl.Return: return true;
-            /* Throw creates a new basic block, but it has no target,
-             * because the object to be thrown is taken from the stack.
-             * Thus, its type is not known before runtime, and we can't
-             * know which catch block will recieve it. */
-            case FlowControl.Throw: return true;
-        }
-        return false;
     }
 
     private static bool HasNext([NonNull] Instruction instruction)
@@ -77,39 +69,6 @@ public class CFG : Graph {
             return false;
         else
             return true;
-    }
-
-    private static int[] BranchTargets([NonNull] Instruction instruction)
-    {
-        int[] result = null;
-        switch(instruction.OpCode.OperandType) {
-            case OperandType.InlineSwitch:
-                Instruction[] targets = (Instruction[])instruction.Operand;
-                result = new int[targets.Length];
-                int i = 0;
-                foreach(Instruction target in targets) {
-                    result[i] = target.Offset;
-                    i++;
-                }
-                break;
-            case OperandType.InlineBrTarget:
-                result = new int[1];
-                result[0] = ((Instruction)instruction.Operand).Offset;
-                break;
-            case OperandType.ShortInlineBrTarget:
-                result = new int[1];
-                result[0] = ((Instruction)instruction.Operand).Offset;
-                break;
-        }
-        return result;
-    }
-
-    public bool BeginsCatch(BasicBlock bb)
-    {
-        ExceptionHandler handler = StartsHandlerRegion(bb.FirstInstruction);
-        if(handler != null && handler.Type == ExceptionHandlerType.Catch)
-            return true;
-        return false;
     }
 
     private BasicBlock GetNearestFinally(Instruction insn, Hashtable insnBB)
@@ -129,99 +88,6 @@ public class CFG : Graph {
         return nearest;
     }
 
-    private static bool OffsetsEqual(Instruction insn1, Instruction insn2)
-    {
-        if(insn1 == insn2) return true;
-        if(insn1 == null) return false;
-        if(insn2 == null) return false;
-        if(insn1.Offset == insn2.Offset) return true;
-        return false;
-    }
-
-    private ExceptionHandler EndsTryRegion([NonNull] Instruction instruction)
-    {
-        foreach(ExceptionHandler handler in method.Body.ExceptionHandlers) {
-            if(instruction != null)
-                if(OffsetsEqual(instruction.Next, handler.TryEnd))
-                    return handler;
-        }
-        return null;
-    }
-
-    private ExceptionHandler EndsHandlerRegion(
-            [NonNull] Instruction instruction)
-    {
-        foreach(ExceptionHandler handler in method.Body.ExceptionHandlers) {
-            if(instruction != null)
-                if(OffsetsEqual(instruction.Next, handler.HandlerEnd))
-                    return handler;
-        }
-        return null;
-    }
-
-    private ExceptionHandler StartsTryRegion(
-            [NonNull] Instruction instruction)
-    {
-        foreach(ExceptionHandler handler in method.Body.ExceptionHandlers) {
-            if(OffsetsEqual(instruction, handler.TryStart))
-                return handler;
-        }
-        return null;
-    }
-
-    private ExceptionHandler StartsHandlerRegion(
-            [NonNull] Instruction instruction)
-    {
-        foreach(ExceptionHandler handler in method.Body.ExceptionHandlers) {
-            if(OffsetsEqual(instruction, handler.HandlerStart))
-                return handler;
-        }
-        return null;
-    }
-
-    private bool IsLeader([NonNull] Instruction instruction,
-            [NonNull] Instruction previous)
-    {
-        /* First instruction in the method */
-        if(previous == null)
-            return true;
-
-        /* Target of a branch */
-        if(branchTable.Contains(instruction.Offset))
-            return true;
-
-        /* Follows a control flow instruction */
-        if(IsBranch(instruction.Previous))
-            return true;
-
-        /* Is the beginning of a try region */
-        if(StartsTryRegion(instruction) != null)
-            return true;
-
-        /* Is the beginning of a handler region */
-        if(StartsHandlerRegion(instruction) != null)
-            return true;
-
-        return false;
-    }
-
-    private void InitBranchTable() {
-        branchTable = new Hashtable();
-        foreach(Instruction instr in instructions) {
-            int[] targets = BranchTargets(instr);
-            if(targets != null)
-                foreach(int target in targets)
-                    if(!branchTable.Contains(target)) {
-                        IList sources = new ArrayList();
-                        sources.Add(target);
-                        branchTable.Add(target, sources);
-                    } else {
-                        IList sources = (IList)branchTable[target];
-                        sources.Add(target);
-                    }
-        }
-    }
-
     private void BuildGraph() {
         BasicBlock tail = null;
         Instruction prevInsn = null;
@@ -234,7 +100,7 @@ public class CFG : Graph {
         AddNode(exit);
 
         foreach(Instruction insn in instructions) {
-            if(IsLeader(insn, prevInsn)) {
+            if (printer.IsLeader (insn, prevInsn)) {
                 tail = new BasicBlock(instructions);
                 tail.first = currentInsnNum;
                 AddNode(tail);
@@ -257,8 +123,8 @@ public class CFG : Graph {
         }
 
         foreach(Instruction insn in instructions) {
-            if((EndsTryRegion(insn) != null) ||
-                    (EndsHandlerRegion(insn) != null)) { 
+            if ((printer.EndsTryRegion (insn) != null) ||
+                    (printer.EndsHandlerRegion (insn) != null)) { 
                 BasicBlock finallyBB = GetNearestFinally(insn, insnBB);
                 if(finallyBB != null)
                     AddEdge(new CFGEdge((BasicBlock)insnBB[insn.Offset],
@@ -280,7 +146,7 @@ public class CFG : Graph {
             if((insn.OpCode.Value != OpCodeConstants.Leave) &&
                     (insn.OpCode.Value != OpCodeConstants.Endfinally)) {
                     */
-                int[] targets = BranchTargets(insn);
+                int[] targets = MethodPrinter.BranchTargets (insn);
                 if(targets == null)
                     continue;
                 foreach(int target in targets) {
@@ -294,49 +160,14 @@ public class CFG : Graph {
         entryPoint = (BasicBlock)insnBB[0];
     }
 
-    public void PrintBasicBlocks() {
-        Instruction prevInstr = null;
-
-        Console.WriteLine(method.Name);
-        Console.WriteLine("Number of parameters: {0}",
-                method.Parameters.Count);
-        foreach(Instruction instr in instructions) {
-            if(StartsTryRegion(instr) != null)
-                Console.WriteLine("Try {");
-            if(StartsHandlerRegion(instr) != null)
-                Console.WriteLine("Handle {");
-
-            if(IsLeader(instr, prevInstr))
-                Console.Write("* ");
-            else
-                Console.Write("  ");
-            Console.Write("  {0}: {1}", instr.Offset.ToString("X4"),
-                    instr.OpCode.Name);
-            int[] targets = BranchTargets(instr);
-            if(targets != null)
-                foreach(int target in targets)
-                    Console.Write(" {0}", target.ToString("X4"));
-            else if(instr.Operand is string)
-                Console.Write(" \"{0}\"", instr.Operand.ToString());
-            else if(instr.Operand != null)
-                Console.Write(" {0}", instr.Operand.ToString());
-            Console.WriteLine();
-            prevInstr = instr;
-            if(EndsTryRegion(instr) != null)
-                Console.WriteLine("} (Try)");
-            if(EndsHandlerRegion(instr) != null)
-                Console.WriteLine("} (Handle)");
-        }
-    }
-
     public void PrintDot() {
         string name = method.DeclaringType.Name + "." + method.Name + ".dot";
         FileMode mode = FileMode.Create;
         StreamWriter writer = new StreamWriter(new FileStream(name, mode));
-	writer.WriteLine ("digraph {0} (", method.Name);
+		writer.WriteLine ("digraph {0} (", method.Name);
         foreach(Node node in Nodes) {
             BasicBlock bb = (BasicBlock)node;
-	    writer.WriteLine ("    \"{0}\" [ label = \"{1}\" ];", bb, bb);
+	   	writer.WriteLine ("    \"{0}\" [ label = \"{1}\" ];", bb, bb);
         }
 
         foreach(Edge edge in Edges) {
