4349455e3a5c4d36ec14e56e778bbfad29f6aa84
Cedric Vivier <cedricv@neonux.com>
2008-12-23 07:05:26 +0000

    2008-12-23  Cedric Vivier  <cedricv@neonux.com>
    	* ProvideCorrectRegexPatternRule.cs: New. Rule that checks if
    	methods/constructors are provided a valid regular expression pattern.
    
    svn path=/trunk/mono-tools/; revision=122025

---
+++ gendarme/rules/Gendarme.Rules.Correctness/ProvideCorrectRegexPatternRule.cs
@@ -0,0 +1,205 @@
+//
+// Gendarme.Rules.Correctness.ProvideCorrectRegexPatternRule class
+//
+// Authors:
+//	Cedric Vivier <cedricv@neonux.com>
+//
+// Copyright (C) 2008 Cedric Vivier
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Text;
+
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+using Gendarme.Framework;
+using Gendarme.Framework.Rocks;
+using Gendarme.Framework.Engines;
+using Gendarme.Framework.Helpers;
+
+using System.Text.RegularExpressions;
+
+namespace Gendarme.Rules.Correctness {
+
+	/// <summary>
+	/// This rule checks if methods/constructors requiring a regular expression
+	/// string are provided a valid pattern.
+	/// </summary>
+	/// <example>
+	/// Bad example:
+	/// <code>
+	/// Regex re = new Regex ("^\\"); //Invalid end of pattern
+	/// </code>
+	/// </example>
+	/// <example>
+	/// Good example:
+	/// <code>
+	/// Regex re = new Regex (@"^\\");
+	/// </code>
+	/// </example>
+	/// <example>
+	/// Bad example:
+	/// <code>
+	/// Regex re = new Regex ("([a-z)*"); //Unterminated [] set
+	/// </code>
+	/// </example>
+	/// <example>
+	/// Good example:
+	/// <code>
+	/// Regex re = new Regex ("([a-z])*");
+	/// </code>
+	/// </example>
+	/// <example>
+	/// Bad example:
+	/// <code>
+	/// return Regex.IsMatch (code, @"(\w)-\2"); //Reference to undefined group number 2
+	/// </code>
+	/// </example>
+	/// <example>
+	/// Good example:
+	/// <code>
+	/// return Regex.IsMatch (code, @"(\w)-\1");
+	/// </code>
+	/// </example>
+
+	[Problem ("An invalid regular expression string is provided to a method/constructor.")]
+	[Solution ("Fix the invalid regular expression.")]
+	[EngineDependency (typeof (OpCodeEngine))]
+	public class ProvideCorrectRegexPatternRule : Rule, IMethodRule {
+
+		static OpCodeBitmask callsAndNewobjBitmask = BuildCallsAndNewobjOpCodeBitmask ();
+
+		MethodDefinition method;
+
+		const string RegexClass = "System.Text.RegularExpressions.Regex";
+		const string ValidatorClass = "System.Configuration.RegexStringValidator";
+
+		public override void Initialize (IRunner runner)
+		{
+			base.Initialize (runner);
+
+			Runner.AnalyzeModule += delegate (object o, RunnerEventArgs e) {
+				bool usingRegexClass = e.CurrentAssembly.Name.Name == "System"
+				                       || e.CurrentModule.TypeReferences.ContainsType (RegexClass);
+				bool usingValidatorClass = e.CurrentAssembly.Runtime >= TargetRuntime.NET_2_0
+				                           && (e.CurrentAssembly.Name.Name == "System.Configuration"
+				                              || e.CurrentModule.TypeReferences.ContainsType (ValidatorClass));
+				Active = usingRegexClass | usingValidatorClass;
+			};
+		}
+
+		void CheckPattern (Instruction ins, int argumentOffset)
+		{
+			Instruction ld = ins.TraceBack (method, argumentOffset);
+			if (null == ld)
+				return;
+
+			switch (ld.OpCode.Code) {
+			case Code.Ldstr:
+				CheckPattern (ins, (string) ld.Operand);
+				break;
+			case Code.Ldsfld:
+				FieldReference f = (FieldReference) ld.Operand;
+				if (f.Name != "Empty" || f.DeclaringType.FullName != "System.String")
+					return;
+				CheckPattern (ins, null);
+				break;
+			case Code.Ldnull:
+				CheckPattern (ins, null);
+				break;
+			}
+		}
+
+		void CheckPattern (Instruction ins, string pattern)
+		{
+			if (string.IsNullOrEmpty (pattern)) {
+				Runner.Report (method, ins, Severity.High, Confidence.High, "Pattern is null or empty.");
+				return;
+			}
+
+			try {
+				new Regex (pattern);
+			} catch (Exception e) {
+				/* potential set of exceptions is not well documented and potentially changes with regarts to
+				   different runtime and/or runtime version. */
+				string msg = string.Format ("Pattern '{0}' is invalid. Reason: {1}", pattern, e.Message);
+				Runner.Report (method, ins, Severity.High, Confidence.High, msg);
+			}
+		}
+
+		void CheckCall (Instruction ins, MethodReference call)
+		{
+			if (null == call) //resolution did not work
+				return;
+			//check only constructors and static non-property methods
+			if (call.Name != MethodDefinition.Ctor && (call.HasThis || call.IsProperty ()))
+				return;
+			if (!call.HasParameters)
+				return;
+			if (call.DeclaringType.FullName != RegexClass && call.DeclaringType.FullName != ValidatorClass)
+				return;
+
+			MethodDefinition mdef = call.Resolve ();
+			if (null == mdef)
+				return;
+
+			foreach (ParameterDefinition p in mdef.Parameters) {
+				if ((p.Name == "pattern" || p.Name == "regex") && p.ParameterType.FullName == "System.String") {
+					CheckPattern (ins, -(call.HasThis ? 0 : -1 + p.Sequence));
+					return;
+				}
+			}
+		}
+
+		public RuleResult CheckMethod (MethodDefinition method)
+		{
+			if (!method.HasBody)
+				return RuleResult.DoesNotApply;
+
+			this.method = method;
+
+			//is there any interesting opcode in the method?
+			if (!callsAndNewobjBitmask.Intersect (OpCodeEngine.GetBitmask (method)))
+				return RuleResult.DoesNotApply;
+
+			foreach (Instruction ins in method.Body.Instructions) {
+				if (!callsAndNewobjBitmask.Get (ins.OpCode.Code))
+					continue;
+
+				CheckCall (ins, (MethodReference) ins.Operand);
+			}
+
+			return Runner.CurrentRuleResult;
+		}
+
+		static OpCodeBitmask BuildCallsAndNewobjOpCodeBitmask ()
+		{
+			#if true
+				return new OpCodeBitmask (0x8000000000, 0x4400000000000, 0x0, 0x0);
+			#else
+				OpCodeBitmask mask = new OpCodeBitmask ();
+				mask.UnionWith (OpCodeBitmask.Calls);
+				mask.Set (Code.Newobj);
+				return mask;
+			#endif
+		}
+	}
+}
---
+++ gendarme/rules/Gendarme.Rules.Correctness/Test/ProvideCorrectRegexPatternTest.cs
@@ -0,0 +1,293 @@
+//
+// Unit tests for ProvideCorrectRegexPatternRule
+//
+// Authors:
+//	Cedric Vivier <cedricv@neonux.com>
+//
+// Copyright (C) 2008 Cedric Vivier
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using NUnit.Framework;
+
+using Gendarme.Rules.Correctness;
+
+using Test.Rules.Fixtures;
+using Test.Rules.Helpers;
+using Test.Rules.Definitions;
+
+using System.Text.RegularExpressions;
+using System.Configuration;
+
+
+namespace Test.Rules.Correctness {
+
+	class RegexCases {
+		string foo = "foo";
+		const string good1 = "good1";
+		const string bad1 = "bad1\\";
+		Regex re;
+
+		string DoesNotApply1 () {
+			return bad1;
+		}
+
+		void Success0 () {
+			re = new Regex (@"^\\");
+		}
+
+		void Failure0 () {
+			re = new Regex ("^\\");
+		}
+
+		void Success1 () {
+			re = new Regex (good1);
+		}
+
+		void Failure1 () {
+			re = new Regex (bad1);
+		}
+
+		void Success2 (string pattern) {
+			var re = new Regex (pattern);
+		}
+
+		void Failure2 () {
+			var re = new Regex (@"^([a-z)*");
+		}
+
+		bool Success2b (Regex re) {
+			return re.IsMatch ("^([a-z])*");
+		}
+
+		void Success2c () {
+			var re = new Regex (this.ToString ());
+		}
+
+		void Success2d (bool good) {
+			var re = new Regex (good ? "bad\\" : "good");
+		}
+
+		void Failure2d (bool good) {
+			var re = new Regex (good ? "good" : "bad\\");
+		}
+
+		bool Success3 (string pattern) {
+			return Regex.IsMatch (foo, pattern);
+		}
+
+		bool Failure3 (string text) {
+			return Regex.IsMatch (text, @"\9");
+		}
+
+		bool Success4 (string pattern) {
+			if (null != Regex.Match (good1, "good"))
+				return true;
+			return Regex.IsMatch (foo, pattern);
+		}
+
+		bool Failure4 (string text) {
+			if (null != Regex.Match (good1, "good"))
+				return true;
+			return Regex.IsMatch (text, @"\9");
+		}
+
+		string Success5 (string source) {
+			return Regex.Replace (source, "foo", "niaa");
+		}
+
+		string Failure5 (string source) {
+			return Regex.Replace (source, "foo\\", "niaa");
+		}
+
+		string Success6 (string source) {
+			var re = new Regex ("good");
+			return re.Replace ("bad\\", "bad\\");
+		}
+
+		string Failure6 (string source) {
+			return Regex.Replace (source, null, "niaa");
+		}
+
+		string Failure7 (string source) {
+			return Regex.Replace (source, "", "niaa");
+		}
+
+		string Failure67 (string source) {
+			source = Regex.Replace (source, null, "niaa");
+			return Regex.Replace (source, "", "niaa");
+		}
+	}
+
+	class ValidatorCases {
+		void Success1 (string input) {
+			(new RegexStringValidator ("^[a-z]+")).Validate (input);
+		}
+
+		void Failure1 (string input) {
+			(new RegexStringValidator ("^[a-z+")).Validate (input);
+		}
+	}
+
+	[TestFixture]
+	public class ProvideCorrectRegexPatternTest : MethodRuleTestFixture<ProvideCorrectRegexPatternRule> {
+
+		static bool raisedAnalyzeModuleEvent;
+
+		[SetUp]
+		public void RaiseAnalyzeModuleEvent ()
+		{
+			if (raisedAnalyzeModuleEvent)
+				return;
+
+			raisedAnalyzeModuleEvent = true;
+			((TestRunner) Runner).OnModule (DefinitionLoader.GetTypeDefinition<RegexCases> ().Module);
+		}
+
+		[Test]
+		public void DoesNotApply0 ()
+		{
+			AssertRuleDoesNotApply (SimpleMethods.EmptyMethod);
+		}
+
+		[Test]
+		public void DoesNotApply ()
+		{
+			AssertRuleDoesNotApply<RegexCases> ("DoesNotApply1");
+		}
+
+		[Test]
+		public void Success0 ()
+		{
+			AssertRuleSuccess<RegexCases> ("Success0");
+		}
+
+		[Test]
+		public void Success1 ()
+		{
+			AssertRuleSuccess<RegexCases> ("Success1");
+		}
+
+		[Test]
+		public void Success2 ()
+		{
+			AssertRuleSuccess<RegexCases> ("Success2");
+			AssertRuleSuccess<RegexCases> ("Success2b");
+			AssertRuleSuccess<RegexCases> ("Success2c");
+			AssertRuleSuccess<RegexCases> ("Success2d");
+		}
+
+		[Test]
+		public void Success3 ()
+		{
+			AssertRuleSuccess<RegexCases> ("Success3");
+		}
+
+		[Test]
+		public void Success4 ()
+		{
+			AssertRuleSuccess<RegexCases> ("Success4");
+		}
+
+		[Test]
+		public void Success5 ()
+		{
+			AssertRuleSuccess<RegexCases> ("Success5");
+		}
+
+		[Test]
+		public void Success6 ()
+		{
+			AssertRuleSuccess<RegexCases> ("Success6");
+		}
+
+		[Test]
+		public void Failure0 ()
+		{
+			AssertRuleFailure<RegexCases> ("Failure0", 1);
+		}
+
+		[Test]
+		public void Failure1 ()
+		{
+			AssertRuleFailure<RegexCases> ("Failure1", 1);
+		}
+
+		[Test]
+		public void Failure2 ()
+		{
+			AssertRuleFailure<RegexCases> ("Failure2", 1);
+		}
+
+		[Test]
+		public void Failure2d ()
+		{
+			AssertRuleFailure<RegexCases> ("Failure2d", 1);
+		}
+
+		[Test]
+		public void Failure3 ()
+		{
+			AssertRuleFailure<RegexCases> ("Failure3", 1);
+		}
+
+		[Test]
+		public void Failure4 ()
+		{
+			AssertRuleFailure<RegexCases> ("Failure4", 1);
+		}
+
+		[Test]
+		public void Failure5 ()
+		{
+			AssertRuleFailure<RegexCases> ("Failure5", 1);
+		}
+
+		[Test]
+		public void Failure6 ()
+		{
+			AssertRuleFailure<RegexCases> ("Failure6", 1);
+		}
+
+		[Test]
+		public void Failure7 ()
+		{
+			AssertRuleFailure<RegexCases> ("Failure7", 1);
+		}
+
+		[Test]
+		public void Failure67 ()
+		{
+			AssertRuleFailure<RegexCases> ("Failure67", 2);
+		}
+
+		[Test]
+		public void ValidatorClassSuccess1 ()
+		{
+			AssertRuleSuccess<ValidatorCases> ("Success1");
+		}
+
+		[Test]
+		public void ValidatorClassFailure1 ()
+		{
+			AssertRuleFailure<ValidatorCases> ("Failure1", 1);
+		}
+	}
+}
