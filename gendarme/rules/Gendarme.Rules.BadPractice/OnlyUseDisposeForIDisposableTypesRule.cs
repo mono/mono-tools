75ea6086dfa3c8cbe1d3c2093cb2c0b200d8fa21
Jesse Jones <jesjones@mono-cvs.ximian.com>
2009-02-18 05:28:02 +0000

    Added OnlyUseDisposeForIDisposableTypesRule.
    svn path=/trunk/mono-tools/; revision=127245

---
+++ gendarme/rules/Gendarme.Rules.BadPractice/OnlyUseDisposeForIDisposableTypesRule.cs
@@ -0,0 +1,123 @@
+// 
+// Gendarme.Rules.BadPractice.OnlyUseDisposeForIDisposableTypesRule
+//
+// Authors:
+//	Jesse Jones <jesjones@mindspring.com>
+//
+// Copyright (C) 2009 Jesse Jones
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Linq;
+
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+using Gendarme.Framework;
+using Gendarme.Framework.Engines;
+using Gendarme.Framework.Helpers;
+using Gendarme.Framework.Rocks;
+
+namespace Gendarme.Rules.BadPractice {
+
+	/// <summary>
+	/// To avoid confusing developers methods named Dispose should be
+	/// reserved for types that implement IDisposable. 
+	/// </summary>
+	/// <example>
+	/// Bad example:
+	/// <code>
+	/// internal sealed class Worker
+	/// {
+	/// 	// This class uses one or more temporary files to do its work.
+	/// 	private List&lt;string&gt; files = new List&lt;string&gt; ();
+	/// 	
+	/// 	// This is confusing: developers will think they can do things
+	/// 	// like use the instance with a using statement.
+	/// 	public void Dispose ()
+	/// 	{
+	/// 		foreach (string path in files) {
+	/// 			File.Delete (path);
+	/// 		}
+	/// 			
+	/// 		files.Clear ();
+	/// 	}
+	/// }
+	/// </code>
+	/// </example>
+	/// <example>
+	/// Good example:
+	/// internal sealed class Worker
+	/// {
+	/// 	// This class uses one or more temporary files to do its work.
+	/// 	private List&lt;string&gt; files = new List&lt;string&gt; ();
+	/// 	
+	/// 	public void Reset ()
+	/// 	{
+	/// 		foreach (string path in files) {
+	/// 			File.Delete (path);
+	/// 		}
+	/// 			
+	/// 		files.Clear ();
+	/// 	}
+	/// }
+	/// <code>
+	/// </code>
+	/// </example>
+
+	[Problem ("A type has a method named Dispose, but does not implement IDisposable.")]
+	[Solution ("Rename the method or implement IDisposable.")]
+	public sealed class OnlyUseDisposeForIDisposableTypesRule : Rule, ITypeRule {
+	
+		public RuleResult CheckType (TypeDefinition type)
+		{
+			if (!type.HasMethods || type.IsEnum || type.IsDelegate ())
+				return RuleResult.DoesNotApply;
+
+			Log.WriteLine (this);
+			Log.WriteLine (this, "----------------------------------");
+			Log.WriteLine (this, type.FullName);
+			
+			if (!type.Implements ("System.IDisposable")) {
+				Log.WriteLine (this, "type does not implement IDisposable");
+
+				MethodDefinition [] methods = type.Methods.GetMethod ("Dispose");
+				foreach (MethodDefinition method in methods)
+				{
+					Log.WriteLine (this, "found {0}", method);
+					
+					Severity severity;
+					if (method.IsVisible ())
+						severity = Severity.High;
+					else if (!method.IsPrivate)
+						severity = Severity.Medium;
+					else
+						severity = Severity.Low;
+					
+					// Confidence is not total because we may not be able to resolve 
+					// every base class.
+					Runner.Report (method, severity, Confidence.High);	
+				}
+			}
+									
+			return Runner.CurrentRuleResult;
+		}
+	}
+}
---
+++ gendarme/rules/Gendarme.Rules.BadPractice/Test/OnlyUseDisposeForIDisposableTypesTest.cs
@@ -0,0 +1,140 @@
+// 
+// Unit tests for OnlyUseDisposeForIDisposableTypesRule
+//
+// Authors:
+//	Jesse Jones <jesjones@mindspring.com>
+//
+// Copyright (C) 2009 Jesse Jones
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+using Mono.Cecil;
+using Gendarme.Framework;
+using Gendarme.Rules.BadPractice;
+
+using NUnit.Framework;
+using Test.Rules.Definitions;
+using Test.Rules.Fixtures;
+
+namespace Test.Rules.BadPractice {
+
+	[TestFixture]
+	public sealed class OnlyUseDisposeForIDisposableTypesTest : TypeRuleTestFixture<OnlyUseDisposeForIDisposableTypesRule> {
+
+		public class Good1 {
+			public void Reset ()
+			{
+			}
+		}
+
+		public class Good2 : IDisposable {
+			public virtual void Dispose ()
+			{
+			}
+		}
+
+		public class Good3 : Good2 {
+			public override void Dispose ()
+			{
+			}
+		}
+
+		public class Bad1 {
+			public void Dispose ()
+			{
+			}
+		}
+
+		public class Bad2 {
+			public void Dispose (int x)
+			{
+			}
+		}
+
+		public interface Bad3 {
+			void Dispose ();
+		}
+
+		internal class Bad4 {
+			public void Dispose ()
+			{
+			}
+		}
+
+		internal class Bad5 {
+			public void Close ()
+			{
+				Dispose (0);
+			}
+
+			private void Dispose (int x)
+			{
+			}
+		}
+
+		internal class Bad6 {
+			public void Dispose ()
+			{
+				Dispose (0);
+			}
+
+			private void Dispose (int x)
+			{
+			}
+		}
+
+		[Test]
+		public void DoesNotApply ()
+		{
+			AssertRuleDoesNotApply (SimpleTypes.Delegate);
+			AssertRuleDoesNotApply (SimpleTypes.Enum);
+		}
+
+		[Test]
+		public void Cases ()
+		{
+			AssertRuleSuccess<Good1> ();
+			AssertRuleSuccess<Good2> ();
+			AssertRuleSuccess<Good3> ();
+			
+			AssertRuleFailure<Bad1> ();
+			Assert.AreEqual (Severity.High, Runner.Defects [0].Severity);
+
+			AssertRuleFailure<Bad2> ();
+			Assert.AreEqual (Severity.High, Runner.Defects [0].Severity);
+
+			AssertRuleFailure<Bad3> ();
+			Assert.AreEqual (Severity.High, Runner.Defects [0].Severity);
+
+			// Bad4 has internal visibility so we consider it less severe.
+			AssertRuleFailure<Bad4> ();
+			Assert.AreEqual (Severity.Medium, Runner.Defects [0].Severity);
+
+			// Private Dispose.
+			AssertRuleFailure<Bad5> ();
+			Assert.AreEqual (Severity.Low, Runner.Defects [0].Severity);
+
+			AssertRuleFailure<Bad6> (2);
+		}
+	}
+}
