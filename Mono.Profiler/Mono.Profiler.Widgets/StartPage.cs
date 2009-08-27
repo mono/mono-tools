a3e16999735bf10cc9662ba6e2a10d98fd1036f3
Mike Kestner <mkestner@gmail.com>
2009-08-27 16:21:13 +0000

    2009-08-27  Mike Kestner <mkestner@novell.com>
    	* MainWindow.cs : use new StartPage widget. Some refactoring for
    	tabbed MDI that I decided not to utilize, but left in the event we
    	do in the future. Refactoring for Opening recent documents which the
    	StartPage and menu don't support yet. Add toolbar with new/open/save
    	items.
    
    2009-08-27  Mike Kestner <mkestner@novell.com>
    
    	* StartPage.cs: beginnings of a startup page similar to MD.
    
    
    svn path=/trunk/mono-tools/; revision=140827

---
+++ Mono.Profiler/Mono.Profiler.Widgets/StartPage.cs
@@ -0,0 +1,349 @@
+// Copyright (c) 2009  Novell, Inc.  <http://www.novell.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+using System;
+using System.Collections.Generic;
+using Mono.Profiler;
+
+namespace Mono.Profiler.Widgets {
+	
+	public delegate void StartEventHandler (object o, StartEventArgs args);
+
+	public enum StartEventType {
+		Create,
+		Open,
+		Repeat,
+	}
+
+	public class StartEventArgs : EventArgs {
+
+		StartEventType type;
+		string detail;
+
+		public StartEventArgs (StartEventType type, string detail)
+		{
+			this.type = type;
+			this.detail = detail;
+		}
+
+		public string Detail {
+			get { return detail; }
+		}
+
+		public StartEventType Type {
+			get { return type; }
+		}
+	}
+
+	public class StartPage : Gtk.DrawingArea {
+
+		const int padding = 10;
+		const int text_padding = 8;
+
+		bool pressed;
+		BannerItem banner;
+		LinkItem prelight_item;
+		LinkItem selected_item;
+		List<Item> items = new List<Item> ();
+		Pango.Layout layout;
+
+		public StartPage () 
+		{
+			CanFocus = true;
+			Events = Gdk.EventMask.ButtonPressMask | Gdk.EventMask.ButtonReleaseMask | Gdk.EventMask.ExposureMask | Gdk.EventMask.KeyPressMask | Gdk.EventMask.PointerMotionMask;
+			layout = CreatePangoLayout (String.Empty);
+		}
+
+		public event StartEventHandler Activated;
+
+		protected override bool OnButtonPressEvent (Gdk.EventButton ev)
+		{
+			if (ev.Button != 1)
+				return base.OnButtonPressEvent (ev);
+
+			Gdk.Point pt = new Gdk.Point ((int)ev.X, (int)ev.Y);
+
+			foreach (Item item in items) {
+				if (item is LinkItem && item.Bounds.Contains (pt)) {
+					// button press animation
+					pressed = true;
+					SelectItem (item as LinkItem);
+					break;
+				}
+			}
+			GrabFocus ();
+			return base.OnButtonPressEvent (ev);
+		}
+
+		protected override bool OnButtonReleaseEvent (Gdk.EventButton ev)
+		{
+			pressed = false;
+			if (selected_item == null)
+				return base.OnButtonReleaseEvent (ev);
+
+			Gdk.Point pt = new Gdk.Point ((int)ev.X, (int)ev.Y);
+
+			if (selected_item.Bounds.Contains (pt)) {
+				OnActivated ();
+				// button release animation
+			}
+
+			return base.OnButtonReleaseEvent (ev);
+		}
+
+		protected override bool OnExposeEvent (Gdk.EventExpose ev)
+		{
+			base.OnExposeEvent (ev);
+			Paint (ev);
+			return true;
+		}
+
+		protected override bool OnKeyPressEvent (Gdk.EventKey ev)
+		{
+			switch (ev.Key) {
+			case Gdk.Key.Left:
+				SelectPreviousItem ();
+				break;
+			case Gdk.Key.Right:
+				SelectNextItem ();
+				break;
+			case Gdk.Key.Up:
+				SelectPreviousItem ();
+				break;
+			case Gdk.Key.Down:
+				SelectNextItem ();
+				break;
+			case Gdk.Key.KP_Enter:
+			case Gdk.Key.ISO_Enter:
+			case Gdk.Key.Return:
+				OnActivated ();
+				break;
+			default:
+				return false;
+			}
+
+			return true;
+		}
+
+		protected override bool OnMotionNotifyEvent (Gdk.EventMotion ev)
+		{
+			SelectItem  (null);
+
+			Gdk.Point pt = new Gdk.Point ((int)ev.X, (int)ev.Y);
+
+			if (prelight_item != null) {
+				if (prelight_item.Bounds.Contains (pt))
+					return true;
+				else {
+					LinkItem item = prelight_item;
+					prelight_item = null;
+					QueueDrawArea (item.Bounds.X, item.Bounds.Y, item.Bounds.Width, item.Bounds.Height);
+				}
+			}
+
+			foreach (Item item in items) {
+				if (item is LinkItem && item.Bounds.Contains (pt)) {
+					prelight_item = item as LinkItem;
+					QueueDrawArea (item.Bounds.X, item.Bounds.Y, item.Bounds.Width, item.Bounds.Height);
+					break;
+				}
+			}
+
+			return true;
+		}
+
+		protected override void OnSizeAllocated (Gdk.Rectangle allocation)
+		{
+			base.OnSizeAllocated (allocation);
+			Layout ();
+		}
+
+		protected override void OnSizeRequested (ref Gtk.Requisition requisition)
+		{
+			requisition.Width = 350;
+			requisition.Height = 340;
+		}
+
+		abstract class Item {
+			protected StartPage owner;
+			public Gdk.Rectangle Bounds;
+
+			protected Item (StartPage owner)
+			{
+				this.owner = owner;
+			}
+
+			public abstract void Draw (Gdk.Rectangle clip);
+		}
+
+		class BannerItem : Item {
+			
+			public BannerItem (StartPage owner) : base (owner) 
+			{
+				Bounds = new Gdk.Rectangle (0, 0, owner.Allocation.Width, 65);
+			}
+
+			public override void Draw (Gdk.Rectangle clip)
+			{
+				using (Cairo.Context ctx = Gdk.CairoHelper.Create (owner.GdkWindow)) {
+					ctx.MoveTo (new Cairo.PointD (0, 0));
+					ctx.LineTo (new Cairo.PointD (Bounds.Width, 0));
+					ctx.LineTo (new Cairo.PointD (Bounds.Width, Bounds.Height));
+					ctx.LineTo (new Cairo.PointD (0, Bounds.Height));
+					ctx.ClosePath ();
+					ctx.Save ();
+					Cairo.Gradient grad = new Cairo.LinearGradient (Bounds.Width / 3, 0, Bounds.Width * 2 / 3, Bounds.Height);
+					grad.AddColorStop (0, new Cairo.Color (0.0, 0.0, 0.7, 1.0));
+					grad.AddColorStop (1, new Cairo.Color (0.0, 0.0, 1.0, 1.0));
+					ctx.Pattern = grad;
+					ctx.FillPreserve ();
+					ctx.Restore ();
+				}
+				Gdk.Pixbuf pixbuf = new Gdk.Pixbuf (null, "Monodevelop-logo.png");
+				owner.GdkWindow.DrawPixbuf (null, pixbuf, 0, 0, padding, 0, -1, -1, Gdk.RgbDither.None, 0, 0);
+				owner.layout.SetMarkup ("<b><span font_size=\"xx-large\" foreground=\"white\">Mono Visual Profiler</span></b>");
+				Pango.Rectangle ink, log;
+				owner.layout.GetPixelExtents (out ink, out log);
+				owner.GdkWindow.DrawLayout (owner.Style.TextGC (Gtk.StateType.Normal), pixbuf.Width + 2 * padding - ink.X, (65 - ink.Height) / 2 - ink.Y, owner.layout);
+			}
+		}
+
+		class LabelItem : Item {
+			string markup;
+			Gdk.Point text_offset;
+
+			public LabelItem (StartPage owner, string label, int y) : base (owner)
+			{
+				markup = "<span foreground=\"#000099\"><b><big>" + label + "</big></b></span>";
+				owner.layout.SetMarkup (markup);
+				Pango.Rectangle ink, log;
+				owner.layout.GetPixelExtents (out ink, out log);
+				Bounds = new Gdk.Rectangle (30, y, ink.Width, ink.Height);
+				text_offset = new Gdk.Point (ink.X, ink.Y);
+			}
+
+			public override void Draw (Gdk.Rectangle clip)
+			{
+				owner.layout.SetMarkup (markup);
+				owner.GdkWindow.DrawLayout (owner.Style.TextGC (Gtk.StateType.Normal), Bounds.X - text_offset.X, Bounds.Y - text_offset.Y, owner.layout);
+			}
+		}
+
+		class LinkItem : Item {
+			public StartEventType Type;
+			public string Detail;
+			string markup;
+			Gdk.Point text_offset;
+
+			public LinkItem (StartPage owner, string caption, StartEventType type, string detail, int y) : base (owner)
+			{
+				markup = "<span underline=\"single\" foreground=\"#0000FF\">" + caption + "</span>";
+				Type = type;
+				Detail = detail;
+				owner.layout.SetMarkup (markup);
+				Pango.Rectangle ink, log;
+				owner.layout.GetPixelExtents (out ink, out log);
+				Bounds = new Gdk.Rectangle (60, y, ink.Width + 2 * padding, ink.Height + 2 * padding);
+				text_offset = new Gdk.Point (ink.X, ink.Y);
+			}
+
+			public override void Draw (Gdk.Rectangle clip)
+			{
+				if (owner.selected_item == this || owner.prelight_item == this)
+					Gtk.Style.PaintBox (owner.Style, owner.GdkWindow, Gtk.StateType.Prelight, owner.pressed ? Gtk.ShadowType.Out : Gtk.ShadowType.In, clip, owner, "GtkButton", Bounds.X, Bounds.Y, Bounds.Width, Bounds.Height);
+
+				owner.layout.SetMarkup (markup);
+				owner.GdkWindow.DrawLayout (owner.Style.TextGC (Gtk.StateType.Normal), Bounds.X - text_offset.X + padding, Bounds.Y - text_offset.Y + padding, owner.layout);
+			}
+		}
+
+		void OnActivated ()
+		{
+			if (Activated != null && selected_item != null)
+				Activated (this, new StartEventArgs (selected_item.Type, selected_item.Detail));
+		}
+
+		void Refresh ()
+		{
+			Layout ();
+			QueueDraw ();
+		}
+
+		void Layout ()
+		{
+			if (banner == null)
+				banner = new BannerItem (this);
+
+			items.Clear ();
+			items.Add (banner);
+			Item item = new LabelItem (this, "Common Actions:", banner.Bounds.Bottom + 3 * text_padding);
+			items.Add (item);
+			item = new LinkItem (this, "Create New Profile", StartEventType.Create, null, item.Bounds.Bottom + text_padding);
+			items.Add (item);
+			item = new LinkItem (this, "Open Profile Log File", StartEventType.Open, null, item.Bounds.Bottom);
+			items.Add (item);
+		}
+
+		void Paint (Gdk.EventExpose ev)
+		{
+			Gtk.Style.PaintBox (Style, GdkWindow, Gtk.StateType.Normal, Gtk.ShadowType.In, ev.Area, this, null, 0, 0, Allocation.Width, Allocation.Height);
+			foreach (Item item in items)
+				if (item.Bounds.IntersectsWith (ev.Area))
+					item.Draw (ev.Area);
+		}
+
+		void SelectItem (LinkItem item)
+		{
+			if (selected_item != null)
+				QueueDrawArea (selected_item.Bounds.X, selected_item.Bounds.Y, selected_item.Bounds.Width, selected_item.Bounds.Height);
+			selected_item = item;
+			if (selected_item != null)
+				QueueDrawArea (selected_item.Bounds.X, selected_item.Bounds.Y, selected_item.Bounds.Width, selected_item.Bounds.Height);
+		}
+
+		void SelectNextItem ()
+		{
+			if (selected_item == null) {
+				SelectItem (items [2] as LinkItem); // skips Banner and Common Actions label
+				return;
+			}
+
+			int idx = items.IndexOf (selected_item);
+			while (++idx < items.Count && !(items[idx] is LinkItem));
+			if (idx < items.Count)
+				SelectItem (items[idx] as LinkItem);
+		}
+
+		void SelectPreviousItem ()
+		{
+			if (selected_item == null) {
+				SelectItem (items [2] as LinkItem); // skips Banner and Common Actions label
+				return;
+			}
+
+			int idx = items.IndexOf (selected_item);
+			while (--idx >= 2 && !(items[idx] is LinkItem));
+			if (idx >= 2)
+				SelectItem (items[idx] as LinkItem);
+		}
+	}
+}
--- Mono.Profiler/mprof-gui/MainWindow.cs
+++ Mono.Profiler/mprof-gui/MainWindow.cs
@@ -1,7 +1,26 @@
+// Copyright (c) 2009  Novell, Inc.  <http://www.novell.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
 using System;
-using System.Diagnostics;
-using System.Text;
-using Gtk;
 using Mono.Profiler;
 using Mono.Profiler.Widgets;
 using Mono.Unix;
@@ -10,34 +29,57 @@ namespace Mono.Profiler.Gui {
 
 	public class MainWindow : Gtk.Window {	
 
-		Gtk.ToggleAction logging_enabled_action;
 		Gtk.Action save_action;
 		Gtk.Action show_system_nodes_action;
+		Gtk.ToggleAction logging_enabled_action;
+		Gtk.Box content_area;
+		Gtk.Widget view;
 		ProfilerProcess proc;
-		ProfileView contents;
 		string filename;
 		
-		public MainWindow () : base (Gtk.WindowType.Toplevel)
+		public MainWindow () : base (Catalog.GetString ("Mono Visual Profiler"))
 		{
 			DefaultSize = new Gdk.Size (800, 600);
-			Title = Catalog.GetString ("Mono Visual Profiler");
-			Box box = new Gtk.VBox (false, 6);
-			box.PackStart (BuildMenubar (), false, false, 0);
-			contents = new ProfileView ();
-			box.PackStart (contents, true, true, 0);
+			Gtk.Box box = new Gtk.VBox (false, 0);
+			Gtk.UIManager uim = BuildUIManager ();
+ 			box.PackStart (uim.GetWidget ("/Menubar"), false, false, 0);
+ 			box.PackStart (uim.GetWidget ("/Toolbar"), false, false, 0);
+			content_area = new Gtk.VBox (false, 0);
+			content_area.Show ();
+			box.PackStart (content_area, true, true, 0);
+			StartPage start_page = new StartPage ();
+			start_page.Activated += OnStartPageActivated;
+			start_page.Show ();
+			View = start_page;
 			box.ShowAll ();
 			Add (box);
 		}
-	
+
+		Gtk.Widget View {
+			get { return view; }
+			set {
+				if (view != null)
+					content_area.Remove (view);
+				view = value;
+				content_area.PackStart (view, true, true, 0);
+			}
+		}
+
 		protected override bool OnDeleteEvent (Gdk.Event ev)
 		{
-			Application.Quit ();
+			Gtk.Application.Quit ();
 			return true;
 		}
 	
-		void Refresh ()
+		protected override void OnShown ()
 		{
-			Application.Invoke (delegate { 
+			base.OnShown ();
+			View.GrabFocus ();
+		}
+
+		void Refresh (ProfileView contents)
+		{
+			Gtk.Application.Invoke (delegate { 
 				if (contents.LoadProfile (proc.LogFile)) {
 					filename = proc.LogFile; 
 					save_action.Sensitive = true;
@@ -49,53 +91,82 @@ namespace Mono.Profiler.Gui {
 		void OnNewActivated (object sender, System.EventArgs e)
 		{
 			ProfileSetupDialog d = new ProfileSetupDialog (this);
-			if (d.Run () == (int) ResponseType.Accept && !String.IsNullOrEmpty (d.AssemblyPath)) {
-				logging_enabled_action.Sensitive = true;
+			if (d.Run () == (int) Gtk.ResponseType.Accept && !String.IsNullOrEmpty (d.AssemblyPath)) {
+				ProfileView view = new ProfileView ();
+				view.Show ();
+				View = view;
+				logging_enabled_action.Visible = true;
 				logging_enabled_action.Active = d.StartEnabled;
 				string args = d.Args;
 				proc = new ProfilerProcess (args, d.AssemblyPath);
-				proc.Paused += delegate { Refresh (); };
-				proc.Exited += delegate { Refresh (); };
+				proc.Paused += delegate { Refresh (view); };
+				proc.Exited += delegate { Refresh (view); logging_enabled_action.Visible = false; };
 				proc.Start ();
 			}
 			d.Destroy ();		
 		}
 
+		static object[] chooser_button_params = new object[] { Gtk.Stock.Cancel, Gtk.ResponseType.Cancel, Gtk.Stock.Open, Gtk.ResponseType.Accept };
+
+		void OpenProfile (string filename)
+		{
+			ProfileView view = new ProfileView ();
+			if (view.LoadProfile (filename)) {
+				view.Show ();
+				View = view;
+				this.filename = filename;
+				logging_enabled_action.Visible = false;
+				save_action.Sensitive = false;
+				show_system_nodes_action.Sensitive = view.SupportsFiltering;
+			}
+		}
+
 		void OnOpenActivated (object sender, System.EventArgs e)
 		{
-			FileChooserDialog d = new FileChooserDialog ("Open Profile Log", this, FileChooserAction.Open, Stock.Cancel, ResponseType.Cancel, Stock.Open, ResponseType.Accept);
-			if (d.Run () == (int) ResponseType.Accept && contents.LoadProfile (d.Filename)) {
-				filename = d.Filename;
-				logging_enabled_action.Active = false;
-				logging_enabled_action.Sensitive = false;
-				save_action.Sensitive = false;
-				show_system_nodes_action.Sensitive = contents.SupportsFiltering;
-			}
+			Gtk.FileChooserDialog d = new Gtk.FileChooserDialog ("Open Profile Log", this, Gtk.FileChooserAction.Open, chooser_button_params);
+			if (d.Run () == (int) Gtk.ResponseType.Accept)
+				OpenProfile (d.Filename);
 			d.Destroy ();
 		}
 	
 		void OnQuitActivated (object sender, System.EventArgs e)
 		{
-			Application.Quit ();
+			Gtk.Application.Quit ();
 		}
 	
 		void OnSaveAsActivated (object sender, System.EventArgs e)
 		{
-			FileChooserDialog d = new FileChooserDialog ("Save Profile Log", this, FileChooserAction.Save, Stock.Cancel, ResponseType.Cancel, Stock.Save, ResponseType.Accept);
-			if (d.Run () == (int) ResponseType.Accept) {
+			Gtk.FileChooserDialog d = new Gtk.FileChooserDialog ("Save Profile Log", this, Gtk.FileChooserAction.Save, chooser_button_params);
+			if (d.Run () == (int) Gtk.ResponseType.Accept) {
 				System.IO.File.Move (filename, d.Filename);
 				filename = d.Filename;
 				save_action.Sensitive = false;
-				show_system_nodes_action.Sensitive = contents.SupportsFiltering;
 			}
 			d.Destroy ();
+		}
+
+		void OnStartPageActivated (object o, StartEventArgs args)
+		{
+			switch (args.Type) {
+			case StartEventType.Create:
+				OnNewActivated (null, null);
+				break;
+			case StartEventType.Open:
+				if (args.Detail == null)
+					OnOpenActivated (null, null);
+				else
+					OpenProfile (args.Detail);
+				break;
+			default:
+				throw new NotSupportedException ();
+			}
 		}
 
 		void OnLoggingActivated (object sender, System.EventArgs e)
 		{
 			if (proc == null)
 				return;
-			ToggleAction ta = sender as ToggleAction;
+			Gtk.ToggleAction ta = sender as Gtk.ToggleAction;
 			if (ta.Active)
 				proc.Resume ();
 			else
@@ -104,8 +175,8 @@ namespace Mono.Profiler.Gui {
 		
 		void OnShowSystemNodesActivated (object sender, System.EventArgs e)
 		{
-			ToggleAction ta = sender as ToggleAction;
-			contents.Options.ShowSystemNodes = ta.Active;
+			Gtk.ToggleAction ta = sender as Gtk.ToggleAction;
+			(View as ProfileView).Options.ShowSystemNodes = ta.Active;
 		}
 		
 		const string ui_info = 
@@ -124,37 +195,43 @@ namespace Mono.Profiler.Gui {
 			"      <menuitem action='ShowSystemNodesAction'/>" +
 			"    </menu>" +
 			"  </menubar>" +
+			"  <toolbar name='Toolbar'>" +
+			"    <toolitem action='NewAction'/>" +
+			"    <toolitem action='OpenAction'/>" +
+			"    <toolitem action='SaveAsAction'/>" +
+			"  </toolbar>" +
 			"</ui>";
 
-		Widget BuildMenubar ()
+		Gtk.UIManager BuildUIManager ()
 		{
-			ActionEntry[] actions = new ActionEntry[] {
-				new ActionEntry ("ProfileMenu", null, Catalog.GetString ("_Profile"), null, null, null),
-				new ActionEntry ("NewAction", Stock.New, null, "<control>N", Catalog.GetString ("Create New Profile"), new EventHandler (OnNewActivated)),
-				new ActionEntry ("OpenAction", Stock.Open, null, "<control>O", Catalog.GetString ("Open Existing Profile Log"), new EventHandler (OnOpenActivated)),
-				new ActionEntry ("SaveAsAction", Stock.SaveAs, null, "<control>S", Catalog.GetString ("Save Profile Data"), new EventHandler (OnSaveAsActivated)),
-				new ActionEntry ("QuitAction", Stock.Quit, null, "<control>Q", Catalog.GetString ("Quit Profiler"), new EventHandler (OnQuitActivated)),
-				new ActionEntry ("RunMenu", null, Catalog.GetString ("_Run"), null, null, null),
-				new ActionEntry ("ViewMenu", null, Catalog.GetString ("_View"), null, null, null),
+			Gtk.ActionEntry[] actions = new Gtk.ActionEntry[] {
+				new Gtk.ActionEntry ("ProfileMenu", null, Catalog.GetString ("_Profile"), null, null, null),
+				new Gtk.ActionEntry ("NewAction", Gtk.Stock.New, null, "<control>N", Catalog.GetString ("Create New Profile"), new EventHandler (OnNewActivated)),
+				new Gtk.ActionEntry ("OpenAction", Gtk.Stock.Open, null, "<control>O", Catalog.GetString ("Open Existing Profile Log"), new EventHandler (OnOpenActivated)),
+				new Gtk.ActionEntry ("SaveAsAction", Gtk.Stock.SaveAs, null, "<control>S", Catalog.GetString ("Save Profile Data"), new EventHandler (OnSaveAsActivated)),
+				new Gtk.ActionEntry ("QuitAction", Gtk.Stock.Quit, null, "<control>Q", Catalog.GetString ("Quit Profiler"), new EventHandler (OnQuitActivated)),
+				new Gtk.ActionEntry ("RunMenu", null, Catalog.GetString ("_Run"), null, null, null),
+				new Gtk.ActionEntry ("ViewMenu", null, Catalog.GetString ("_View"), null, null, null),
 			};
 
-			ToggleActionEntry[] toggle_actions = new ToggleActionEntry[] {
-				new ToggleActionEntry ("ShowSystemNodesAction", null, Catalog.GetString ("_Show system nodes"), null, Catalog.GetString ("Shows internal nodes of system library method invocations"), new EventHandler (OnShowSystemNodesActivated), false),
-				new ToggleActionEntry ("LogEnabledAction", null, Catalog.GetString ("_Logging enabled"), null, Catalog.GetString ("Profile logging enabled"), new EventHandler (OnLoggingActivated), true),
+			Gtk.ToggleActionEntry[] toggle_actions = new Gtk.ToggleActionEntry[] {
+				new Gtk.ToggleActionEntry ("ShowSystemNodesAction", null, Catalog.GetString ("_Show system nodes"), null, Catalog.GetString ("Shows internal nodes of system library method invocations"), new EventHandler (OnShowSystemNodesActivated), false),
+				new Gtk.ToggleActionEntry ("LogEnabledAction", null, Catalog.GetString ("_Logging enabled"), null, Catalog.GetString ("Profile logging enabled"), new EventHandler (OnLoggingActivated), true),
 			};
-	    		ActionGroup group = new ActionGroup ("group");
+	    		Gtk.ActionGroup group = new Gtk.ActionGroup ("group");
 			group.Add (actions);
 			group.Add (toggle_actions);
-	    		UIManager uim = new UIManager ();
+	    		Gtk.UIManager uim = new Gtk.UIManager ();
  
 	    		uim.InsertActionGroup (group, (int) uim.NewMergeId ());
 	    		uim.AddUiFromString (ui_info);
 			AddAccelGroup (uim.AccelGroup);
-			logging_enabled_action = group.GetAction ("LogEnabledAction") as ToggleAction;
+			logging_enabled_action = group.GetAction ("LogEnabledAction") as Gtk.ToggleAction;
+			logging_enabled_action.Visible = false;
 			save_action = group.GetAction ("SaveAsAction");
 			save_action.Sensitive = false;
 			show_system_nodes_action = group.GetAction ("ShowSystemNodesAction");
- 			return uim.GetWidget ("/Menubar");
+ 			return uim;
 		}
 	}
 }
