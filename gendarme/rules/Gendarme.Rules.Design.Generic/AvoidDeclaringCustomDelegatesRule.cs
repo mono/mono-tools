3ea1876c5748d1f4fbdb84eeb4dee72825f5aea9
Sebastien Pouliot <sebastien@ximian.com>
2010-01-01 21:20:15 +0000

    In Test: 2010-01-01  Sebastien Pouliot  <sebastien@ximian.com>
    	* AvoidDeclaringCustomDelegatesTest.cs: New. Unit tests for
    	AvoidDeclaringCustomDelegatesRule
    
    In .:
    2010-01-01  Sebastien Pouliot  <sebastien@ximian.com>
    
    	* AvoidDeclaringCustomDelegatesRule.cs: New. Rule to check
    	if custom delegates could be replaced with Action/Func for
    	.net 2.0 and later
    	* Makefile.am: Add new rule and tests to the build
    
    
    svn path=/trunk/mono-tools/; revision=148964

---
+++ gendarme/rules/Gendarme.Rules.Design.Generic/AvoidDeclaringCustomDelegatesRule.cs
@@ -0,0 +1,179 @@
+//
+// Gendarme.Rules.Design.Generic.AvoidDeclaringCustomDelegatesRule
+//
+// Authors:
+//	Sebastien Pouliot <sebastien@ximian.com>
+//
+// Copyright (C) 2010 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+
+using Mono.Cecil;
+
+using Gendarme.Framework;
+using Gendarme.Framework.Helpers;
+using Gendarme.Framework.Rocks;
+
+namespace Gendarme.Rules.Design.Generic {
+
+	/// <summary>
+	/// This rule will fire if custom delegates are defined when either pre-defined
+	/// <code>Action</code>, <code>Action&lt;T[,...]&gt;</code> or <code>Func&lt;[Tx,...]TResult&gt;</code>
+	/// could have been used. This rule applies only to code using the framework version 2.0 (or later).
+	/// </summary>
+	/// <example>
+	/// Bad example (without return value):
+	/// <code>
+	/// delegate void MyCustomDelegate (int a);
+	/// private MyCustomDelegate custom_delegate;
+	/// </code>
+	/// </example>
+	/// <example>
+	/// Good example (without return value):
+	/// <code>
+	/// private Action&lt;int&gt; action_delegate;
+	/// </code>
+	/// </example>
+	/// <example>
+	/// Bad example (with return value):
+	/// <code>
+	/// delegate int MyCustomDelegate (int a, string s);
+	/// private MyCustomDelegate custom_delegate;
+	/// </code>
+	/// </example>
+	/// <example>
+	/// Good example (with return value):
+	/// <code>
+	/// private Func&lt;int,string,int&gt; func_delegate;
+	/// </code>
+	/// </example>
+	/// <remarks>This rule is available since Gendarme 2.8</remarks>
+	[Problem ("This delegate could be replaced with an existing framework delegate.")]
+	[Solution ("Prefer the use of Action, Action<T...> and Func<...,TResult> types.")]
+	public class AvoidDeclaringCustomDelegatesRule : Rule, ITypeRule {
+
+		private int MaxParameter = 4; // NET_2_0
+
+		public override void Initialize (IRunner runner)
+		{
+			base.Initialize (runner);
+
+			// we only want to run this on assemblies that use 2.0 or later
+			// since generics were not available before
+			Runner.AnalyzeAssembly += delegate (object o, RunnerEventArgs e) {
+				TargetRuntime runtime = e.CurrentAssembly.Runtime;
+				if (runtime >= TargetRuntime.NET_4_0) {
+					MaxParameter = 16;
+					Active = true;
+				} else if (runtime >= TargetRuntime.NET_2_0) {
+					MaxParameter = 4;
+					Active = true;
+				} else {
+					Active = false;
+				}
+			};
+		}
+
+		static string[] ActionMessage = {
+			"Replace with Action()",
+			"Replace with Action<T>",
+			"Replace with Action<T1,T2>",
+			"Replace with Action<T1,T2,T3>",
+			"Replace with Action<T1,T2,T3,T4>",
+			// NET_4_0
+			"Replace with Action<T1,T2,T3,T4,T5>",
+			"Replace with Action<T1,T2,T3,T4,T5,T6>",
+			"Replace with Action<T1,T2,T3,T4,T5,T6,T7>",
+			"Replace with Action<T1,T2,T3,T4,T5,T6,T7,T8>",
+			"Replace with Action<T1,T2,T3,T4,T5,T6,T7,T8,T9>",
+			"Replace with Action<T1,T2,T3,T4,T5,T6,T7,T8,T9,T10>",
+			"Replace with Action<T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11>",
+			"Replace with Action<T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12>",
+			"Replace with Action<T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13>",
+			"Replace with Action<T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13,T14>",
+			"Replace with Action<T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13,T14,T15>",
+			"Replace with Action<T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13,T14,T15,T16>",
+		};
+
+		static string [] FuncMessage = {
+			"Replace with Func<TResult>",
+			"Replace with Func<T,TResult>",
+			"Replace with Func<T1,T2,TResult>",
+			"Replace with Func<T1,T2,T3,TResult>",
+			"Replace with Func<T1,T2,T3,T4,TResult>",
+			// NET_4_0
+			"Replace with Func<T1,T2,T3,T4,T5,TResult>",
+			"Replace with Func<T1,T2,T3,T4,T5,T6,TResult>",
+			"Replace with Func<T1,T2,T3,T4,T5,T6,T7,TResult>",
+			"Replace with Func<T1,T2,T3,T4,T5,T6,T7,T8,TResult>",
+			"Replace with Func<T1,T2,T3,T4,T5,T6,T7,T8,T9,TResult>",
+			"Replace with Func<T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,TResult>",
+			"Replace with Func<T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,TResult>",
+			"Replace with Func<T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,TResult>",
+			"Replace with Func<T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13,TResult>",
+			"Replace with Func<T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13,T14,TResult>",
+			"Replace with Func<T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13,T14,T15,TResult>",
+			"Replace with Func<T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13,T14,T15,T16,TResult>",
+		};
+
+		public RuleResult CheckType (TypeDefinition type)
+		{
+			// rule only apply to (non generated) delegates
+			if (!type.IsDelegate () || type.IsGeneratedCode ())
+				return RuleResult.DoesNotApply;
+
+			MethodDefinition invoke = type.GetMethod ("Invoke");
+			// this happens for System.MulticastDelegate
+			if (invoke == null)
+				return RuleResult.DoesNotApply;
+
+			int n = 0;
+			Severity severity = Severity.Medium;
+			bool use_structure = false;
+			// check parameters for 'ref', 'out', 'params'
+			if (invoke.HasParameters) {
+				n = invoke.Parameters.Count;
+				// too many parameters to directly use Action/Func
+				// so we lower severity and suggest grouping them
+				if (n > MaxParameter) {
+					severity = Severity.Low;
+					n = 1;
+					use_structure = true;
+				}
+
+				// special cases not directly usable with Action/Func
+				foreach (ParameterDefinition pd in invoke.Parameters) {
+					if (pd.IsOut || pd.IsRef () || pd.IsParams ())
+						return RuleResult.Success;
+				}
+			}
+
+			string msg = (invoke.ReturnType.ReturnType.FullName == "System.Void") ? ActionMessage [n] : FuncMessage [n];
+			if (use_structure)
+				msg += " and use a structure to hold all your parameters into <T>.";
+			Runner.Report (type, severity, Confidence.High, msg);
+			return Runner.CurrentRuleResult;
+		}
+	}
+}
+
---
+++ gendarme/rules/Gendarme.Rules.Design.Generic/Test/AvoidDeclaringCustomDelegatesTest.cs
@@ -0,0 +1,129 @@
+//
+// Unit tests for AvoidDeclaringCustomDelegatesRule
+//
+// Authors:
+//	Sebastien Pouliot <sebastien@ximian.com>
+//
+// Copyright (C) 2010 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using Mono.Cecil;
+
+using Gendarme.Rules.Design.Generic;
+
+using NUnit.Framework;
+using Test.Rules.Definitions;
+using Test.Rules.Fixtures;
+using Test.Rules.Helpers;
+
+namespace Test.Rules.Design.Generic {
+
+	[TestFixture]
+	public class AvoidDeclaringCustomDelegatesTest : TypeRuleTestFixture<AvoidDeclaringCustomDelegatesRule> {
+
+		[Test]
+		public void NotApplicableBefore2_0 ()
+		{
+			// ensure that the rule does not apply for types defined in 1.x assemblies
+			TypeDefinition violator = DefinitionLoader.GetTypeDefinition<AvoidDeclaringCustomDelegatesRule> ();
+			TargetRuntime realRuntime = violator.Module.Assembly.Runtime;
+			try {
+
+				// fake assembly runtime version and do the check
+				violator.Module.Assembly.Runtime = TargetRuntime.NET_1_1;
+				Rule.Active = true;
+				Rule.Initialize (Runner);
+				Assert.IsFalse (Rule.Active, "Active");
+			}
+			catch {
+				// rollback
+				violator.Module.Assembly.Runtime = realRuntime;
+				Rule.Active = true;
+			}
+		}
+
+		[Test]
+		public void NotApplicable ()
+		{
+			AssertRuleDoesNotApply (SimpleTypes.Class);
+			AssertRuleDoesNotApply (SimpleTypes.Enum);
+			AssertRuleDoesNotApply (SimpleTypes.Interface);
+			AssertRuleDoesNotApply (SimpleTypes.Structure);
+		}
+
+		delegate void VoidNoParameter ();
+		delegate void VoidOneParameter (int a);
+		delegate void VoidTwoParameters (int a, string b);
+		delegate void VoidThreeParameters (int a, string b, object c);
+		delegate void VoidFourParameters (int a, string b, object c, float d);
+		delegate void Void17Parameters (int a, string b, object c, float d, double e, short f, byte g, long h,
+			int i, string j, object k, float l, double m, short n, byte o, long p, DateTime q);
+
+		[Test]
+		public void ReplaceWithAction ()
+		{
+			AssertRuleFailure<VoidNoParameter> ();
+			AssertRuleFailure<VoidOneParameter> ();
+			AssertRuleFailure<VoidTwoParameters> ();
+			AssertRuleFailure<VoidThreeParameters> ();
+			AssertRuleFailure<VoidFourParameters> ();
+			// we don't test 5-16 since they will be valid for NET_4_0
+			// having more than 16 parameters is another issue (unrelated to this rule)
+			AssertRuleFailure<Void17Parameters> ();
+		}
+
+		delegate int NonVoidNoParameter ();
+		delegate int NonVoidOneParameter (int a);
+		delegate int NonVoidTwoParameters (int a, string b);
+		delegate int NonVoidThreeParameters (int a, string b, object c);
+		delegate int NonVoidFourParameters (int a, string b, object c, float d);
+		delegate long NonVoid17Parameters (int a, string b, object c, float d, double e, short f, byte g, long h,
+			int i, string j, object k, float l, double m, short n, byte o, long p, DateTime q);
+
+		[Test]
+		public void ReplaceWithFunc ()
+		{
+			AssertRuleFailure<NonVoidNoParameter> ();
+			AssertRuleFailure<NonVoidOneParameter> ();
+			AssertRuleFailure<NonVoidTwoParameters> ();
+			AssertRuleFailure<NonVoidThreeParameters> ();
+			AssertRuleFailure<NonVoidFourParameters> ();
+			// we don't test 5-16 since they will be valid for NET_4_0
+			// having more than 16 parameters is another issue (unrelated to this rule)
+			AssertRuleFailure<NonVoid17Parameters> ();
+		}
+
+		delegate int RefDelegate (ref int a);
+		delegate void OutDelegate (out int a);
+		delegate void ParamsDelegate (params object[] args);
+
+		[Test]
+		public void SpecialCases ()
+		{
+			AssertRuleSuccess<RefDelegate> ();
+			AssertRuleSuccess<OutDelegate> ();
+			AssertRuleSuccess<ParamsDelegate> ();
+		}
+	}
+}
+
