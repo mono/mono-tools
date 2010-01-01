f8d2fe39e2a3bc5ad3e236b122312f46ce5ce59c
Sebastien Pouliot <sebastien@ximian.com>
2010-01-01 20:38:23 +0000

    In Test/Gendarme.Framework.Rocks: 2010-01-01  Sebastien Pouliot  <sebastien@ximian.com>
    	* ParametersRocksTest.cs: New. Test cases for new rocks
    
    In Gendarme.Framework.Rocks:
    2010-01-01  Sebastien Pouliot  <sebastien@ximian.com>
    
    	* ParameterRocks.cs: New. IsRef and IsParams rocks to use like
    	IsOut property.
    
    
    svn path=/trunk/mono-tools/; revision=148960

---
+++ gendarme/framework/Gendarme.Framework.Rocks/ParametersRocks.cs
@@ -0,0 +1,61 @@
+//
+// Gendarme.Framework.Rocks.ParameterRocks
+//
+// Authors:
+//	Sebastien Pouliot  <sebastien@ximian.com>
+//
+// Copyright (C) 2010 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+
+using Mono.Cecil;
+
+namespace Gendarme.Framework.Rocks {
+
+	public static class ParameterRocks {
+
+		/// <summary>
+		/// Check if the parameter represents a list of parameters (<c>params</c> keyword in C#)
+		/// </summary>
+		/// <param name="self">The ParameterDefinition on which the extension method can be called.</param>
+		/// <returns>True if the parameter represents a list of parameters, false otherwise.</returns>
+		public static bool IsParams (this ParameterDefinition self)
+		{
+			if (self == null || !self.HasCustomAttributes)
+				return false;
+			return self.CustomAttributes.ContainsType ("System.ParamArrayAttribute");
+		}
+
+		/// <summary>
+		/// Check if the parameter is passed by reference (<c>ref</c> keyword in C#)
+		/// </summary>
+		/// <param name="self">The ParameterReference on which the extension method can be called.</param>
+		/// <returns>True if the parameter is passed by reference, false otherwise.</returns>
+		public static bool IsRef (this ParameterReference self)
+		{
+			if (self == null)
+				return false;
+			string name = self.ParameterType.Name;
+			return (name [name.Length - 1] == '&');
+		}
+	}
+}
+
---
+++ gendarme/framework/Test/Gendarme.Framework.Rocks/ParametersRocksTest.cs
@@ -0,0 +1,82 @@
+// 
+// Unit tests for ParameterRocksTest
+//
+// Authors:
+//	Sebastien Pouliot  <sebastien@ximian.com>
+//
+// Copyright (C) 2010 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Reflection;
+
+using Gendarme.Framework.Rocks;
+
+using Mono.Cecil;
+using NUnit.Framework;
+
+namespace Test.Framework.Rocks {
+
+	[TestFixture]
+	public class ParameterRocksTest {
+
+		private AssemblyDefinition assembly;
+		private TypeDefinition type;
+
+		[TestFixtureSetUp]
+		public void FixtureSetUp ()
+		{
+			string unit = Assembly.GetExecutingAssembly ().Location;
+			assembly = AssemblyFactory.GetAssembly (unit);
+			type = assembly.MainModule.Types ["Test.Framework.Rocks.ParameterRocksTest"];
+		}
+
+		public void UseParams (int a, params int [] args)
+		{
+		}
+
+		[Test]
+		public void IsParams ()
+		{
+			ParameterDefinition pd = null;
+			Assert.IsFalse (pd.IsParams (), "null");
+
+			MethodDefinition md = type.GetMethod ("UseParams");
+			Assert.IsFalse (md.Parameters [0].IsParams (), "0");
+			Assert.IsTrue (md.Parameters [1].IsParams (), "1");
+		}
+
+		public void UseRef (int a, ref int b)
+		{
+		}
+
+		[Test]
+		public void IsRef ()
+		{
+			ParameterReference pr = null;
+			Assert.IsFalse (pr.IsRef (), "null");
+
+			MethodDefinition md = type.GetMethod ("UseRef");
+			Assert.IsFalse (md.Parameters [0].IsRef (), "0");
+			Assert.IsTrue (md.Parameters [1].IsRef (), "1");
+		}
+	}
+}
+
