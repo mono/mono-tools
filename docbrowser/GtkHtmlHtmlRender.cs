b5c1fd421f04d28bb9e8843d86cc8a2d8706da3f
Miguel de Icaza <miguel@gnome.org>
2010-01-19 21:33:03 +0000

    Fix
    svn path=/trunk/mono-tools/; revision=149849

--- docbrowser/GtkHtmlHtmlRender.cs
+++ docbrowser/GtkHtmlHtmlRender.cs
@@ -7,7 +7,6 @@
 //
 using System;
 using Gtk;
-using Gnome;
 using System.IO;
 using System.Reflection;
 
@@ -102,13 +101,14 @@ class GtkHtmlHtmlRender : IHtmlRender {
 	}
 	
 	public void Print (string Html) {
-		
+#ifndef MACOS
 		if (Html == null) {
 			Console.WriteLine ("empty print");
 			return;
 		}
 
-		PrintManager.Print (Html);
+		Gnome.PrintManager.Print (Html);
+#endif
 	}
 
 	public bool Initialize ()
