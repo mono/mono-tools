f9f75d10bd043d6cd3f9b20f9f4ca2d8dc97a371
Sebastien Pouliot <sebastien@ximian.com>
2010-02-01 01:45:08 +0000

    In Test: 2010-01-31  Sebastien Pouliot  <sebastien@ximian.com>
    	* RemoveDependenceOnObsoleteCodeTest.cs: New. Unit tests for
    	RemoveDependenceOnObsoleteCodeRule
    
    In .:
    2010-01-31  Sebastien Pouliot  <sebastien@ximian.com>
    
    	* Makefile.am: Add new rule and tests to the build
    	* RemoveDependenceOnObsoleteCodeRule.cs: New. Rule to warn you if
    	your code depends on code decorated with the [Obsolete] attribute
    
    
    svn path=/trunk/mono-tools/; revision=150611

---
+++ gendarme/rules/Gendarme.Rules.Maintainability/RemoveDependenceOnObsoleteCodeRule.cs
@@ -0,0 +1,373 @@
+//
+// Gendarme.Rules.Maintainability.RemoveDependenceOnObsoleteCodeRule
+//
+// Authors:
+//	Sebastien Pouliot <sebastien@ximian.com>
+//
+// Copyright (C) 2010 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Collections.Generic;
+
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+using Gendarme.Framework;
+using Gendarme.Framework.Engines;
+using Gendarme.Framework.Rocks;
+
+namespace Gendarme.Rules.Maintainability {
+
+	/// <summary>
+	/// This rule will warn you if your code depends on (e.g. inherit, implement, call...)
+	/// code that is decorated with the <c>[Obsolete]</c> attribute.
+	/// Note that the rule does not report <c>[Obsolete]</c> types, methods...
+	/// but only their use by your code.
+	/// </summary>
+	/// <example>
+	/// Bad example:
+	/// <code>
+	/// [Obsolete ("Limited to In32.MaxValue, use new Int64 ReportAll method")]
+	/// abstract int Report (IList list);
+	/// 
+	/// abstract long ReportAll (IList list);
+	/// 
+	/// public int GetCount ()
+	/// {
+	///	// this method is not ok since it use an obsolete method
+	///	return Report (list);
+	/// }
+	/// </code>
+	/// </example>
+	/// <example>
+	/// Good example (dependency removed):
+	/// <code>
+	/// [Obsolete ("Limited to In32.MaxValue, use new Int64 ReportAll method")]
+	/// abstract int Report (IList list);
+	/// 
+	/// abstract long ReportAll (IList list);
+	/// 
+	/// // this method is correct but this changed the public API
+	/// public long GetCount ()
+	/// {
+	///	return ReportAll (list);
+	/// }
+	/// </code>
+	/// </example>
+	/// <example>
+	/// Good example (decorated as [Obsolete]):
+	/// <code>
+	/// [Obsolete ("Limited to In32.MaxValue, use new Int64 ReportAll method")]
+	/// abstract int Report (IList list);
+	/// 
+	/// abstract long ReportAll (IList list);
+	/// 
+	/// [Obsolete ("Limited to In32.MaxValue, use new Int64 GetLongCount method")]
+	/// public int GetCount ()
+	/// {
+	///	// this method is now correct since it is decorated with [Obsolete]
+	///	return Report (list);
+	/// }
+	///
+	/// public long GetLongCount ()
+	/// {
+	///	return ReportAll (list);
+	/// }
+	/// </code>
+	/// </example>
+	/// <remarks>This rule is available since Gendarme 2.8</remarks>
+	[Problem ("This type or method is not obsolete but depends on code decorated with [Obsolete] attribute.")]
+	[Solution ("Remove the dependence on obsolete code or decorate this code with the [Obsolete] attribute.")]
+	[EngineDependency (typeof (OpCodeEngine))]
+	public class RemoveDependenceOnObsoleteCodeRule : Rule, ITypeRule, IMethodRule {
+
+		const string Obsolete = "System.ObsoleteAttribute";
+
+		static Dictionary<TypeReference, bool> types = new Dictionary<TypeReference, bool> ();
+		static Dictionary<MethodReference, bool> methods = new Dictionary<MethodReference, bool> ();
+		static Dictionary<FieldReference, bool> fields = new Dictionary<FieldReference, bool> ();
+
+		static bool IsObsolete (TypeReference type)
+		{
+			if (type == null)
+				return false;
+
+			bool obsolete = false;
+			if (!types.TryGetValue (type, out obsolete)) {
+				obsolete = type.HasAttribute (Obsolete);
+				types.Add (type, obsolete);
+			}
+			return obsolete;
+		}
+
+		static bool IsObsolete (MethodReference method)
+		{
+			if (method == null)
+				return false;
+
+			bool obsolete = false;
+			if (!methods.TryGetValue (method, out obsolete)) {
+				MethodDefinition md = method.Resolve ();
+				obsolete = (md == null) ? false : md.HasAttribute (Obsolete);
+				methods.Add (method, obsolete);
+			}
+			return obsolete;
+		}
+
+		static bool IsObsolete (FieldReference field)
+		{
+			if (field == null)
+				return false;
+
+			bool obsolete = false;
+			if (!fields.TryGetValue (field, out obsolete)) {
+				FieldDefinition fd = field.Resolve ();
+				obsolete = (fd == null) ? false : fd.HasAttribute (Obsolete);
+				fields.Add (field, obsolete);
+			}
+			return obsolete;
+		}
+
+		void CheckBaseType (TypeDefinition type)
+		{
+			if (!IsObsolete (type.BaseType))
+				return;
+
+			string msg = String.Format ("Inherit from obsolete type '{0}'.", type.BaseType);
+			Runner.Report (type, type.IsVisible () ? Severity.High : Severity.Medium, Confidence.Total, msg);
+		}
+
+		void CheckInterfaces (TypeDefinition type)
+		{
+			foreach (TypeReference intf in type.Interfaces) {
+				if (IsObsolete (intf)) {
+					string msg = String.Format ("Implement obsolete interface '{0}'.", intf);
+					Runner.Report (type, type.IsVisible () ? Severity.Medium : Severity.Low, Confidence.Total, msg);
+				}
+			}
+		}
+
+		void CheckFields (TypeDefinition type)
+		{
+			foreach (FieldDefinition field in type.Fields) {
+				if (IsObsolete (field.FieldType)) {
+					string msg = String.Format ("Field type '{0}' is obsolete.", field.FieldType);
+					Runner.Report (field, field.IsVisible () ? Severity.Medium : Severity.Low, Confidence.Total, msg);
+				}
+			}
+		}
+
+		// check the properties (not the individual getter/setters)
+		void CheckProperties (TypeDefinition type)
+		{
+			foreach (PropertyDefinition property in type.Properties) {
+				if (IsObsolete (property.PropertyType)) {
+					string msg = String.Format ("Property type '{0}' is obsolete.", property.PropertyType);
+					bool visible = property.GetMethod.IsVisible () || property.SetMethod.IsVisible ();
+					Runner.Report (property, visible ? Severity.High : Severity.Medium, Confidence.Total, msg);
+				}
+			}
+		}
+
+		void CheckEvents (TypeDefinition type)
+		{
+			foreach (EventDefinition evnt in type.Events) {
+				if (IsObsolete (evnt.EventType)) {
+					string msg = String.Format ("Event type '{0}' is obsolete.", evnt.EventType);
+					bool visible = (evnt.AddMethod != null && evnt.AddMethod.IsVisible ()) ||
+						(evnt.RemoveMethod != null && evnt.RemoveMethod.IsVisible ()) ||
+						(evnt.InvokeMethod != null && evnt.InvokeMethod.IsVisible ());
+					Runner.Report (evnt, visible ? Severity.High : Severity.Medium, Confidence.Total, msg);
+				}
+			}
+		}
+
+		// Type			Visible		Non-Visible
+		// ------------------------------------------------
+		// BaseType*		High		Medium
+		// Interfaces*		Medium		Low
+		// Fields		Medium		Low
+		// Properties		High		Medium
+		// Events		High		Medium
+		// * type visibility
+
+		public RuleResult CheckType (TypeDefinition type)
+		{
+			// we're not interested in the details of [Obsolete] types
+			if (type.HasAttribute (Obsolete))
+				return RuleResult.DoesNotApply;
+
+			// check if we inherit from an [Obsolete] class / struct / enum
+			CheckBaseType (type);
+
+			// check if we implement an [Obsolete] interface
+			if (type.HasInterfaces)
+				CheckInterfaces (type);
+
+			// check fields types
+			if (type.HasFields)
+				CheckFields (type);
+
+			// check properties (not the getter / setter)
+			if (type.HasProperties)
+				CheckProperties (type);
+
+			// check events (not add / remove / invoke)
+			if (type.HasEvents)
+				CheckEvents (type);
+
+			return Runner.CurrentRuleResult;
+		}
+
+		void CheckParameters (MethodDefinition method)
+		{
+			foreach (ParameterDefinition p in method.Parameters) {
+				if (IsObsolete (p.ParameterType)) {
+					string msg = String.Format ("Parameter type '{0}' is obsolete.", p.ParameterType);
+					Runner.Report (p, method.IsVisible () ? Severity.High : Severity.Medium, Confidence.Total, msg);
+				}
+			}
+		}
+
+		void CheckReturnType (MethodDefinition method)
+		{
+			if (!IsObsolete (method.ReturnType.ReturnType))
+				return;
+
+			string msg = String.Format ("Return type '{0}' is obsolete.", method.ReturnType.ReturnType);
+			Runner.Report (method, method.IsVisible () ? Severity.High : Severity.Medium, Confidence.Total, msg);
+		}
+
+		void CheckVariables (MethodDefinition method)
+		{
+			foreach (VariableDefinition v in method.Body.Variables) {
+				if (IsObsolete (v.VariableType)) {
+					string msg = String.Format ("Variable type '{0}' is obsolete.", v.VariableType);
+					Runner.Report (method, Severity.Low, Confidence.High, msg);
+				}
+			}
+		}
+
+		void CheckTypeCreation (MethodDefinition method, Instruction ins, TypeReference type)
+		{
+			if (!IsObsolete (type))
+				return;
+
+			string msg = String.Format ("Type '{0}' is obsolete.", type);
+			Severity severity = type.IsVisible () ? Severity.Medium : Severity.Low;
+			Runner.Report (method, ins, severity, Confidence.High, msg);
+		}
+
+		void CheckMethodCall (MethodDefinition method, Instruction ins, MethodReference call)
+		{
+			if (call == null)
+				return;
+
+			string msg = null;
+			if (IsObsolete (call)) {
+				msg = String.Format ("Method '{0}' is obsolete.", call);
+			} else if (IsObsolete (call.DeclaringType)) {
+				msg = String.Format ("Type '{0}' is obsolete.", call.DeclaringType);
+			} else {
+				return;
+			}
+
+			Severity severity = call.IsVisible () ? Severity.Medium : Severity.Low;
+			Runner.Report (method, ins, severity, Confidence.High, msg);
+		}
+
+		void CheckFieldAccess (MethodDefinition method, Instruction ins, FieldReference field)
+		{
+			if (field == null)
+				return;
+
+			string msg = null;
+			if (IsObsolete (field)) {
+				msg = String.Format ("Field '{0}' is obsolete.", field.Name);
+			} else if (IsObsolete (field.DeclaringType)) {
+				msg = String.Format ("Field type '{0}' is obsolete.", field.FieldType);
+			} else {
+				return;
+			}
+
+			Severity severity = field.IsVisible () ? Severity.Medium : Severity.Low;
+			Runner.Report (method, ins, severity, Confidence.High, msg);
+		}
+
+		// Method		Visible		Non-Visible
+		// ------------------------------------------------
+		// Parameters		High		Medium
+		// ReturnType		High		Medium
+		// Variables		Low		Low
+		// Method calls		Medium*		Low*
+		// Fields access	Medium*		Low*
+		// * target visibility
+
+		public RuleResult CheckMethod (MethodDefinition method)
+		{
+			// [Obsolete] cannot be applied to property or event accessors
+			if (method.IsProperty () || method.IsAddOn || method.IsRemoveOn || method.IsFire)
+				return RuleResult.DoesNotApply;
+
+			if (method.HasAttribute (Obsolete))
+				return RuleResult.DoesNotApply;
+
+			// check method signature (parameters, return value)
+			if (method.HasParameters)
+				CheckParameters (method);
+
+			CheckReturnType (method);
+
+			// then check what the IL calls/access
+			if (method.HasBody) {
+				if (method.Body.HasVariables)
+					CheckVariables (method);
+
+				foreach (Instruction ins in method.Body.Instructions) {
+					switch (ins.OpCode.Code) {
+					case Code.Newarr:
+					case Code.Newobj:
+					case Code.Call:
+					case Code.Callvirt:
+						CheckMethodCall (method, ins, (ins.Operand as MethodReference));
+						break;
+					case Code.Initobj:
+						CheckTypeCreation (method, ins, (ins.Operand as TypeReference));
+						break;
+					case Code.Ldfld:
+					case Code.Ldflda:
+					case Code.Ldsfld:
+					case Code.Ldsflda:
+					case Code.Stfld:
+					case Code.Stsfld:
+						CheckFieldAccess (method, ins, (ins.Operand as FieldReference));
+						break;
+					}
+				}
+			}
+
+			return Runner.CurrentRuleResult;
+		}
+	}
+}
+
---
+++ gendarme/rules/Gendarme.Rules.Maintainability/Test/RemoveDependenceOnObsoleteCodeTest.cs
@@ -0,0 +1,302 @@
+//
+// Unit tests for RemoveDependenceOnObsoleteCodeRule
+//
+// Authors:
+//	Sebastien Pouliot <sebastien@ximian.com>
+//
+// Copyright (C) 2010 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using Gendarme.Rules.Maintainability;
+
+using NUnit.Framework;
+using Test.Rules.Definitions;
+using Test.Rules.Fixtures;
+
+namespace Tests.Rules.Maintainability {
+
+	[Obsolete]
+	class ObsoleteType {
+
+		public void Show ()
+		{
+		}
+	}
+
+	[Obsolete]
+	struct ObsoleteStruct {
+		public int Field;
+	}
+
+	[Obsolete]
+	struct ObsoleteEnum {
+	}
+
+	[Obsolete]
+	interface IObsolete {
+	}
+
+	class TypeInheritObsoleteType : ObsoleteType {
+	}
+
+	class TypeImplementObsoleteInterface : IObsolete {
+	}
+
+	interface InterfaceImplementObsoleteInterface : IObsolete {
+	}
+
+	enum EnumWithObsoleteValues {
+		One,
+		[Obsolete] Two,
+		Three,
+		[Obsolete] Four
+	}
+
+	class TypeWithEnumFields {
+		private ObsoleteEnum a;
+		private ObsoleteEnum b;
+		private EnumWithObsoleteValues c; // ok, the enum is not obsolete
+
+		public void SetEnum (ObsoleteEnum oe)
+		{
+			// parameter is not used so we detect it on the signature only
+		}
+
+		public void UseEnum ()
+		{
+			c = EnumWithObsoleteValues.Two; // obsolete
+		}
+	}
+
+	struct StructWithStuctFields {
+		ObsoleteStruct a;
+		DateTime b;
+
+		public void SetStruct (ObsoleteStruct os)
+		{
+			// parameter is not used so we detect it on the signature only
+		}
+
+		public void UseStruct ()
+		{
+			a = new ObsoleteStruct ();
+		}
+	}
+
+	class TypeWithObsoleteProperties {
+		[Obsolete]
+		int Integer { get; set; }
+		[Obsolete]
+		public string String { get; set; }
+	}
+
+	class TypeWithObsoletePropertiesType {
+		ObsoleteEnum Enum { get; set; }
+		ObsoleteStruct Struct { get; set; }
+		ObsoleteType Type { get; set; }
+	}
+
+	struct StructWithObsoleteEvents {
+		[Obsolete]
+		event EventHandler WayTooLate;
+		[Obsolete]
+		public event EventHandler<EventArgs> TooLate;
+	}
+
+	[Obsolete]
+	public class ObsoleteEventArgs : EventArgs {
+	}
+
+	[Obsolete]
+	public delegate void ObsoleteEventHandler (object sender, ObsoleteEventArgs e);
+
+	struct StructWithObsoleteEventsType {
+		event ObsoleteEventHandler WayTooLate;
+		public event EventHandler<ObsoleteEventArgs> TooLate;
+	}
+
+	[TestFixture]
+	public class RemoveDependenceOnObsoleteCodeTypeTest : TypeRuleTestFixture<RemoveDependenceOnObsoleteCodeRule> {
+
+		[Test]
+		public void NonObsolete ()
+		{
+			AssertRuleSuccess (SimpleTypes.Class);
+			AssertRuleSuccess (SimpleTypes.Delegate);
+			AssertRuleSuccess (SimpleTypes.Enum);
+			AssertRuleSuccess (SimpleTypes.GeneratedType);
+			AssertRuleSuccess (SimpleTypes.Interface);
+			AssertRuleSuccess (SimpleTypes.Structure);
+		}
+
+		[Test]
+		public void Obsolete ()
+		{
+			// having obsolete types / interfaces ... is not an issue
+			AssertRuleDoesNotApply<ObsoleteType> ();
+			AssertRuleDoesNotApply<ObsoleteStruct> ();
+			AssertRuleDoesNotApply<ObsoleteEnum> ();
+			AssertRuleDoesNotApply<IObsolete> ();
+			AssertRuleDoesNotApply<ObsoleteEventHandler> ();
+			// but using them is not
+			AssertRuleFailure<TypeInheritObsoleteType> (1);
+			AssertRuleFailure<TypeImplementObsoleteInterface> (1);
+			AssertRuleFailure<InterfaceImplementObsoleteInterface> (1);
+		}
+
+		[Test]
+		public void ObsoleteMembers ()
+		{
+			// having obsolete fields is not an issue
+			AssertRuleSuccess<EnumWithObsoleteValues> ();
+			AssertRuleSuccess<TypeWithObsoleteProperties> ();
+			AssertRuleSuccess<StructWithObsoleteEvents> ();
+
+			// unless they are used (obsolete type as field)
+			AssertRuleFailure<TypeWithEnumFields> (2);
+			AssertRuleFailure<StructWithStuctFields> (1);
+			AssertRuleFailure<TypeWithObsoletePropertiesType> (6); // 3 properties + 3 backing fields
+			AssertRuleFailure<StructWithObsoleteEventsType> (2);
+		}
+	}
+
+	class TypeWithObsoleteFields {
+
+		[Obsolete]
+		public static int Field;
+
+		void Structure ()
+		{
+			ObsoleteStruct s = new ObsoleteStruct ();
+			s.Field = 0;
+		}
+	}
+
+	class TypeWithObsoleteMethods {
+
+		[Obsolete]
+		public TypeWithObsoleteMethods ()
+		{
+		}
+
+		[Obsolete]
+		public void Show ()
+		{
+			Console.WriteLine ();
+		}
+
+		public void ShowData (object o)
+		{
+			// variable (and use) of an obsolete type
+			ObsoleteType t = (o as ObsoleteType);
+			Console.WriteLine (t);
+		}
+
+		public void ShowType (object o)
+		{
+			(o as ObsoleteType).Show ();
+		}
+
+		ObsoleteType GetObsolete ()
+		{
+			return null;
+		}
+
+		void SetObsoleteField (int x)
+		{
+			TypeWithObsoleteFields.Field = x;
+		}
+
+		static void Main ()
+		{
+			new TypeWithObsoleteMethods ().Show ();
+		}
+	}
+
+	[TestFixture]
+	public class RemoveDependenceOnObsoleteCodeMethodTest : MethodRuleTestFixture<RemoveDependenceOnObsoleteCodeRule> {
+
+		[Test]
+		public void DoesNotApply ()
+		{
+			// we can define [Obsolete] if we don't use them (backward compatibility)
+			AssertRuleDoesNotApply<TypeWithObsoleteMethods> ("Show");
+			AssertRuleDoesNotApply<TypeWithObsoleteMethods> (".ctor");
+		}
+
+		[Test]
+		public void ObsoleteMethods ()
+		{
+			// call .ctor and Show
+			AssertRuleFailure<TypeWithObsoleteMethods> ("Main", 2);
+			// variable (type and it's use)
+			AssertRuleFailure<TypeWithObsoleteMethods> ("ShowData", 1);
+			// call a non-obsolete method from an obsolete type
+			AssertRuleFailure<TypeWithObsoleteMethods> ("ShowType", 1);
+			// obsolete return value (+ implied local variable for csc, but not gmcs)
+			AssertRuleFailure<TypeWithObsoleteMethods> ("GetObsolete");
+		}
+
+		[Test]
+		public void ObsoleteFields ()
+		{
+			// the number of defects can vary between compiler (gmcs versus csc)
+			AssertRuleFailure<StructWithStuctFields> ("UseStruct");
+			AssertRuleFailure<TypeWithObsoleteFields> ("Structure", 3); // variable + initobj + field
+			AssertRuleFailure<TypeWithObsoleteMethods> ("SetObsoleteField", 1);
+		}
+
+		[Test]
+		[Ignore ("Enums")]
+		public void ObsoleteEnums ()
+		{
+			// enums are special since they are compiled as integers (or their base type)
+			// and we "lose" the real, named, value. We need to track every case back to the type
+			AssertRuleFailure<TypeWithEnumFields> ("UseEnum", 1);
+		}
+
+		[Test]
+		public void ObsoleteParameterTypes ()
+		{
+			AssertRuleFailure<TypeWithEnumFields> ("SetEnum", 1);
+			AssertRuleFailure<StructWithStuctFields> ("SetStruct", 1);
+		}
+
+		[Test]
+		public void ObsoleteProperty ()
+		{
+			// property is [Obsolete] - not the getter/setter
+			AssertRuleDoesNotApply<TypeWithObsoleteProperties> ("get_Integer");
+			AssertRuleDoesNotApply<TypeWithObsoleteProperties> ("set_Integer");
+		}
+
+		[Test]
+		public void ObsoleteEvents ()
+		{
+			// event is [Obsolete] - not the getter/setter
+			AssertRuleDoesNotApply<StructWithObsoleteEvents> ("add_TooLate");
+			AssertRuleDoesNotApply<StructWithObsoleteEvents> ("remove_TooLate");
+		}
+	}
+}
+
