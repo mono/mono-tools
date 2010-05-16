caa915e9b5e5d0d29fc89639e52c3bfd6dcb2127
Sebastien Pouliot <sebastien@ximian.com>
2010-05-16 23:00:39 +0000

    oops that was not meant to be committed (yet ;-)
    svn path=/trunk/mono-tools/; revision=157407

--- gendarme/framework/Gendarme.Framework/Runner.cs
+++ gendarme/framework/Gendarme.Framework/Runner.cs
@@ -27,7 +27,6 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Diagnostics.Contracts;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
