eed13e1148eef5d67df80afa9a69722553cc65a7
Sebastien Pouliot <sebastien@ximian.com>
2010-11-25 20:53:38 -0500

    Add first rule in Gendarme.Rules.Interoperability.Com from Google Code In
    * configure.in: Add new rule and test assemblies
    * gendarme/rules/Makefile.am: Add new rule directory
    
    * gendarme/rules/Gendarme.Rules.Interoperability.Com/Makefile.am: basic
    build file for rule assembly
    * gendarme/rules/Gendarme.Rules.Interoperability.Com/AvoidNonPublicFieldsInComVisibleValueTypesRule.cs:
    New rule from N Lum (with few format changes)
    
    * gendarme/rules/Gendarme.Rules.Interoperability.Com/Test/Makefile.am:
    basic build file for unit tests
    * gendarme/rules/Gendarme.Rules.Interoperability.Com/Test/AvoidNonPublicFieldsInComVisibleValueTypesTest.cs:
    New unit tests from N Lum (with few format changes)

---
+++ gendarme/rules/Gendarme.Rules.Interoperability.Com/AvoidNonPublicFieldsInComVisibleValueTypesRule.cs
@@ -0,0 +1,112 @@
+﻿//
+// Gendarme.Rules.Interoperability.Com.AvoidNonPublicFieldsInComVisibleValueTypesRule
+//
+// Authors:
+//	N Lum <nol888@gmail.com>
+// 
+// Copyright (C) 2010 N Lum
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+
+using Mono.Cecil;
+
+using Gendarme.Framework;
+using Gendarme.Framework.Rocks;
+
+namespace Gendarme.Rules.Interoperability.Com {
+
+	/// <summary>
+	/// This rule checks for ComVisible value types which contain fields that are non-public.
+	/// </summary>
+	/// <example>
+	/// Bad example:
+	/// <code>
+	/// [assembly: ComVisible (false)]
+	/// namespace InteropLibrary {
+	///	[ComVisible (true)]
+	///	public struct BadStruct {
+	///		internal int SomeValue;
+	///	}
+	/// }
+	/// </code>
+	/// </example>
+	/// <example>
+	/// Good example:
+	/// <code>
+	/// [assembly: ComVisible (false)]
+	/// namespace InteropLibrary {
+	///	[ComVisible (true)]
+	///	public struct BadStruct {
+	///		public int SomeValue;
+	///	}
+	/// }
+	/// </code>
+	/// </example>
+
+	[Problem ("ComVisible value types which contain non-public fields will have such fields exposed to COM clients.")]
+	[Solution ("Review fields for sensitive information, and either make the type a reference type, or remove the ComVisibleAttribute from the type")]
+	[FxCopCompatibility ("Microsoft.Interoperability", "CA1413:AvoidNonpublicFieldsInComVisibleValueTypes")]
+	public class AvoidNonPublicFieldsInComVisibleValueTypesRule : Rule, ITypeRule {
+
+		private const string ComVisible = "System.Runtime.InteropServices.ComVisibleAttribute";
+
+		public RuleResult CheckType (TypeDefinition type)
+		{
+			// Only check for value types and types with fields.
+			// But also for types with attributes, since FXCop only issues a warning if ComVisible is explicitly defined.
+			if (!type.IsValueType || !type.HasCustomAttributes || !type.HasFields)
+				return RuleResult.DoesNotApply;
+
+			// Ensure the defining assembly is not null, then perform the same check for attributes on the assembly.
+			AssemblyDefinition assembly = type.GetAssembly ();
+			if (assembly != null && !assembly.HasCustomAttributes)
+				return RuleResult.DoesNotApply;
+
+			// Iterate through attributes on the type and assembly to ensure that ComVisible is false on the assembly,
+			// and true on the type.
+			if (assembly != null) {
+				foreach (CustomAttribute attribute in assembly.CustomAttributes) {
+					if (attribute.Constructor.DeclaringType.FullName != ComVisible)
+						continue;
+					if ((bool) attribute.ConstructorArguments [0].Value)
+						return RuleResult.Success;
+					break;
+				}
+			}
+			foreach (CustomAttribute attribute in type.CustomAttributes) {
+				if (attribute.Constructor.DeclaringType.FullName != ComVisible)
+					continue;
+				if (!(bool) attribute.ConstructorArguments [0].Value)
+					return RuleResult.Success;
+				break;
+			}
+
+			// If we find any, low severity as the code works, but it's bad practice.
+			foreach (FieldDefinition field in type.Fields)
+				if (!field.IsPublic)
+					Runner.Report (field, Severity.Low, Confidence.Total);
+
+			return Runner.CurrentRuleResult;
+		}
+	}
+}
