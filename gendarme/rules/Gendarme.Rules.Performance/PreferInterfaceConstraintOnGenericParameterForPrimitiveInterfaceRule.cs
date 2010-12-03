206a1bbcb9b13a581f3b9d086e30854b32174fef
Sebastien Pouliot <sebastien@ximian.com>
2010-12-02 22:32:15 -0500

    Add PreferInterfaceConstraintOnGenericParameterForPrimitiveInterfaceRule|Test to gendarme
    * Makefile.am: Add new rule / tests to build.
    
    * PreferInterfaceConstraintOnGenericParameterForPrimitiveInterfaceRule.cs: New rule
    * Test/PreferInterfaceConstraintOnGenericParameterForPrimitiveInterfaceTest.cs: New tests

---
+++ gendarme/rules/Gendarme.Rules.Performance/PreferInterfaceConstraintOnGenericParameterForPrimitiveInterfaceRule.cs
@@ -0,0 +1,117 @@
+//
+// Gendarme.Rules.Performance.PreferInterfaceConstraintOnGenericParameterForPrimitiveInterfaceRule
+//
+// Authors:
+//	Julien Hoarau <madgnome@gmail.com>
+//
+// Copyright (C) 2010 Julien Hoarau
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Collections.Generic;
+using Gendarme.Framework;
+using Mono.Cecil;
+
+namespace Gendarme.Rules.Performance {
+	/// <summary>
+	/// This rule fires if a method use an interface of a primitive type as parameter.  
+	/// (IComparable, IFormattable, IConvertible, IComparable&lt;T&gt;, IEquatable&lt;int&gt; ...)
+	/// Using generic method with an interface constraint instead avoid boxing of value
+	/// type when calling the method.
+	/// </summary>
+	/// <example>
+	/// Bad example:
+	/// <code>
+	/// public bool GreaterThan (IComparable arg1, IComparable arg2)
+	/// {
+	///		return arg1.CompareTo (arg2) > 0
+	/// }
+	/// </code>
+	/// </example>
+	/// <example>
+	/// Good example:
+	/// <code>
+	/// public bool GreaterThan&lt;T&gt; (T arg1, T arg2) where T : IComparable
+	/// {
+	///		return arg1.CompareTo (arg2) > 0
+	/// }
+	/// </code>
+	/// </example>
+
+	[Problem("The method should use interface constraint on generic type parameter to avoid boxing of value type")]
+	[Solution("Replace the interface parameter with interface constraint on generic type parameter.")]
+	public class PreferInterfaceConstraintOnGenericParameterForPrimitiveInterfaceRule : Rule, IMethodRule {
+
+		private readonly HashSet<string> primitiveTypeInterfaces = new HashSet<string> ();
+
+		public PreferInterfaceConstraintOnGenericParameterForPrimitiveInterfaceRule ()
+		{
+			primitiveTypeInterfaces.Add ("System.IComparable");
+			primitiveTypeInterfaces.Add ("System.IComparable`1<T>");
+			primitiveTypeInterfaces.Add ("System.IComparable`1<System.Boolean>");
+			primitiveTypeInterfaces.Add ("System.IComparable`1<System.Byte>");
+			primitiveTypeInterfaces.Add ("System.IComparable`1<System.Char>");
+			primitiveTypeInterfaces.Add ("System.IComparable`1<System.Decimal>");
+			primitiveTypeInterfaces.Add ("System.IComparable`1<System.Double>");
+			primitiveTypeInterfaces.Add ("System.IComparable`1<System.Float>");
+			primitiveTypeInterfaces.Add ("System.IComparable`1<System.Int32>");
+			primitiveTypeInterfaces.Add ("System.IComparable`1<System.Int64>");
+			primitiveTypeInterfaces.Add ("System.IComparable`1<System.SByte>");
+			primitiveTypeInterfaces.Add ("System.IComparable`1<System.Short>");
+			primitiveTypeInterfaces.Add ("System.IComparable`1<System.UInt16>");
+			primitiveTypeInterfaces.Add ("System.IComparable`1<System.UInt32>");
+			primitiveTypeInterfaces.Add ("System.IComparable`1<System.UInt64>");
+			primitiveTypeInterfaces.Add ("System.IFormattable");
+			primitiveTypeInterfaces.Add ("System.IConvertible");
+			primitiveTypeInterfaces.Add ("System.IEquatable`1<T>");
+			primitiveTypeInterfaces.Add ("System.IEquatable`1<System.Boolean>");
+			primitiveTypeInterfaces.Add ("System.IEquatable`1<System.Byte>");
+			primitiveTypeInterfaces.Add ("System.IEquatable`1<System.Char>");
+			primitiveTypeInterfaces.Add ("System.IEquatable`1<System.Decimal>");
+			primitiveTypeInterfaces.Add ("System.IEquatable`1<System.Double>");
+			primitiveTypeInterfaces.Add ("System.IEquatable`1<System.Float>");
+			primitiveTypeInterfaces.Add ("System.IEquatable`1<System.Int32>");
+			primitiveTypeInterfaces.Add ("System.IEquatable`1<System.Int64>");
+			primitiveTypeInterfaces.Add ("System.IEquatable`1<System.SByte>");
+			primitiveTypeInterfaces.Add ("System.IEquatable`1<System.Short>");
+			primitiveTypeInterfaces.Add ("System.IEquatable`1<System.UInt16>");
+			primitiveTypeInterfaces.Add ("System.IEquatable`1<System.UInt32>");
+			primitiveTypeInterfaces.Add ("System.IEquatable`1<System.UInt64>");
+		}
+
+		public RuleResult CheckMethod (MethodDefinition method)
+		{
+			if (!method.HasParameters)
+				return RuleResult.DoesNotApply;
+
+			foreach (ParameterDefinition parameter in method.Parameters) {
+				if (primitiveTypeInterfaces.Contains (parameter.ParameterType.FullName)) {
+					string msg = String.Format ("You are using {0} as parameter, which cause boxing with value type as argument",
+					                            parameter.ParameterType.FullName);
+					Runner.Report (method, Severity.Low, Confidence.Total, msg);
+				}
+			}
+
+			return Runner.CurrentRuleResult;
+		}
+	}
+}
---
+++ gendarme/rules/Gendarme.Rules.Performance/Test/PreferInterfaceConstraintOnGenericParameterForPrimitiveInterfaceTest.cs
@@ -0,0 +1,155 @@
+//
+// Unit tests for PreferInterfaceConstraintOnGenericParameterForPrimitiveInterfaceRule
+//
+// Authors:
+//	Julien Hoarau <madgnome@gmail.com>
+//
+// Copyright (C) 2010 Julien Hoarau
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using Gendarme.Rules.Performance;
+using NUnit.Framework;
+using Test.Rules.Fixtures;
+
+namespace Tests.Rules.Performance {
+	[TestFixture]
+	public class PreferInterfaceConstraintOnGenericParameterForPrimitiveInterfaceTest :
+		MethodRuleTestFixture<PreferInterfaceConstraintOnGenericParameterForPrimitiveInterfaceRule> {
+
+		private class TestCase {
+			private void MethodWithoutParameters ()
+			{
+				
+			}
+
+			private void MethodWithInterfaceOfBaseTypeParameter (IComparable comparable)
+			{
+
+			}
+
+			private void MethodWithGenericInterfaceOfBaseTypeParameter<T> (IComparable<T> comparable)
+			{
+
+			}
+
+			private void MethodWithGenericInterfaceOfBaseTypeParameter2 (IComparable<int> comparable)
+			{
+
+			}
+
+			private void MethodWithInterfaceParameter (IDisposable disposable)
+			{
+				
+			}
+
+			private void MethodWithMultipleParameters (IDisposable disposable, IConvertible convertible)
+			{
+				
+			}
+
+			private void GenericMethodWithMultipleParameters<T> (IDisposable disposable, T convertible) 
+				where T : IConvertible
+			{
+				
+			}
+
+			private void MethodWithInterfaceConstraintOnGeneric<T> (T comparable) where T : IComparable
+			{
+				
+			}
+
+			private void MethodWithInterfaceConstraintOnGeneric2<TComparable, TType> (TComparable comparable)
+				where TComparable : IComparable<TType>
+			{
+
+			}
+
+			private void MethodWithInterfaceConstraintOnGeneric3<TComparable> (TComparable comparable)
+				where TComparable : IComparable<int>
+			{
+
+			}
+		}
+
+
+		[Test]
+		public void DoesNotApply ()
+		{
+			AssertRuleDoesNotApply<TestCase> ("MethodWithoutParameters");
+		}
+
+		[Test]
+		public void MethodWithInterfaceOfBaseTypeParameter ()
+		{
+			AssertRuleFailure<TestCase> ("MethodWithInterfaceOfBaseTypeParameter");
+		}
+
+		[Test]
+		public void MethodWithGenericInterfaceOfBaseTypeParameter ()
+		{
+			AssertRuleFailure<TestCase> ("MethodWithGenericInterfaceOfBaseTypeParameter");
+		}
+
+		[Test]
+		public void MethodWithGenericInterfaceOfBaseTypeParameter2 ()
+		{
+			AssertRuleFailure<TestCase> ("MethodWithGenericInterfaceOfBaseTypeParameter2");
+		}
+
+		[Test]
+		public void MethodWithMultipleParameters ()
+		{
+			AssertRuleFailure<TestCase> ("MethodWithMultipleParameters");
+		}
+
+		[Test]
+		public void MethodWithInterfaceParameter ()
+		{
+			AssertRuleSuccess<TestCase> ("MethodWithInterfaceParameter");
+		}
+
+		[Test]
+		public void MethodWithInterfaceConstraintOnGeneric ()
+		{
+			AssertRuleSuccess<TestCase> ("MethodWithInterfaceConstraintOnGeneric");
+		}
+
+		[Test]
+		public void MethodWithInterfaceConstraintOnGeneric2 ()
+		{
+			AssertRuleSuccess<TestCase> ("MethodWithInterfaceConstraintOnGeneric2");
+		}
+
+		[Test]
+		public void MethodWithInterfaceConstraintOnGeneric3 ()
+		{
+			AssertRuleSuccess<TestCase> ("MethodWithInterfaceConstraintOnGeneric3");
+		}
+
+		[Test]
+		public void GenericMethodWithMultipleParameters ()
+		{
+			AssertRuleSuccess<TestCase> ("GenericMethodWithMultipleParameters");
+		}
+	}
+}
