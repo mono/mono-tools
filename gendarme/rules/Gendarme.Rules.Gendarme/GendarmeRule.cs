cb2fb8d265eebacfc5640f0073a2e714b8d728c1
Sebastien Pouliot <sebastien@ximian.com>
2010-12-15 21:42:14 -0500

    Add new Gendarme.Rules.Gendarme.dll rule assembly
    * configure.in: Add makefile generation for new rule/test assemblies
    * gendarme/rules/Gendarme.Rules.Gendarme/.gitignore: New
    * gendarme/rules/Gendarme.Rules.Gendarme/DefectsMustBeReportedRule.cs: New rule
    * gendarme/rules/Gendarme.Rules.Gendarme/GendarmeRule.cs: New abstract base rule
    * gendarme/rules/Gendarme.Rules.Gendarme/Makefile.am: New, build rule assembly
    * gendarme/rules/Gendarme.Rules.Gendarme/Test/DefectsMustBeReportedTest.cs: New unit tests
    * gendarme/rules/Gendarme.Rules.Gendarme/Test/Makefile.am: New, build unit tests
    * gendarme/rules/Makefile.am: Build Gendarme.Rules.Gendarme rules / tests
    * gendarme/rules/rules.xml: Add in 'self-test' set and its own set
    * gendarme/self-test.ignore: Some entries to ignore

---
+++ gendarme/rules/Gendarme.Rules.Gendarme/GendarmeRule.cs
@@ -0,0 +1,54 @@
+﻿// 
+// Gendarme.Rules.Gendarme.GendarmeRule
+//
+// Authors:
+//	Yuri Stuken <stuken.yuri@gmail.com>
+//
+// Copyright (C) 2010 Yuri Stuken
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+using Gendarme.Framework;
+using Gendarme.Framework.Engines;
+using Gendarme.Framework.Helpers;
+using Gendarme.Framework.Rocks;
+
+namespace Gendarme.Rules.Gendarme {
+
+	abstract public class GendarmeRule : Rule {
+
+		public override void Initialize (IRunner runner)
+		{
+			base.Initialize (runner);
+
+			// If the assembly doesn't reference Gendarme.Framework then it 
+			// obviously is not a Gendarme Rules assembly
+			Runner.AnalyzeModule += (object o, RunnerEventArgs e) =>
+			{
+				Active = e.CurrentAssembly.References ("Gendarme.Framework");
+
+			};
+		}
+	}
+}
