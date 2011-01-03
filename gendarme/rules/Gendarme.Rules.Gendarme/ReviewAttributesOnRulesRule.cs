25de8967540677cb6d0e361ff5aa19101c2cf7dd
Sebastien Pouliot <sebastien@ximian.com>
2011-01-03 15:36:17 -0500

    Add new ReviewAttributesOnRulesRule from Yuri Stuken (CGI)
    * Makefile.am: Add new rule/tests to the build
    * Gendarme.Rules.Gendarme.csproj: Add new rule to csproj
    * ReviewAttributesOnRulesRule.cs: New rule
    
    * Test/Tests.Rules.Gendarme.csproj: Add new tests to csproj
    * Test/ReviewAttributesOnRulesTest.cs: New tests

---
+++ gendarme/rules/Gendarme.Rules.Gendarme/ReviewAttributesOnRulesRule.cs
@@ -0,0 +1,258 @@
+﻿// 
+// Gendarme.Rules.Gendarme.ReviewAttributesOnRulesRule
+//
+// Authors:
+//	Yuri Stuken <stuken.yuri@gmail.com>
+//
+// Copyright (C) 2010 Yuri Stuken
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+using Gendarme.Framework;
+using Gendarme.Framework.Engines;
+using Gendarme.Framework.Helpers;
+using Gendarme.Framework.Rocks;
+
+namespace Gendarme.Rules.Gendarme {
+
+	/// <summary>
+	/// This rule checks if attribute usage match the following rules:	
+	/// <list>
+	/// <item>
+	/// <term>[Problem] and [Solution] attributes</term>
+	/// <description>should be used on rules only, every concrete rule must have both 
+	/// attributes (or inherit them), and their arguments cannot be null or empty
+	/// </description>
+	/// </item>
+	/// <item>
+	/// <term>[FxCopCompatibility] attribute</term>
+	/// <description>should be used on rules only, its arguments cannot be null or empty,
+	/// and second argument should match this format: AB1234:FxCopRuleName</description>
+	/// </item>
+	/// <item>
+	/// <term>[EngineDependency] attribute</term>
+	/// <description>should be used on rules only, its argument cannot be null or empty,
+	/// and its argument should inherit from Gendarme.Framework.Engine</description>
+	/// </item>
+	/// <item>
+	/// <term>[DocumentationUri] attribute</term>
+	/// <description>should be used on rules only</description>
+	/// </item>
+	/// <item>
+	/// <term>[Description] and [DefaultValue] attributes</term>
+	/// <description>should be used on rules' public properties only,
+	/// Description attribute argument cannot be null or empty</description>
+	/// </item>
+	/// </list>
+	/// </summary>
+
+	[Problem ("Attributes should be correctly placed and have correct values provided in their arguments")]
+	[Solution ("Change the code so that it satisfies attribute usage rules")]
+	public class ReviewAttributesOnRulesRule : Rule, ITypeRule {
+
+		Dictionary<string, Action<CustomAttribute, ICustomAttributeProvider>> attributes;
+
+		Dictionary<string, bool> typeIsRule = new Dictionary<string, bool> ();
+
+		public ReviewAttributesOnRulesRule ()
+		{
+			attributes = new Dictionary<string, Action<CustomAttribute, ICustomAttributeProvider>>
+			{
+				{"Gendarme.Framework.ProblemAttribute", CheckProblemAndSolutionAttributes},
+				{"Gendarme.Framework.SolutionAttribute", CheckProblemAndSolutionAttributes},
+				{"Gendarme.Framework.FxCopCompatibilityAttribute", CheckFxCopCompatibilityAttribute},
+				{"Gendarme.Framework.EngineDependencyAttribute", CheckEngineDependencyAttribute},
+				{"Gendarme.Framework.DocumentationUriAttribute", CheckIfAttributeUsedOnRule},
+				{"System.ComponentModel.DescriptionAttribute", CheckDescriptionAttribute},
+				{"System.ComponentModel.DefaultValueAttribute", CheckIfAttributeUsedOnRulesProperty},
+			};
+		}
+
+		public RuleResult CheckType (TypeDefinition type)
+		{
+			CheckAttributes (type);
+
+			if (type.HasMethods)
+				foreach (MethodDefinition method in type.Methods)
+					CheckAttributes (method);
+
+			if (type.HasProperties)
+				foreach (PropertyDefinition property in type.Properties)
+					CheckAttributes (property);
+			if (type.HasFields)
+				foreach (FieldDefinition field in type.Fields)
+					CheckAttributes (field);
+
+			// finally check if this is a rule and either has all required attributes
+			// or inherits from a type that has all required attributes
+			CheckIfRuleHasAllRequiredAttributes (type);
+
+			return Runner.CurrentRuleResult;
+		}
+
+		private void CheckIfRuleHasAllRequiredAttributes (TypeDefinition type)
+		{
+			if (!type.IsAbstract && IsRule (type)) {
+				bool foundSolution = false;
+				bool foundProblem = false;
+				TypeDefinition td = type;
+				while (!foundSolution || !foundProblem) {
+					if (td.HasCustomAttributes)
+						foreach (CustomAttribute attribute in td.CustomAttributes) {
+							var attributeTypeName = attribute.AttributeType.FullName;
+							if (attributeTypeName == "Gendarme.Framework.SolutionAttribute")
+								foundSolution = true;
+							if (attributeTypeName == "Gendarme.Framework.ProblemAttribute")
+								foundProblem = true;
+						}
+
+					TypeReference tr = td.BaseType;
+					if (tr == null)
+						break;
+					TypeDefinition resolved = tr.Resolve ();
+					if (resolved == null)
+						break;
+					td = resolved;
+				}
+				if (!foundProblem || !foundSolution)
+					Runner.Report (type, Severity.High, Confidence.High,
+						"Rules should have both Problem and Solution attributes");
+			}
+		}
+
+		private void CheckAttributes (ICustomAttributeProvider provider)
+		{
+			if (!provider.HasCustomAttributes)
+				return;
+
+			foreach (CustomAttribute attribute in provider.CustomAttributes) {
+				var attributeTypeName = attribute.AttributeType.FullName;
+				Action<CustomAttribute, ICustomAttributeProvider> f;
+				if (attributes.TryGetValue(attributeTypeName, out f))
+					f (attribute, provider);
+			}
+		}
+
+		private bool IsRule (TypeReference type)
+		{
+			var typeName = type.FullName;
+			bool result;
+			if (!typeIsRule.TryGetValue (typeName, out result)) {
+				result = type.Implements ("Gendarme.Framework.IRule");
+				typeIsRule [typeName] = result;
+			}
+			return result;
+		}
+
+		private void CheckProblemAndSolutionAttributes (CustomAttribute attribute, ICustomAttributeProvider provider)
+		{
+			CheckIfAttributeUsedOnRule (attribute, provider);
+			CheckIfStringArgumentsAreNotNullOrEmpty (attribute, provider);
+		}
+
+		private void CheckFxCopCompatibilityAttribute (CustomAttribute attribute, ICustomAttributeProvider provider)
+		{
+			CheckIfAttributeUsedOnRule (attribute, provider);
+			if (!CheckIfStringArgumentsAreNotNullOrEmpty (attribute, provider))
+				return;
+
+			// check if second argument has correct format
+			if (!attribute.HasConstructorArguments)
+				return;
+			var attributeTypeName = attribute.AttributeType.FullName;
+			var argumentValue = attribute.ConstructorArguments [1].Value.ToString ();
+			var length = argumentValue.Length;
+			if (!((length == 6 || (length > 8 && argumentValue [6] == ':')) && 
+				Char.IsLetter (argumentValue [0]) && Char.IsLetter(argumentValue[1]) && 
+				Char.IsDigit(argumentValue[2]) && Char.IsDigit(argumentValue[3]) && 
+				Char.IsDigit(argumentValue[4])  && Char.IsDigit(argumentValue[5]))) 
+				Runner.Report (provider, Severity.Medium, Confidence.High,
+					attributeTypeName + " second argument should match the followint format: XX9999:Name");
+			else if (length == 6)
+				Runner.Report (provider, Severity.Medium, Confidence.High,
+					attributeTypeName + " second argument should contain both rule ID and name");
+		}
+
+		private void CheckEngineDependencyAttribute (CustomAttribute attribute, ICustomAttributeProvider provider)
+		{
+			CheckIfAttributeUsedOnRule (attribute, provider);
+			CheckIfStringArgumentsAreNotNullOrEmpty (attribute, provider);
+
+			if (!attribute.HasConstructorArguments)
+				return;
+			var argument = attribute.ConstructorArguments [0];
+
+			// if possible, check if argument type implements IEngine
+			if (argument.Type.FullName == "System.Type") {
+				TypeReference tr = (argument.Value as TypeReference);
+				if (tr == null || !tr.Inherits ("Gendarme.Framework.Engine")) // IEngine does not exist yet
+					Runner.Report (provider, Severity.Medium, Confidence.High,
+						"EngineDependency attribute argument should implement IEngine interface");
+
+			}
+		}
+
+		private void CheckDescriptionAttribute (CustomAttribute attribute, ICustomAttributeProvider provider)
+		{
+			CheckIfStringArgumentsAreNotNullOrEmpty (attribute, provider);
+			CheckIfAttributeUsedOnRulesProperty (attribute, provider);
+		}
+
+		private void CheckIfAttributeUsedOnRule (ICustomAttribute attribute, ICustomAttributeProvider provider)
+		{
+			TypeDefinition td = (provider as TypeDefinition);
+			if (td == null || !IsRule (td))
+				Runner.Report (td, Severity.Medium, Confidence.High,
+					attribute.AttributeType.FullName + " can be used on rules only");
+		}
+
+		private void CheckIfAttributeUsedOnRulesProperty (ICustomAttribute attribute, ICustomAttributeProvider provider)
+		{
+			PropertyDefinition property = (provider as PropertyDefinition);
+			if (property == null || !IsRule (property.DeclaringType) || 
+				!property.GetMethod.IsPublic || !property.SetMethod.IsPublic)
+				Runner.Report (provider, Severity.High, Confidence.High,
+					attribute.AttributeType.FullName + " should be used only on rules' public properties");
+		}
+
+		// returns true when all arguments are fine, false otherwise
+		private bool CheckIfStringArgumentsAreNotNullOrEmpty (CustomAttribute attribute, ICustomAttributeProvider provider)
+		{
+			if (!attribute.HasConstructorArguments)
+				return true;
+			foreach (CustomAttributeArgument argument in attribute.ConstructorArguments) {
+				if (argument.Type.FullName != "System.String")
+					continue;
+				if (String.IsNullOrEmpty ((string) argument.Value)) {
+					Runner.Report (provider, Severity.Medium, Confidence.High,
+						attribute.AttributeType.FullName + " argument cannot be null or empty");
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+}
---
+++ gendarme/rules/Gendarme.Rules.Gendarme/Test/ReviewAttributesOnRulesTest.cs
@@ -0,0 +1,361 @@
+﻿// 
+// Test.Rules.Gendarme.ReviewAttributesOnRulesTest
+//
+// Authors:
+//	Yuri Stuken <stuken.yuri@gmail.com>
+//
+// Copyright (C) 2010 Yuri Stuken
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Reflection;
+using System.ComponentModel;
+
+using Mono.Cecil;
+using Gendarme.Rules.Gendarme;
+using Gendarme.Framework;
+using Gendarme.Framework.Engines;
+
+using NUnit.Framework;
+using Test.Rules.Fixtures;
+using Test.Rules.Helpers;
+using Test.Rules.Definitions;
+
+namespace Test.Rules.Gendarme {
+
+	[TestFixture]
+	public class ReviewAttributesOnRulesTest : TypeRuleTestFixture<ReviewAttributesOnRulesRule> {
+
+		class Case1 : Rule, IMethodRule {
+			public RuleResult CheckMethod (MethodDefinition method)
+			{
+				throw new NotImplementedException ();
+			}
+		}
+
+		[Problem ("Problem description")]
+		class Case2 : Rule, IMethodRule {
+			public RuleResult CheckMethod (MethodDefinition method)
+			{
+				throw new NotImplementedException ();
+			}
+		}
+
+		[Problem (null)]
+		class Case3 : Rule, IMethodRule {
+			public RuleResult CheckMethod (MethodDefinition method)
+			{
+				throw new NotImplementedException ();
+			}
+		}
+
+		[Solution ("")]
+		class Case4 : Rule, IMethodRule {
+			public RuleResult CheckMethod (MethodDefinition method)
+			{
+				throw new NotImplementedException ();
+			}
+		}
+
+		[Problem ("")]
+		[Solution (null)]
+		class Case5 : Rule, IMethodRule {
+			public RuleResult CheckMethod (MethodDefinition method)
+			{
+				throw new NotImplementedException ();
+			}
+		}
+
+		[Problem ("Problem description")]
+		[Solution (null)]
+		class Case6 : Rule, IMethodRule {
+			public RuleResult CheckMethod (MethodDefinition method)
+			{
+				throw new NotImplementedException ();
+			}
+		}
+
+		[Problem ("Problem description")]
+		[Solution ("Solution description")]
+		class Case7 {
+		}
+
+		[Problem ("Problem description")]
+		[Solution ("Solution description")]
+		class Case8 : Rule, IMethodRule {
+			public RuleResult CheckMethod (MethodDefinition method)
+			{
+				throw new NotImplementedException ();
+			}
+		}
+
+		abstract class Case9 : Rule, IMethodRule {
+			public RuleResult CheckMethod (MethodDefinition method)
+			{
+				throw new NotImplementedException ();
+			}
+		}
+
+		[Problem ("Problem description")]
+		[Solution ("Solution description")]
+		abstract class Case10 : Rule, IMethodRule {
+			public RuleResult CheckMethod (MethodDefinition method)
+			{
+				throw new NotImplementedException ();
+			}
+		}
+
+		class Case11 : Case10 {
+		}
+
+		[Test]
+		public void ProblemSolution ()
+		{
+			AssertRuleFailure<Case1> (1); // required attributes missing
+			AssertRuleFailure<Case2> (1); // Solution attribute missing
+			AssertRuleFailure<Case3> (2); // null argument and Solution attribute missing
+			AssertRuleFailure<Case4> (2); // empty string argument and Problem attribute missing
+			AssertRuleFailure<Case5> (2); // null and empty string arguments
+			AssertRuleFailure<Case6> (1); // null argument on Solution attribute
+			AssertRuleFailure<Case7> (2); // two attributes are used on non-rule
+
+			AssertRuleSuccess<Case8> (); // concrete rule has both attributes with correct arguments
+			AssertRuleSuccess<Case9> (); // abstract rule has no attributes
+			AssertRuleSuccess<Case10> (); // abstract rule has both required attributes
+			AssertRuleSuccess<Case11> (); // concrete rule inherits attributes from abstract rule
+
+		}
+
+		[Problem ("Problem description")]
+		[Solution ("Solution description")]
+		[FxCopCompatibility ("", null)]
+		class Case12 : Rule, IMethodRule {
+			public RuleResult CheckMethod (MethodDefinition method)
+			{
+				throw new NotImplementedException ();
+			}
+		}
+
+		[Problem ("Problem description")]
+		[Solution ("Solution description")]
+		[FxCopCompatibility ("Microsoft.Test", "12345")]
+		class Case13 : Rule, IMethodRule {
+			public RuleResult CheckMethod (MethodDefinition method)
+			{
+				throw new NotImplementedException ();
+			}
+		}
+
+
+		[Problem ("Problem description")]
+		[Solution ("Solution description")]
+		[FxCopCompatibility ("Microsoft.Test", "CA12345")]
+		class Case14 : Rule, IMethodRule {
+			public RuleResult CheckMethod (MethodDefinition method)
+			{
+				throw new NotImplementedException ();
+			}
+		}
+
+		[Problem ("Problem description")]
+		[Solution ("Solution description")]
+		[FxCopCompatibility ("Microsoft.Test", "CA1234")]
+		class Case15 : Rule, IMethodRule {
+			public RuleResult CheckMethod (MethodDefinition method)
+			{
+				throw new NotImplementedException ();
+			}
+		}
+
+		[FxCopCompatibility ("Microsoft.Test", "CA1234:RuleName")]
+		class Case16 {
+		}
+
+		[Problem ("Problem description")]
+		[Solution ("Solution description")]
+		[FxCopCompatibility ("Microsoft.Test", "CA1234:RuleName")]
+		class Case17 : Rule, IMethodRule {
+			public RuleResult CheckMethod (MethodDefinition method)
+			{
+				throw new NotImplementedException ();
+			}
+		}
+
+
+
+		[Test]
+		public void FxCopCompatibility ()
+		{
+			AssertRuleFailure<Case12> (1); // arguments has null or empty string values
+			AssertRuleFailure<Case13> (1); // second argument has wrong format
+			AssertRuleFailure<Case14> (1); // second argument has wrong format
+			AssertRuleFailure<Case15> (1); // second argument has correct format but no FxCop rule name is provided
+			AssertRuleFailure<Case16> (1); // attribute used on non-rule
+
+			AssertRuleSuccess<Case17> (); // arguments have correct formats and FxCop rule name is provided
+		}
+
+		[EngineDependency (typeof (OpCodeEngine))]
+		class Case18 {
+		}
+
+
+		[Problem ("Problem description")]
+		[Solution ("Solution description")]
+		[EngineDependency ("")]
+		class Case19 : Rule, IMethodRule {
+			public RuleResult CheckMethod (MethodDefinition method)
+			{
+				throw new NotImplementedException ();
+			}
+		}
+
+		[Problem ("Problem description")]
+		[Solution ("Solution description")]
+		[EngineDependency (typeof (ReviewAttributesOnRulesRule))]
+		class Case20 : Rule, IMethodRule {
+			public RuleResult CheckMethod (MethodDefinition method)
+			{
+				throw new NotImplementedException ();
+			}
+		}
+
+		[Problem ("Problem description")]
+		[Solution ("Solution description")]
+		[EngineDependency (typeof (OpCodeEngine))]
+		class Case21 : Rule, IMethodRule {
+			public RuleResult CheckMethod (MethodDefinition method)
+			{
+				throw new NotImplementedException ();
+			}
+		}
+
+		[Problem ("Problem description")]
+		[Solution ("Solution description")]
+		[EngineDependency ("Gendarme.Framework.Engines.OpCodeEngine")]
+		class Case22 : Rule, IMethodRule {
+			public RuleResult CheckMethod (MethodDefinition method)
+			{
+				throw new NotImplementedException ();
+			}
+		}
+
+		[Test]
+		public void EngineDependency ()
+		{
+			AssertRuleFailure<Case18> (1); // attribute used on non-rule
+			AssertRuleFailure<Case19> (1); // empty string argument
+			AssertRuleFailure<Case20> (1); // argument does not inherit from Engine
+
+			AssertRuleSuccess<Case21> (); // argument type inherit from Engine
+			AssertRuleSuccess<Case22> (); // non-empty string argument provided
+		}
+
+		[DocumentationUri ("http://www.mono-project.com/Gendarme")]
+		class Case23 {
+		}
+
+		[Problem ("Problem description")]
+		[Solution ("Solution description")]
+		[DocumentationUri ("http://www.mono-project.com/Gendarme")]
+		class Case24 : Rule, IMethodRule {
+			public RuleResult CheckMethod (MethodDefinition method)
+			{
+				throw new NotImplementedException ();
+			}
+		}
+
+		[Test]
+		public void DocumentationUri ()
+		{
+			AssertRuleFailure<Case23> (1); // attribute used on non-rule
+
+			AssertRuleSuccess<Case24> (); // attribute used on rule
+		}
+
+
+		[Problem ("Problem description")]
+		[Solution ("Solution description")]
+		[System.ComponentModel.Description ("Description")]
+		[DefaultValue (null)]
+		class Case25 : Rule, IMethodRule {
+			public RuleResult CheckMethod (MethodDefinition method)
+			{
+				throw new NotImplementedException ();
+			}
+		}
+
+		class Case26 {
+			[System.ComponentModel.Description ("Description")]
+			[DefaultValue (null)]
+			public int Property { get; set; }
+		}
+
+		[Problem ("Problem description")]
+		[Solution ("Solution description")]
+		class Case27 : Rule, IMethodRule {
+			[System.ComponentModel.Description ("")]
+			[DefaultValue (null)]
+			public int Property { get; set; }
+
+			public RuleResult CheckMethod (MethodDefinition method)
+			{
+				throw new NotImplementedException ();
+			}
+		}
+
+		[Problem ("Problem description")]
+		[Solution ("Solution description")]
+		class Case28 : Rule, IMethodRule {
+			[System.ComponentModel.Description ("Description")]
+			[DefaultValue (null)]
+			private int Property { get; set; }
+
+			public RuleResult CheckMethod (MethodDefinition method)
+			{
+				throw new NotImplementedException ();
+			}
+		}
+
+		[Problem ("Problem description")]
+		[Solution ("Solution description")]
+		class Case29 : Rule, IMethodRule {
+			[System.ComponentModel.Description ("Description")]
+			[DefaultValue (42)]
+			public int Property { get; set; }
+
+			public RuleResult CheckMethod (MethodDefinition method)
+			{
+				throw new NotImplementedException ();
+			}
+		}
+
+		[Test]
+		public void DescriptionDefaultValue ()
+		{
+			AssertRuleFailure<Case25> (2); // two attributes used in wrong place (on rule)
+			AssertRuleFailure<Case26> (2); // two attributes used in wrong place (on non-rule property)
+			AssertRuleFailure<Case27> (1); // Description argument is an empty string
+			AssertRuleFailure<Case28> (2); // two attributes used on private property
+
+			AssertRuleSuccess<Case29> (); // both attributes are used in correct place and Desciprion argument is not empty
+		}
+	}
+}
