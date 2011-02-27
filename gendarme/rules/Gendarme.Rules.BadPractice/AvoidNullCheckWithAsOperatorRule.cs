07a50b92c1cc3bc06db9d69464d54b10a40bf2dc
Sebastien Pouliot <sebastien@ximian.com>
2011-02-27 16:26:17 -0500

    New AvoidNullCheckWithAsOperatorRule - bnc #651305
    * AvoidNullCheckWithAsOperatorRule.cs: New rule to detect unneeded null
    check before the use of the 'as' operator
    * Makefile.am: Add rule and test to the build
    * Test/AvoidNullCheckWithAsOperatorTest.cs: New. unit tests

---
+++ gendarme/rules/Gendarme.Rules.BadPractice/AvoidNullCheckWithAsOperatorRule.cs
@@ -0,0 +1,111 @@
+//
+// Gendarme.Rules.BadPractice.AvoidNullCheckWithAsOperatorRule
+//
+// Authors:
+//	Sebastien Pouliot <sebastien@ximian.com>
+//
+// Copyright (C) 2011 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+using Gendarme.Framework;
+using Gendarme.Framework.Engines;
+using Gendarme.Framework.Helpers;
+using Gendarme.Framework.Rocks;
+
+namespace Gendarme.Rules.BadPractice {
+
+	// as suggested in https://bugzilla.novell.com/show_bug.cgi?id=651305
+	[Problem ("An unneeded null check is done before using the 'as' operator.")]
+	[Solution ("Remove the extraneous null check")]
+	[EngineDependency (typeof (OpCodeEngine))]
+	public class AvoidNullCheckWithAsOperatorRule : Rule, IMethodRule {
+
+		OpCodeBitmask mask = new OpCodeBitmask (0x100000, 0x10000000000000, 0x0, 0x0);
+
+		static bool CheckFalseBranch (Instruction ins)
+		{
+			Instruction next = ins.Next;
+			if (!next.Is (ins.Previous.OpCode.Code))
+				return false;
+
+			if (!(ins.Operand as Instruction).Is (Code.Ldnull))
+				return false;
+
+			return next.Next.Is (Code.Isinst);
+		}
+
+		static bool CheckTrueBranch (Instruction ins)
+		{
+			if (!ins.Next.Is (Code.Ldnull))
+				return false;
+
+			Instruction br = (ins.Operand as Instruction);
+			if (ins.Previous.OpCode.Code != br.OpCode.Code)
+				return false;
+
+			return br.Next.Is (Code.Isinst);
+		}
+
+		public RuleResult CheckMethod (MethodDefinition method)
+		{
+			// rule only applies to methods with IL...
+			if (!method.HasBody)
+				return RuleResult.DoesNotApply;
+
+			// and when the IL contains both a isinst and ldnull
+			if (!mask.IsSubsetOf (OpCodeEngine.GetBitmask (method)))
+				return RuleResult.DoesNotApply;
+
+			foreach (Instruction ins in method.Body.Instructions) {
+				bool detected = false;
+				switch (ins.OpCode.Code) {
+				case Code.Brfalse_S:
+				case Code.Brfalse:
+					detected = CheckFalseBranch (ins);
+					break;
+				case Code.Brtrue_S:
+				case Code.Brtrue:
+					detected = CheckTrueBranch (ins);
+					break;
+				}
+				if (detected)
+					Runner.Report (method, ins, Severity.Medium, Confidence.Normal);
+			}
+
+			return Runner.CurrentRuleResult;
+		}
+#if false
+		public void Bitmask ()
+		{
+			OpCodeBitmask mask = new OpCodeBitmask ();
+			mask.Set (Code.Isinst);
+			mask.Set (Code.Ldnull);
+			Console.WriteLine (mask);
+		}
+#endif
+	}
+}
---
+++ gendarme/rules/Gendarme.Rules.BadPractice/Test/AvoidNullCheckWithAsOperatorTest.cs
@@ -0,0 +1,128 @@
+//
+// Unit Tests for AvoidNullCheckWithAsOperatorRule
+//
+// Authors:
+//	Sebastien Pouliot <sebastien@ximian.com>
+//
+// Copyright (C) 2011 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using Gendarme.Rules.BadPractice;
+
+using NUnit.Framework;
+using Test.Rules.Definitions;
+using Test.Rules.Fixtures;
+
+namespace Test.Rules.BadPractice {
+
+	[TestFixture]
+	public class AvoidNullCheckWithAsOperatorTest : MethodRuleTestFixture<AvoidNullCheckWithAsOperatorRule> {
+
+		string AsString_Bad1a (object obj)
+		{
+			return obj == null ? null : obj as string;
+		}
+
+		string AsString_Bad1b (object obj)
+		{
+			if (obj != null)
+				return obj as string;
+			return null;
+		}
+
+		string AsString_Bad2a (object obj)
+		{
+			return obj != null ? obj as string : null;
+		}
+
+		string AsString_Bad2b (object obj)
+		{
+			if (obj == null)
+				return null;
+			return obj as string;
+		}
+
+		string AsString_Good (object obj)
+		{
+			return obj as string;
+		}
+
+		string AsString_Good2 (string message, object obj)
+		{
+			if (message == null)
+				throw new ArgumentNullException ("message");
+			Console.WriteLine (message);
+			return obj as string;
+		}
+
+		string AsString_Good3 (string message, object obj)
+		{
+			if (message == null)
+				return null;
+			Console.WriteLine (message);
+			return obj as string;
+		}
+
+		void LocalsBad1a ()
+		{
+			object o = null;
+			string a = o == null ? null : o as string;
+			Console.WriteLine (a);
+		}
+
+		void LocalsBad2a ()
+		{
+			object o = null;
+			string a = o != null ? o as string : null;
+			Console.WriteLine (a);
+		}
+
+		[Test]
+		public void DoesNotApply ()
+		{
+			AssertRuleDoesNotApply (SimpleMethods.ExternalMethod);
+			AssertRuleDoesNotApply (SimpleMethods.EmptyMethod);
+			// no ldnull instruction
+			AssertRuleDoesNotApply<AvoidNullCheckWithAsOperatorTest> ("AsString_Good");
+			AssertRuleDoesNotApply<AvoidNullCheckWithAsOperatorTest> ("AsString_Good2");
+		}
+
+		[Test]
+		public void Success ()
+		{
+			AssertRuleSuccess<AvoidNullCheckWithAsOperatorTest> ("AsString_Good3");
+		}
+
+		[Test]
+		public void Failure ()
+		{
+			AssertRuleFailure<AvoidNullCheckWithAsOperatorTest> ("AsString_Bad1a");
+			AssertRuleFailure<AvoidNullCheckWithAsOperatorTest> ("AsString_Bad1b");
+			AssertRuleFailure<AvoidNullCheckWithAsOperatorTest> ("AsString_Bad2a");
+			AssertRuleFailure<AvoidNullCheckWithAsOperatorTest> ("AsString_Bad2b");
+
+			AssertRuleFailure<AvoidNullCheckWithAsOperatorTest> ("LocalsBad1a");
+			AssertRuleFailure<AvoidNullCheckWithAsOperatorTest> ("LocalsBad2a");
+		}
+	}
+}
