2cb6cc8bcef9f2adc987b28669c7fe3125c06329
Jeremie Laval <jeremie.laval@gmail.com>
2012-02-01 10:05:34 +0100

    [webdoc] add necessary files

---
+++ webdoc/fullsearch.js
@@ -0,0 +1,40 @@
+var spinner = $('#s_spinner');
+
+(function ($) {
+	var query = window.location.hash;
+	if (query == null || query.length < 2)
+		return;
+
+	var currentNumber = 0;
+	var count = 0;
+
+	// Remove hash '#' symbol
+	query = query.substring(1);
+	$('#s_term').text (query);
+
+	var fetch_and_add_results = function (url) {
+		spinner.toggleClass ('hidden');
+		$.getJSON (url, function (data) {
+			spinner.toggleClass ('hidden');
+			count = data.count;
+			currentNumber += data.result.length;
+
+			if (data.result.length == 0) {
+				$('<div/>', { 'class': 's_message' }).text('No more results').replaceAll($('#s_morebtn')).fadeOut(4000, function () { $(this).remove(); });
+			} else {
+				var lis = $.map (data.result, function (element) {
+					return '<li><a href="/monodoc.ashx?link=' + element.url + '"><span class="name">'
+						+ element.name + '</span> '
+						+ (element.fulltitle.length > 0 ? '<span class="fulltitle">(' + element.fulltitle + ')</span>' : '') + '</a></li>';
+				});
+				$('#s_results ul').append (lis.join (''));
+			}
+		});
+	};
+
+	fetch_and_add_results ('/monodoc.ashx?search=' + query);
+
+	$('#s_morebtn input').click (function () {
+		fetch_and_add_results ('/monodoc.ashx?search=' + query + '&start=' + currentNumber + '&count=' + count);
+	});
+})(window.jQuery);
