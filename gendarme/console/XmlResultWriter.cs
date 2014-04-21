94f3d3445edce722399289d894d6a8025a38c3d5
Anthony DeMartini <anthony.demartini@avi.com>
2014-04-21 14:14:04 -0400

    Fixed XML encoding issue for defect descriptions
    Some defect descriptions in XML reports had invalid characters.

--- gendarme/console/XmlResultWriter.cs
+++ gendarme/console/XmlResultWriter.cs
@@ -42,6 +42,7 @@ using Mono.Cecil;
 
 using Gendarme.Framework;
 using Gendarme.Framework.Rocks;
+using System.Collections.Generic;
 
 namespace Gendarme {
 
@@ -181,7 +182,9 @@ namespace Gendarme {
 			writer.WriteAttributeString ("Confidence", defect.Confidence.ToString ());
 			writer.WriteAttributeString ("Location", defect.Location.ToString ());
 			writer.WriteAttributeString ("Source", defect.Source);
-			writer.WriteString (defect.Text);
+            //Built-in .net encoding for XML strings doesn't appear to handle all cases, so
+            //we need an alternative approach.
+            writer.WriteRaw(XmlTextEncoder.Encode(defect.Text));
 			writer.WriteEndElement ();
 		}
 
@@ -203,5 +206,105 @@ namespace Gendarme {
 				writer = null;
 			}
 		}
+
+        /// <summary>
+        /// Borrowed from mkropat's .NET-Snippets:
+        /// https://github.com/mkropat/.NET-Snippets/blob/master/XmlTextEncoder.cs
+        /// Encodes data so that it can be safely embedded as text in XML documents.
+        /// </summary>
+        public class XmlTextEncoder : TextReader
+        {
+            public static string Encode(string s)
+            {
+                using (var stream = new StringReader(s))
+                using (var encoder = new XmlTextEncoder(stream))
+                {
+                    return encoder.ReadToEnd();
+                }
+            }
+
+            /// <param name="source">The data to be encoded in UTF-16 format.</param>
+            /// <param name="filterIllegalChars">It is illegal to encode certain
+            /// characters in XML. If true, silently omit these characters from the
+            /// output; if false, throw an error when encountered.</param>
+            public XmlTextEncoder(TextReader source, bool filterIllegalChars = true)
+            {
+                _source = source;
+                _filterIllegalChars = filterIllegalChars;
+            }
+
+            readonly Queue<char> _buf = new Queue<char>();
+            readonly bool _filterIllegalChars;
+            readonly TextReader _source;
+
+            public override int Peek()
+            {
+                PopulateBuffer();
+                if (_buf.Count == 0) return -1;
+                return _buf.Peek();
+            }
+
+            public override int Read()
+            {
+                PopulateBuffer();
+                if (_buf.Count == 0) return -1;
+                return _buf.Dequeue();
+            }
+
+            void PopulateBuffer()
+            {
+                const int endSentinel = -1;
+                while (_buf.Count == 0 && _source.Peek() != endSentinel)
+                {
+                    // Strings in .NET are assumed to be UTF-16 encoded [1].
+                    var c = (char)_source.Read();
+                    if (Entities.ContainsKey(c))
+                    {
+                        // Encode all entities defined in the XML spec [2].
+                        foreach (var i in Entities[c]) _buf.Enqueue(i);
+                    }
+                    else if (!(0x0 <= c && c <= 0x8) &&
+                             !new[] { 0xB, 0xC }.Contains(c) &&
+                             !(0xE <= c && c <= 0x1F) &&
+                             !(0x7F <= c && c <= 0x84) &&
+                             !(0x86 <= c && c <= 0x9F) &&
+                             !(0xD800 <= c && c <= 0xDFFF) &&
+                             !new[] { 0xFFFE, 0xFFFF }.Contains(c))
+                    {
+                        // Allow if the Unicode codepoint is legal in XML [3].
+                        _buf.Enqueue(c);
+                    }
+                    else if (char.IsHighSurrogate(c) &&
+                             _source.Peek() != endSentinel &&
+                             char.IsLowSurrogate((char)_source.Peek()))
+                    {
+                        // Allow well-formed surrogate pairs [1].
+                        _buf.Enqueue(c);
+                        _buf.Enqueue((char)_source.Read());
+                    }
+                    else if (!_filterIllegalChars)
+                    {
+                        // Note that we cannot encode illegal characters as entity
+                        // references due to the "Legal Character" constraint of
+                        // XML [4]. Nor are they allowed in CDATA sections [5].
+                        throw new ArgumentException(
+                            String.Format("Illegal character: '{0:X}'", (int)c));
+                    }
+                }
+            }
+
+            static readonly Dictionary<char, string> Entities =
+                new Dictionary<char, string> {
+                { '"', "&quot;" }, { '&', "&amp;"}, { '\'', "&apos;" },
+                { '<', "&lt;" }, { '>', "&gt;" },
+            };
+
+            // References:
+            // [1] http://en.wikipedia.org/wiki/UTF-16/UCS-2
+            // [2] http://www.w3.org/TR/xml11/#sec-predefined-ent
+            // [3] http://www.w3.org/TR/xml11/#charsets
+            // [4] http://www.w3.org/TR/xml11/#sec-references
+            // [5] http://www.w3.org/TR/xml11/#sec-cdata-sect
+        }
 	}
 }
